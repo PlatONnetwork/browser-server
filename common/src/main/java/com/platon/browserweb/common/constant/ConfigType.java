11d9bbb1dfd2294b54fde5e4a8e94d7d687a46a8
dongqile <dongqile@juzix.io>
2018-10-23 10:26:54 +0800

    Directory update

--- common/src/main/java/com/platon/browserweb/common/constant/ConfigType.java
+++
@@ -1,7 +0,0 @@
-package com.platon.browserweb.common.constant;
-
-public interface ConfigType {
-	//系统设置配置类型
-    public static final String INVESTOR = "investor";
-    
-}
--- common/src/main/java/com/platon/browserweb/common/validate/CategoryGroup.java
+++
@@ -1,9 +0,0 @@
-package com.platon.browserweb.common.validate;
-
-/**
- * User: dongqile
- * Date: 2018/7/2
- * Time: 19:57
- */
-public interface CategoryGroup {
-}
