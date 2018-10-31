ca6cc05e173a89b025ec30ce296098d2545454af
dongqile <dongqile@juzix.io>
2018-10-31 17:45:02 +0800

    update

--- agent/src/main/java/com/platon/browser/agent/service/impl/BlockStorageServiceImpl.java
+++ agent/src/main/java/com/platon/browser/agent/service/impl/BlockStorageServiceImpl.java
@@ -11,6 +11,7 @@ import com.platon.browser.common.enums.MqMessageTypeEnum;
 import com.platon.browser.common.enums.RetEnum;
 import com.platon.browser.dao.entity.Block;
 import com.platon.browser.dao.entity.Transaction;
+import com.platon.browser.dao.entity.TransactionWithBLOBs;
 import com.platon.browser.dao.mapper.BlockMapper;
 import com.platon.browser.dao.mapper.TransactionMapper;
 import org.slf4j.Logger;
@@ -55,64 +56,75 @@ public class BlockStorageServiceImpl implements BlockStorageService {
     )
     public void receive ( String msg ) {
         Message message = JSON.parseObject(msg,Message.class);
-        BaseResp resp;
         switch (MqMessageTypeEnum.valueOf(message.getType().toUpperCase())){
             case BLOCK:
                 logger.info("STOMP区块信息入库: {}",msg);
                 BlockDto blockDto = JSON.parseObject(message.getStruct(),BlockDto.class);
-                resp = BaseResp.build(RetEnum.RET_SUCCESS.getCode(),RetEnum.RET_SUCCESS.getName(),blockDto);
-                messagingTemplate.convertAndSend("/topic/block/new?cid="+message.getChainId(), resp);
                 //构建dto结构，转存数据库结构
                 //区块相关block
-                Block block = new Block();
-                block.setNumber(Long.valueOf(blockDto.getNumber()));
-                block.setTimestamp(new Date(blockDto.getTimestamp()));
-                block.setSize((int)blockDto.getSize());//考虑转换格式类型，高精度转低精度可能会导致数据失准
-                block.setMiner(blockDto.getMiner());
-                block.setExtraData(blockDto.getExtraData());
-                block.setNonce(blockDto.getNonce());
-                block.setParentHash(blockDto.getParentHash());
-                block.setChainId(message.getChainId());
-                block.setHash(blockDto.getHash());
-                block.setBlockReward(blockDto.getBlockReward());
-                block.setEnergonAverage(blockDto.getEnergonAverage().toString());
-                block.setEnergonLimit(blockDto.getEnergonLimit().toString());
-                block.setEnergonUsed(blockDto.getEnergonUsed().toString());
-                block.setCreateTime(new Date());
-                block.setUpdateTime(new Date());
+                Block block = bulidBlock(blockDto,message);
                 blockMapper.insert(block);
 
                 //交易相关transaction
-                List<Transaction> transactionList = new ArrayList <>();
-                List<TransactionDto> transactionDtos = blockDto.getTransaction();
-                for(TransactionDto transactionDto : transactionDtos){
-                    Transaction transaction = new Transaction();
-                    transaction.setBlockHash(transactionDto.getBlockHash());
-                    transaction.setActualTxCost(transactionDto.getActualTxCoast().toString());
-                    transaction.setBlockNumber(Long.valueOf(transactionDto.getBlockNumber().toString()));
-                    transaction.setChainId(message.getChainId());
-                    transaction.setCreateTime(new Date());
-                    transaction.setUpdateTime(new Date());
-                    transaction.setEnergonLimit(transactionDto.getEnergonLimit().toString());
-                    transaction.setEnergonPrice(transactionDto.getEnergonPrice().toString());
-                    transaction.setEnergonUsed(transactionDto.getEnergonUsed().toString());
-                    transaction.setFrom(transactionDto.getFrom());
-                    transaction.setTo(transactionDto.getTo());
-                    transaction.setHash(transactionDto.getHash());
-                    transaction.setNonce(transactionDto.getNonce());
-                    transaction.setTimestamp(transaction.getTimestamp());
-                    transaction.setTransactionIndex(transactionDto.getTransactionIndex().intValue());
-                    transaction.setTxReceiptStatus(transaction.getTxReceiptStatus());
-                    transaction.setTxType(transactionDto.getTxType());
-                    transaction.setValue(transactionDto.getValue());
-                    transactionList.add(transaction);
+                if(blockDto.getTransaction().size() > 0){
+                    List<TransactionWithBLOBs> transactionList = buildTransaction(blockDto,message);
+                    transactionMapper.batchInsert(transactionList);
+                }else {
+                    logger.info("STOMP区块信息中交易信息为空: {}",msg);
                 }
-                transactionMapper.insert(transactionList);
                 break;
 
             case PENDING:
                 break;
 
         }
+    }
+
+    private Block bulidBlock(BlockDto blockDto,Message message){
+        Block block = new Block();
+        block.setNumber(Long.valueOf(blockDto.getNumber()));
+        block.setTimestamp(new Date(blockDto.getTimestamp()));
+        block.setSize((int)blockDto.getSize());//考虑转换格式类型，高精度转低精度可能会导致数据失准
+        block.setMiner(blockDto.getMiner());
+        block.setExtraData(blockDto.getExtraData());
+        block.setNonce(blockDto.getNonce());
+        block.setParentHash(blockDto.getParentHash());
+        block.setChainId(message.getChainId());
+        block.setHash(blockDto.getHash());
+        block.setBlockReward(blockDto.getBlockReward());
+        block.setEnergonAverage(blockDto.getEnergonAverage().toString());
+        block.setEnergonLimit(blockDto.getEnergonLimit().toString());
+        block.setEnergonUsed(blockDto.getEnergonUsed().toString());
+        block.setCreateTime(new Date());
+        block.setUpdateTime(new Date());
+        return block;
+    }
+
+    private List<TransactionWithBLOBs> buildTransaction(BlockDto blockDto,Message message){
+        List<TransactionWithBLOBs> transactionList = new ArrayList <>();
+        List<TransactionDto> transactionDtos = blockDto.getTransaction();
+        for(TransactionDto transactionDto : transactionDtos){
+            TransactionWithBLOBs transaction = new TransactionWithBLOBs();
+            transaction.setBlockHash(transactionDto.getBlockHash());
+            transaction.setActualTxCost(transactionDto.getActualTxCoast().toString());
+            transaction.setBlockNumber(Long.valueOf(transactionDto.getBlockNumber().toString()));
+            transaction.setChainId(message.getChainId());
+            transaction.setCreateTime(new Date());
+            transaction.setUpdateTime(new Date());
+            transaction.setEnergonLimit(transactionDto.getEnergonLimit().toString());
+            transaction.setEnergonPrice(transactionDto.getEnergonPrice().toString());
+            transaction.setEnergonUsed(transactionDto.getEnergonUsed().toString());
+            transaction.setFrom(transactionDto.getFrom());
+            transaction.setTo(transactionDto.getTo());
+            transaction.setHash(transactionDto.getHash());
+            transaction.setNonce(transactionDto.getNonce());
+            transaction.setTimestamp(transaction.getTimestamp());
+            transaction.setTransactionIndex(transactionDto.getTransactionIndex().intValue());
+            transaction.setTxReceiptStatus(transaction.getTxReceiptStatus());
+            transaction.setTxType(transactionDto.getTxType());
+            transaction.setValue(transactionDto.getValue());
+            transactionList.add(transaction);
+        }
+        return transactionList;
     }
 }
