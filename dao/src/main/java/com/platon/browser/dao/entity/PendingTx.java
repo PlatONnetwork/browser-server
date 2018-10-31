b271bfcb0650dde70652eeae81b53be29c660719
chendongming <chendongming@juzix.net>
2018-10-31 10:45:34 +0800

    代码结构调整

--- dao/src/main/java/com/platon/browser/dao/entity/PendingTx.java
+++ dao/src/main/java/com/platon/browser/dao/entity/PendingTx.java
@@ -9,9 +9,9 @@ public class PendingTx {
 
     private String to;
 
-    private Integer energonUsed;
+    private String energonUsed;
 
-    private Integer energonLimit;
+    private String energonLimit;
 
     private String energonPrice;
 
@@ -53,20 +53,20 @@ public class PendingTx {
         this.to = to == null ? null : to.trim();
     }
 
-    public Integer getEnergonUsed() {
+    public String getEnergonUsed() {
         return energonUsed;
     }
 
-    public void setEnergonUsed(Integer energonUsed) {
-        this.energonUsed = energonUsed;
+    public void setEnergonUsed(String energonUsed) {
+        this.energonUsed = energonUsed == null ? null : energonUsed.trim();
     }
 
-    public Integer getEnergonLimit() {
+    public String getEnergonLimit() {
         return energonLimit;
     }
 
-    public void setEnergonLimit(Integer energonLimit) {
-        this.energonLimit = energonLimit;
+    public void setEnergonLimit(String energonLimit) {
+        this.energonLimit = energonLimit == null ? null : energonLimit.trim();
     }
 
     public String getEnergonPrice() {
