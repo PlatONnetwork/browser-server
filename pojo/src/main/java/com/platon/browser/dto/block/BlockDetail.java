fd54894297bd5c042df62c5d55baf60e767aa6e3
chendongming <chendongming@juzix.net>
2018-11-02 14:47:26 +0800

    代码结构调整

--- pojo/src/main/java/com/platon/browser/dto/block/BlockDetail.java
+++ pojo/src/main/java/com/platon/browser/dto/block/BlockDetail.java
@@ -11,8 +11,8 @@ public class BlockDetail {
     private String parentHash;
     private String miner;
     private int size;
-    private int energonLimit;
-    private int energonUsed;
+    private String energonLimit;
+    private String energonUsed;
     private String blockReward;
     private String extraData;
     private long timeDiff;
