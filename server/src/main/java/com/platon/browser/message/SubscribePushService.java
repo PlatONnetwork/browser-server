bca855378403d04d4ecb07fd267e4e7b9d436085
chendongming <chendongming@juzix.net>
2018-11-12 16:31:59 +0800

    代码结构调整

--- server/src/main/java/com/platon/browser/message/SubscribePushService.java
+++ server/src/main/java/com/platon/browser/message/SubscribePushService.java
@@ -2,13 +2,11 @@ package com.platon.browser.message;
 
 import com.alibaba.fastjson.JSON;
 import com.maxmind.geoip.Location;
-import com.platon.browser.common.base.BaseResp;
 import com.platon.browser.common.dto.agent.BlockDto;
 import com.platon.browser.common.dto.agent.NodeDto;
 import com.platon.browser.common.dto.agent.TransactionDto;
 import com.platon.browser.common.dto.mq.Message;
 import com.platon.browser.common.enums.MqMessageTypeEnum;
-import com.platon.browser.common.enums.RetEnum;
 import com.platon.browser.config.ChainsConfig;
 import com.platon.browser.dto.IndexInfo;
 import com.platon.browser.dto.StatisticInfo;
@@ -23,7 +21,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -35,9 +32,6 @@ public class SubscribePushService {
     private final Logger logger = LoggerFactory.getLogger(SubscribePushService.class);
 
     @Autowired
-    private SimpMessagingTemplate messagingTemplate;
-
-    @Autowired
     private CacheService cacheService;
 
     @Autowired
@@ -46,8 +40,6 @@ public class SubscribePushService {
     @RabbitListener(queues = "#{platonQueue.name}")
     public void receive(String msg) {
         Message message = JSON.parseObject(msg,Message.class);
-        BaseResp resp;
-
         String chainId = message.getChainId();
         if(!chainsConfig.isValid(chainId)){
             logger.error("链ID={}在本系统中未作配置！",message.getChainId());
@@ -58,13 +50,11 @@ public class SubscribePushService {
             case NODE:
                 logger.debug("更新增量节点缓存: {}",msg);
                 NodeDto nodeDto = JSON.parseObject(message.getStruct(), NodeDto.class);
-
                 NodeInfo nodeInfo = new NodeInfo();
                 BeanUtils.copyProperties(nodeDto,nodeInfo);
                 Location location = GeoUtil.getLocation(nodeDto.getIp());
                 nodeInfo.setLatitude(location.latitude);
                 nodeInfo.setLongitude(location.longitude);
-                // 更新节点缓存信息
                 List<NodeInfo> nodeInfoList = new ArrayList<>();
                 nodeInfoList.add(nodeInfo);
                 cacheService.updateNodeCache(nodeInfoList,false,chainId);
@@ -72,7 +62,6 @@ public class SubscribePushService {
             case BLOCK:
                 logger.debug("更新增量区块缓存: {}",msg);
                 BlockDto blockDto = JSON.parseObject(message.getStruct(),BlockDto.class);
-
                 BlockInfo blockInfo = new BlockInfo();
                 BeanUtils.copyProperties(blockDto,blockInfo);
                 blockInfo.setServerTime(System.currentTimeMillis());
@@ -81,31 +70,20 @@ public class SubscribePushService {
                 blockInfo.setHeight(blockDto.getNumber());
                 blockInfo.setBlockReward(blockDto.getBlockReward());
                 blockInfo.setTransaction(blockDto.getTransaction().size());
-                // 更新区块缓存
                 List<BlockInfo> blockInfoList = new ArrayList<>();
                 blockInfoList.add(blockInfo);
                 cacheService.updateBlockCache(blockInfoList,chainId);
-                // 推送新区块信息
-                resp = BaseResp.build(RetEnum.RET_SUCCESS.getCode(),RetEnum.RET_SUCCESS.getName(),blockInfoList);
-                messagingTemplate.convertAndSend("/topic/block/new?cid="+chainId, resp);
 
-                logger.debug("更新整体指标缓存: {}",msg);
+                logger.debug("整体更新指标缓存: {}",msg);
                 IndexInfo indexInfo = new IndexInfo();
                 indexInfo.setCurrentHeight(blockInfo.getHeight());
                 indexInfo.setCurrentTransaction(blockDto.getTransaction().size());
-                // 更新指标缓存信息
                 cacheService.updateIndexCache(indexInfo,false, chainId);
-                // 推送整体指标信息
-                indexInfo = cacheService.getIndexInfo(chainId);
-                resp = BaseResp.build(RetEnum.RET_SUCCESS.getCode(),RetEnum.RET_SUCCESS.getName(),indexInfo);
-                messagingTemplate.convertAndSend("/topic/index/new?cid="+chainId, resp);
 
                 logger.debug("更新交易缓存: {}",msg);
                 List<TransactionDto> transactionDtos = blockDto.getTransaction();
-
                 List<TransactionInfo> transactionInfos = new ArrayList<>();
                 if(transactionDtos.size()>0){
-                    // 只有区块有交易数据才推送交易信息
                     transactionDtos.forEach(transactionDto -> {
                         TransactionInfo bean = new TransactionInfo();
                         BeanUtils.copyProperties(transactionDto,bean);
@@ -118,14 +96,10 @@ public class SubscribePushService {
                         bean.setValue(transactionDto.getValue());
                         transactionInfos.add(bean);
                     });
-                    // 更新交易缓存信息
                     cacheService.updateTransactionCache(transactionInfos,chainId);
-                    // 推送新的交易信息
-                    resp = BaseResp.build(RetEnum.RET_SUCCESS.getCode(),RetEnum.RET_SUCCESS.getName(),transactionInfos);
-                    messagingTemplate.convertAndSend("/topic/transaction/new?cid="+chainId, resp);
                 }
 
-                logger.debug("更新整体统计缓存: {}",msg);
+                logger.debug("整体更新统计缓存: {}",msg);
                 StatisticInfo statisticInfo = new StatisticInfo();
                 statisticInfo.setHighestBlockNumber(blockInfo.getHeight());
                 statisticInfo.setBlockCount(1l);
@@ -137,7 +111,6 @@ public class SubscribePushService {
                 statisticItem.setTransaction(Long.valueOf(transactionInfos.size()));
                 statisticItems.add(statisticItem);
                 statisticInfo.setBlockStatisticList(statisticItems);
-                // 更新统计缓存信息
                 cacheService.updateStatisticCache(statisticInfo,false,chainId);
                 break;
         }
