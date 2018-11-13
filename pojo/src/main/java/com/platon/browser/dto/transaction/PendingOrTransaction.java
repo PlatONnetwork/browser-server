fc6530e1655e137d973e9f5d239609da68072573
chendongming <chendongming@juzix.net>
2018-11-13 09:20:11 +0800

    接口调整

--- pojo/src/main/java/com/platon/browser/dto/transaction/PendingOrTransaction.java
+++ pojo/src/main/java/com/platon/browser/dto/transaction/PendingOrTransaction.java
@@ -5,5 +5,5 @@ import lombok.Data;
 @Data
 public class PendingOrTransaction {
     private String type; // transaction , pending
-    private PendingTxDetail data;
+    private PendingTxDetail pending; // 待处理交易数据
 }
