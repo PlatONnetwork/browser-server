e7e2e0856dbb5591acffa103f76a1e7457d6fed7
dongqile <dongqile@juzix.io>
2018-11-13 14:36:15 +0800

    syn block task update

--- server/src/test/java/com/platon/browser/queue/QueueTest.java
+++ server/src/test/java/com/platon/browser/queue/QueueTest.java
@@ -1,7 +1,7 @@
 package com.platon.browser.queue;
 
 
-import com.platon.browser.dto.LimitQueue;
+import com.platon.browser.dto.cache.LimitQueue;
 
 public class QueueTest {
     public static void main(String[] args) {
@@ -10,6 +10,6 @@ public class QueueTest {
         queue.offer(2);
         queue.offer(4);
         queue.offer(5);
-        System.out.println(queue.elements());
+   /*     System.out.println(queue.elements());*/
     }
 }
