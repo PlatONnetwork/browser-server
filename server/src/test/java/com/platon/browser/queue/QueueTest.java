1ed6a83bb8222022b5d1961ba402b14288efcd80
chendongming <chendongming@juzix.net>
2018-11-13 11:50:58 +0800

    代码结构调整

--- server/src/test/java/com/platon/browser/queue/QueueTest.java
+++ server/src/test/java/com/platon/browser/queue/QueueTest.java
@@ -1,15 +1,25 @@
 package com.platon.browser.queue;
 
 
-import com.platon.browser.dto.LimitQueue;
+import com.platon.browser.dto.cache.LimitQueue;
+
+import java.util.Collections;
+import java.util.List;
 
 public class QueueTest {
     public static void main(String[] args) {
         LimitQueue<Integer> queue = new LimitQueue<>(3);
         queue.offer(1);
         queue.offer(2);
-        queue.offer(4);
+        queue.offer(8);
         queue.offer(5);
-        System.out.println(queue.elements());
+
+        List<Integer> list = queue.list();
+        Collections.sort(list,(c1,c2)->{
+            if(c1>c2) return -1;
+            if(c1<c2) return 1;
+            return 0;
+        });
+        System.out.println(list);
     }
 }
