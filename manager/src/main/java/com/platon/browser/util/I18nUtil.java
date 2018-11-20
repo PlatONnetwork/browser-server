d34cd1ae74f5b90501deb72594b985c384b274d7
chendongming <chendongming@juzix.net>
2018-11-20 17:17:29 +0800

    消息国际化

--- manager/src/main/java/com/platon/browser/util/I18nUtil.java
+++ manager/src/main/java/com/platon/browser/util/I18nUtil.java
@@ -1,15 +1,16 @@
 package com.platon.browser.util;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
 
 import java.util.Locale;
 
+@Component
 public class I18nUtil {
+    @Autowired
     private MessageSource messageSource;
-    public I18nUtil(MessageSource messageSource){
-        this.messageSource=messageSource;
-    }
     public String i(I18nEnum key, Object... param){
         Locale locale = LocaleContextHolder.getLocale();
         String msg = messageSource.getMessage(key.name().toLowerCase(),param, locale);
