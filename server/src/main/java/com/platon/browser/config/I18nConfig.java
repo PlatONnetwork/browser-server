63598a8d518cdcacf924c98be6494ce2eb665453
chendongming <chendongming@juzix.net>
2018-11-20 17:02:39 +0800

    消息国际化

--- server/src/main/java/com/platon/browser/config/I18nConfig.java
+++ server/src/main/java/com/platon/browser/config/I18nConfig.java
@@ -1,30 +1,14 @@
 package com.platon.browser.config;
 
+import com.platon.browser.util.I18nUtil;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class I18NConfig extends WebMvcConfigurerAdapter {
-
-    /**
-     * 配置自己的国际化语言解析器
-     * @return
-     */
+public class I18nConfig {
     @Bean
-    public LocaleResolver localeResolver() {
-        return new PlatonLocaleResolver();
+    public I18nUtil i18n(MessageSource messageSource) {
+        return new I18nUtil(messageSource);
     }
-
-    /**
-     * 配置自己的拦截器
-     */
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        //super.addInterceptors(registry);
-    }
-
-
 }
