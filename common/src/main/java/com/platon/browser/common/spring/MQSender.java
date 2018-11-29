77f51f31286c122855be57c8410932dbe2f008ff
chendongming <chendongming@juzix.net>
2018-11-29 17:11:13 +0800

    RabbitMQ消息发送添加回调检查是否成功机制

--- common/src/main/java/com/platon/browser/common/spring/MQSender.java
+++ common/src/main/java/com/platon/browser/common/spring/MQSender.java
@@ -2,19 +2,34 @@ package com.platon.browser.common.spring;
 
 import com.alibaba.fastjson.JSON;
 import com.platon.browser.common.dto.mq.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class MQSender {
+
+    private final static Logger logger = LoggerFactory.getLogger(MQSender.class);
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
     @Autowired
     private FanoutExchange fanout;
+
+    @PostConstruct
+    private void init(){
+        rabbitTemplate.setReturnCallback(((message, replyCode, replyText, exchange, routingKey) -> {
+            String correlationId = message.getMessageProperties().getCorrelationIdString();
+            logger.info("消息：{}发送失败，应答码：{},原因：{}，交换机：{}, 路由器：{}",
+                    correlationId,replyCode,replyText,exchange,routingKey);
+        }));
+    }
 
     /**
      * 发送消息
