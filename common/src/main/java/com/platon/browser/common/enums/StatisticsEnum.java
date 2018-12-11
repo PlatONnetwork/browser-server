38aedd8c0d12d2caacd7d25a070ccce7ee823923
chendongming <chendongming@juzix.net>
2018-12-11 14:31:54 +0800

    统计枚举添加反向获取方法

--- common/src/main/java/com/platon/browser/common/enums/StatisticsEnum.java
+++ common/src/main/java/com/platon/browser/common/enums/StatisticsEnum.java
@@ -1,5 +1,9 @@
 package com.platon.browser.common.enums;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * \*
  * \* User: dongqile
@@ -8,5 +12,13 @@ package com.platon.browser.common.enums;
  * \
  */
 public enum StatisticsEnum {
-    reward_amount,profit_amount,verify_count,block_count,block_reward
+    reward_amount,profit_amount,verify_count,block_count,block_reward;
+
+    private static Map<String, StatisticsEnum> map = new HashMap<>();
+    static {
+        Arrays.asList(StatisticsEnum.values()).forEach(snum->map.put(snum.name(),snum));
+    }
+    public static StatisticsEnum getEnum(String name){
+        return map.get(name);
+    }
 }
