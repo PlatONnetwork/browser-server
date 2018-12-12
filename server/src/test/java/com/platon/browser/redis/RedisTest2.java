5c97390984ef8116a68f1b00c1e144341b04e98b
chendongming <chendongming@juzix.net>
2018-12-12 16:32:53 +0800

    修改

--- server/src/test/java/com/platon/browser/redis/RedisTest2.java
+++ server/src/test/java/com/platon/browser/redis/RedisTest2.java
@@ -1,32 +1,17 @@
 package com.platon.browser.redis;
 
-import com.alibaba.fastjson.JSON;
-import com.github.pagehelper.PageHelper;
-import com.platon.browser.ServiceApplication;
-import com.platon.browser.dao.entity.Block;
-import com.platon.browser.dao.entity.BlockExample;
-import com.platon.browser.dao.entity.Transaction;
-import com.platon.browser.dao.entity.TransactionExample;
-import com.platon.browser.dao.mapper.BlockMapper;
-import com.platon.browser.dao.mapper.TransactionMapper;
+import com.platon.browser.ServerApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.DefaultTypedTuple;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.ObjectUtils;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes= ServiceApplication.class)
+@SpringBootTest(classes= ServerApplication.class)
 public class RedisTest2 {
 
     @Autowired
