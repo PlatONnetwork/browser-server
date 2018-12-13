2b026530f0ec69cecbab6b3348e516d88038b621
dongqile <dongqile@juzix.io>
2018-12-13 10:39:33 +0800

    node data modify

--- agent/src/main/java/com/platon/browser/agent/job/PendingTxUpdateTaskJob.java
+++ agent/src/main/java/com/platon/browser/agent/job/PendingTxUpdateTaskJob.java
@@ -10,12 +10,11 @@ import com.platon.browser.dao.entity.Transaction;
 import com.platon.browser.dao.entity.TransactionExample;
 import com.platon.browser.dao.mapper.PendingTxMapper;
 import com.platon.browser.dao.mapper.TransactionMapper;
-import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StopWatch;
 
-import java.math.BigInteger;
 import java.util.List;
 
 
