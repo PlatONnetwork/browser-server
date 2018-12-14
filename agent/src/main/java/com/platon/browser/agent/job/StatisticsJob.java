395f994df20317f2b4a2bd2a72a3d3c75a05f2b3
dongqile <dongqile@juzix.io>
2018-12-14 11:03:54 +0800

    update

--- agent/src/main/java/com/platon/browser/agent/job/StatisticsJob.java
+++ agent/src/main/java/com/platon/browser/agent/job/StatisticsJob.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.StopWatch;
 import org.springframework.util.StringUtils;
