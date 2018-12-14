950c93833621d1060be26d5bea38e4e95e390df2
chendongming <chendongming@juzix.net>
2018-12-14 09:54:40 +0800

    RabbitMq交换机名称配置抽到application.properties文件中

--- manager/src/main/java/com/platon/browser/config/RabbitMqConfig.java
+++ manager/src/main/java/com/platon/browser/config/RabbitMqConfig.java
@@ -1,15 +1,19 @@
 package com.platon.browser.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfig {
 
+    @Value("${spring.rabbitmq.exchange.name}")
+    private String exchangeName;
+
     @Bean
     public FanoutExchange fanout() {
-        return new FanoutExchange("platon.fanout");
+        return new FanoutExchange(exchangeName);
     }
 
     @Bean
