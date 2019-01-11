6e80732bd26792c031b293f162944d3213df6841
chendongming <chendongming@juzix.net>
2019-01-11 18:52:30 +0800

    update

--- server/src/main/java/com/platon/browser/ServerApplication.java
+++ server/src/main/java/com/platon/browser/ServerApplication.java
@@ -2,9 +2,6 @@ package com.platon.browser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
