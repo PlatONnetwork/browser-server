76fd4c2aa0a59d2c5101c072364f48da3f8593a4
chendongming <chendongming@juzix.net>
2019-01-11 18:52:19 +0800

    update

--- server/src/main/java/com/platon/browser/ServerApplication.java
+++ server/src/main/java/com/platon/browser/ServerApplication.java
@@ -11,13 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class ServerApplication extends SpringBootServletInitializer {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(ServerApplication.class);
-    }
 }
