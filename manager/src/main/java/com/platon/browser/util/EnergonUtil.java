7e9980813d5ca5aa4a90bcbab7c1be35076d95a3
chendongming <chendongming@juzix.net>
2019-01-17 17:22:54 +0800

    update

--- manager/src/main/java/com/platon/browser/util/EnergonUtil.java
+++ manager/src/main/java/com/platon/browser/util/EnergonUtil.java
@@ -4,7 +4,7 @@ public class EnergonUtil {
     private final static String PATTERN = ".##################";
     public static String convert(Object number){
         String result = NumberUtil.format(number,PATTERN);
-        if("0.0".equals(result)) return "0";
+        if(".0".equals(result)) return "0";
         if(result.startsWith(".")) result = "0"+result;
         return result;
     }
