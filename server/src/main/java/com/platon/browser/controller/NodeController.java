de9ccc785d643b9a2351aab25ee823f338e60af1
chendongming <chendongming@juzix.net>
2019-01-17 20:50:35 +0800

    update

--- server/src/main/java/com/platon/browser/controller/NodeController.java
+++ server/src/main/java/com/platon/browser/controller/NodeController.java
@@ -12,6 +12,7 @@ import com.platon.browser.dto.node.NodeDetail;
 import com.platon.browser.dto.node.NodeListItem;
 import com.platon.browser.exception.ResponseException;
 import com.platon.browser.req.block.BlockDownloadReq;
+import com.platon.browser.req.block.BlockListReq;
 import com.platon.browser.req.node.NodeDetailReq;
 import com.platon.browser.req.node.NodePageReq;
 import com.platon.browser.service.ExportService;
@@ -230,7 +231,7 @@ public class NodeController {
      * }
      */
     @PostMapping("blockList")
-    public BaseResp blockList (@Valid @RequestBody BlockDownloadReq req) {
+    public BaseResp blockList (@Valid @RequestBody BlockListReq req) {
         if(!chainsConfig.isValid(req.getCid())){
             throw new ResponseException(i18n.i(I18nEnum.CHAIN_ID_ERROR,req.getCid()));
         }
