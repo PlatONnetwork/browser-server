e1574a408861a6ccce1d5e65114539921fbbe3f4
chendongming <chendongming@juzix.net>
2019-01-24 15:28:32 +0800

    update

--- dao/src/main/java/com/platon/browser/dao/mapper/CustomBlockMapper.java
+++ dao/src/main/java/com/platon/browser/dao/mapper/CustomBlockMapper.java
@@ -4,11 +4,12 @@ import com.platon.browser.dao.entity.Block;
 import com.platon.browser.dao.entity.BlockPage;
 import org.apache.ibatis.annotations.Param;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
 
 public interface CustomBlockMapper {
     List<Block> selectByPage(@Param("page") BlockPage page);
 
     List<Block> selectBlockByNumber( @Param("chainId")String chainId, @Param("start")long start,@Param("end")long end);
+
+    void updateBlockNodeName(@Param("chainId")String chainId);
 }
