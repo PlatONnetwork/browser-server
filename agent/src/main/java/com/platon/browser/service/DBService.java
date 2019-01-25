0172e9341ee86692c2e4e4bc1404df3e57a650e5
chendongming <chendongming@juzix.net>
2019-01-25 11:54:40 +0800

    update

--- agent/src/main/java/com/platon/browser/service/DBService.java
+++ agent/src/main/java/com/platon/browser/service/DBService.java
@@ -1,7 +1,5 @@
 package com.platon.browser.service;
 
-import com.platon.browser.dao.entity.Transaction;
-import com.platon.browser.dao.entity.TransactionExample;
 import com.platon.browser.dao.mapper.BlockMapper;
 import com.platon.browser.dao.mapper.BlockMissingMapper;
 import com.platon.browser.dao.mapper.CustomBlockMapper;
@@ -12,10 +10,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 
 @Component
 public class DBService {
@@ -45,22 +41,9 @@ public class DBService {
         }
 
         if(result.transactions.size()>0){
-            // 更新交易前，需要先根据区块号和交易索引排序
-            Collections.sort(result.transactions,(t1,t2)->{
-                if(t1.getBlockNumber()>t2.getBlockNumber()) return 1;
-                if(t1.getBlockNumber()<t2.getBlockNumber()) return -1;
-                if(t1.getTransactionIndex()>t2.getTransactionIndex()) return 1;
-                if(t1.getTransactionIndex()<t2.getTransactionIndex()) return -1;
-                return 0;
-            });
             transactionMapper.batchInsert(result.transactions);
             // 更新缓存
-            List<String> txHashes = new ArrayList<>();
-            result.transactions.forEach(transaction->txHashes.add(transaction.getHash()));
-            TransactionExample condition = new TransactionExample();
-            condition.createCriteria().andChainIdEqualTo(chainId).andHashIn(txHashes);
-            List<Transaction> dbTrans = transactionMapper.selectByExample(condition);
-            redisCacheService.updateTransactionCache(chainId,new HashSet<>(dbTrans));
+            redisCacheService.updateTransactionCache(chainId,new HashSet<>(result.transactions));
         }
 
         if(result.errorBlocks.size()>0){
