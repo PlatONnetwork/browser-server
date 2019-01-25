0782e1102c540e658f6154ad79cb75abcc396efc
chendongming <chendongming@juzix.net>
2019-01-25 18:46:24 +0800

    update

--- manager/src/main/java/com/platon/browser/dto/StatisticGraphData.java
+++ manager/src/main/java/com/platon/browser/dto/StatisticGraphData.java
@@ -8,6 +8,6 @@ import java.util.List;
 @Data
 public class StatisticGraphData {
     private List<Long> x=new LinkedList<>();
-    private List<Long> ya=new LinkedList<>();
+    private List<Double> ya=new LinkedList<>();
     private List<Long> yb=new LinkedList<>();
 }
