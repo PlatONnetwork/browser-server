ff6099acbb817c76647a04eaee1e907245f71228
chendongming <chendongming@juzix.net>
2019-01-26 12:53:34 +0800

    update

--- agent/src/main/java/com/platon/browser/job/BlockAnalyseJob.java
+++ agent/src/main/java/com/platon/browser/job/BlockAnalyseJob.java
@@ -10,7 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.web3j.protocol.core.DefaultBlockParameter;
 import org.web3j.protocol.core.methods.response.EthBlock;
@@ -31,8 +30,8 @@ public class BlockAnalyseJob {
     private static Logger logger = LoggerFactory.getLogger(BlockAnalyseJob.class);
     @Autowired
     private BlockMapper blockMapper;
-    @Value("${platon.thread.batch.size}")
-    private int threadBatchSize;
+    @Value("${platon.block.batch.num}")
+    private int batchNum;
     @Autowired
     private PlatonClient platon;
     // 起始区块号
@@ -54,47 +53,50 @@ public class BlockAnalyseJob {
         } else {
             beginNumber = blocks.get(0).getNumber()+1;
         }
+        analyseBlock();
     }
 
     /**
      * 分析区块及其内部的交易数据
      */
-    @Scheduled(cron="0/1 * * * * ?")
+//    @Scheduled(cron="0/1 * * * * ?")
     protected void analyseBlock () {
-        logger.debug("*** In the BlockAnalyseJob *** ");
-        try {
-            // 需要并发处理的区块数据
-            List<EthBlock> concurrentBlocks = new ArrayList<>();
-            // 结束区块号
-            BigInteger endNumber = platon.getWeb3j().ethBlockNumber().send().getBlockNumber();
-            if((endNumber.intValue()-beginNumber)<threadBatchSize){
-                while (beginNumber<=endNumber.longValue()){
-                    EthBlock ethBlock = platon.getWeb3j().ethGetBlockByNumber(DefaultBlockParameter.valueOf(BigInteger.valueOf(beginNumber)),true).send();
-                    concurrentBlocks.add(ethBlock);
-                    beginNumber++;
-                }
-                analyseThread.analyse(concurrentBlocks);
-                concurrentBlocks.clear();
-            }else{
-                while (beginNumber<=endNumber.longValue()){
-                    EthBlock ethBlock = platon.getWeb3j().ethGetBlockByNumber(DefaultBlockParameter.valueOf(BigInteger.valueOf(beginNumber)),true).send();
-                    concurrentBlocks.add(ethBlock);
-                    long range = endNumber.longValue()-beginNumber+1;
-                    if(
-                        // 如果并发区块数量达到线程处理阈值，开启线程处理
-                        (concurrentBlocks.size()>=threadBatchSize)||
-                        // 追上之后，有多少处理多少
-                        (concurrentBlocks.size()>=range)
-                    ){
-                        analyseThread.analyse(concurrentBlocks);
-                        concurrentBlocks.clear();
+        while (true){
+            logger.debug("*** In the BlockAnalyseJob *** ");
+            try {
+                // 需要并发处理的区块数据
+                List<EthBlock> concurrentBlocks = new ArrayList<>();
+                // 结束区块号
+                BigInteger endNumber = platon.getWeb3j().ethBlockNumber().send().getBlockNumber();
+                if((endNumber.intValue()-beginNumber)<batchNum){
+                    while (beginNumber<=endNumber.longValue()){
+                        EthBlock ethBlock = platon.getWeb3j().ethGetBlockByNumber(DefaultBlockParameter.valueOf(BigInteger.valueOf(beginNumber)),true).send();
+                        concurrentBlocks.add(ethBlock);
+                        beginNumber++;
                     }
-                    beginNumber++;
+                    analyseThread.analyse(concurrentBlocks);
+                    concurrentBlocks.clear();
+                }else{
+                    while (beginNumber<=endNumber.longValue()){
+                        EthBlock ethBlock = platon.getWeb3j().ethGetBlockByNumber(DefaultBlockParameter.valueOf(BigInteger.valueOf(beginNumber)),true).send();
+                        concurrentBlocks.add(ethBlock);
+                        long range = endNumber.longValue()-beginNumber+1;
+                        if(
+                            // 如果并发区块数量达到线程处理阈值，开启线程处理
+                                (concurrentBlocks.size()>=batchNum)||
+                                        // 追上之后，有多少处理多少
+                                        (concurrentBlocks.size()>=range)
+                        ){
+                            analyseThread.analyse(concurrentBlocks);
+                            concurrentBlocks.clear();
+                        }
+                        beginNumber++;
+                    }
                 }
+            } catch (IOException e) {
+                logger.error("BlockAnalyseJob Exception:{}", e.getMessage());
             }
-        } catch (IOException e) {
-            logger.error("BlockAnalyseJob Exception:{}", e.getMessage());
+            logger.debug("*** End the BlockAnalyseJob *** ");
         }
-        logger.debug("*** End the BlockAnalyseJob *** ");
     }
 }
