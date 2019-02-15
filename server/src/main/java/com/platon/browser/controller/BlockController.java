183eb90cb4f97eb84c0248dcdaed1f95556d4caf
chendongming <chendongming@juzix.net>
2019-02-15 15:34:15 +0800

    添加查询区块投票和竞选交易列表接口

--- server/src/main/java/com/platon/browser/controller/BlockController.java
+++ server/src/main/java/com/platon/browser/controller/BlockController.java
@@ -96,95 +96,6 @@ public class BlockController  {
     }
 
     /**
-      @api {post} block/transactionList a.区块列表
-      @apiVersion 1.0.0
-      @apiName transactionList
-      @apiGroup block
-      @apiDescription 区块列表
-      @apiUse CommonHeaderFiled
-      @apiParamExample {json} Request-Example:
-      {
-      "cid":"", // 链ID (必填)
-      "pageNo": 1,//页数(必填)
-      "pageSize": 10,//页大小(必填)
-      "blockNumber":500, // 区块号(必填)
-      "txType":"", // 交易类型 (可选), 可以设置多个类型，使用逗号“,”分隔
-              transfer ：转账
-              MPCtransaction ： MPC交易
-              contractCreate ： 合约创建
-              voteTicket ： 投票
-              transactionExecute ： 合约执行
-              authorization ： 权限
-              candidateDeposit ： 竞选质押
-              candidateApplyWithdraw ： 减持质押
-              candidateWithdraw ： 提取质押
-              unknown ： 未知
-      }
-
-      @apiSuccessExample {json} Success-Response:
-      HTTP/1.1 200 OK
-      {
-      "errMsg": "",//描述信息
-      "code": 0,//成功（0），失败则由相关失败码
-      "displayTotalCount":18,//显示总数
-      "totalCount":18,// 小于等于500000记录的总数
-      "totalPages":1,//总页数
-      "data": [
-      {
-          "txHash": "0x234234",//交易hash
-          "blockHeight": "15566",//交易所在区块高度
-          "blockTime": 18080899999,//出块时间
-          "from": "0x667766",//发送方, 必定是钱包地址
-          "to": "0x667766",//接收方, 此字段存储的可能是钱包地址，也可能是合约地址，需要使用receiveType来进一步区分：
-          // 如果receiveType的值为account，则是钱包地址；如果receiveType的值为contract，则是合约地址
-          "value": "222",//数额(单位:Energon)
-          "actualTxCost": "22",//交易费用(单位:Energon)
-          "txReceiptStatus": 1,//交易状态 -1 pending 1 成功  0 失败
-          "txType": "", // 交易类型
-              transfer ：转账
-              MPCtransaction ： MPC交易
-              contractCreate ： 合约创建
-              voteTicket ： 投票
-              transactionExecute ： 合约执行
-              authorization ： 权限
-              candidateDeposit ： 竞选质押
-              candidateApplyWithdraw ： 减持质押
-              candidateWithdraw ： 提取质押
-              unknown ： 未知
-          "txInfo": "{
-              "functionName":"",//方法名称
-              "parameters":{},//参数
-              "type":"1"//交易类型
-                  0：转账
-                  1：合约发布
-                  2：合约调用
-                  4：权限
-                  5：MPC交易
-                  1000：投票
-                  1001：竞选质押
-                  1002：减持质押
-                  1003：提取质押
-          }"//返回交易解析结构
-
-          "serverTime": 1123123,//服务器时间
-          "failReason":"",//失败原因
-          "receiveType":"account" // 此字段表示的是to字段存储的账户类型：account-钱包地址，contract-合约地址，
-          // 前端页面在点击接收方的地址时，根据此字段来决定是跳转到账户详情还是合约详情
-        }
-     ]
-
-     */
-    @PostMapping("transactionList")
-    public RespPage<TransactionListItem> transactionList (@Valid @RequestBody BlockTransactionPageReq req) {
-        if(!chainsConfig.isValid(req.getCid())){
-            throw new ResponseException(i18n.i(I18nEnum.CHAIN_ID_ERROR,req.getCid()));
-        }
-        RespPage<TransactionListItem> page = blockService.getBlockTransactionList(req);
-        return page;
-    }
-
-
-    /**
      * @api {post} block/blockDetails b.区块详情
      * @apiVersion 1.0.0
      * @apiName blockDetails
@@ -328,5 +239,93 @@ public class BlockController  {
         return blockService.getBlockTicketList(req);
     }
 
+
+    /**
+     @api {post} block/transactionList a.区块交易列表
+     @apiVersion 1.0.0
+     @apiName transactionList
+     @apiGroup block
+     @apiDescription 区块交易列表
+     @apiUse CommonHeaderFiled
+     @apiParamExample {json} Request-Example:
+     {
+     "cid":"", // 链ID (必填)
+     "pageNo": 1,//页数(必填)
+     "pageSize": 10,//页大小(必填)
+     "blockNumber":500, // 区块号(必填)
+     "txType":"", // 交易类型 (可选), 可以设置多个类型，使用逗号“,”分隔
+         transfer ：转账
+         MPCtransaction ： MPC交易
+         contractCreate ： 合约创建
+         voteTicket ： 投票
+         transactionExecute ： 合约执行
+         authorization ： 权限
+         candidateDeposit ： 竞选质押
+         candidateApplyWithdraw ： 减持质押
+         candidateWithdraw ： 提取质押
+         unknown ： 未知
+     }
+
+     @apiSuccessExample {json} Success-Response:
+     HTTP/1.1 200 OK
+     {
+     "errMsg": "",//描述信息
+     "code": 0,//成功（0），失败则由相关失败码
+     "displayTotalCount":18,//显示总数
+     "totalCount":18,// 小于等于500000记录的总数
+     "totalPages":1,//总页数
+     "data": [
+     {
+     "txHash": "0x234234",//交易hash
+     "blockHeight": "15566",//交易所在区块高度
+     "blockTime": 18080899999,//出块时间
+     "from": "0x667766",//发送方, 必定是钱包地址
+     "to": "0x667766",//接收方, 此字段存储的可能是钱包地址，也可能是合约地址，需要使用receiveType来进一步区分：
+     // 如果receiveType的值为account，则是钱包地址；如果receiveType的值为contract，则是合约地址
+     "value": "222",//数额(单位:Energon)
+     "actualTxCost": "22",//交易费用(单位:Energon)
+     "txReceiptStatus": 1,//交易状态 -1 pending 1 成功  0 失败
+     "txType": "", // 交易类型
+         transfer ：转账
+         MPCtransaction ： MPC交易
+         contractCreate ： 合约创建
+         voteTicket ： 投票
+         transactionExecute ： 合约执行
+         authorization ： 权限
+         candidateDeposit ： 竞选质押
+         candidateApplyWithdraw ： 减持质押
+         candidateWithdraw ： 提取质押
+         unknown ： 未知
+     "txInfo": "{
+         "functionName":"",//方法名称
+         "parameters":{},//参数
+         "type":"1"//交易类型
+             0：转账
+             1：合约发布
+             2：合约调用
+             4：权限
+             5：MPC交易
+             1000：投票
+             1001：竞选质押
+             1002：减持质押
+             1003：提取质押
+     }"//返回交易解析结构
+
+     "serverTime": 1123123,//服务器时间
+     "failReason":"",//失败原因
+     "receiveType":"account" // 此字段表示的是to字段存储的账户类型：account-钱包地址，contract-合约地址，
+     // 前端页面在点击接收方的地址时，根据此字段来决定是跳转到账户详情还是合约详情
+     }
+     ]
+
+     */
+    @PostMapping("transactionList")
+    public RespPage<TransactionListItem> transactionList (@Valid @RequestBody BlockTransactionPageReq req) {
+        if(!chainsConfig.isValid(req.getCid())){
+            throw new ResponseException(i18n.i(I18nEnum.CHAIN_ID_ERROR,req.getCid()));
+        }
+        RespPage<TransactionListItem> page = blockService.getBlockTransactionList(req);
+        return page;
+    }
 
 }
