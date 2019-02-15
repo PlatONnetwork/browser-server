17c6638b3b3d93b8921facb47b1b1cdb0648ebf5
chendongming <chendongming@juzix.net>
2019-02-15 15:40:31 +0800

    添加查询区块投票和竞选交易列表接口

--- server/src/main/java/com/platon/browser/controller/BlockController.java
+++ server/src/main/java/com/platon/browser/controller/BlockController.java
@@ -197,84 +197,83 @@ public class BlockController  {
     }
 
     /**
-     @api {post} block/transactionList d.区块交易列表
-     @apiVersion 1.0.0
-     @apiName transactionList
-     @apiGroup block
-     @apiDescription 区块交易列表
-     @apiUse CommonHeaderFiled
-     @apiParamExample {json} Request-Example:
-     {
-     "cid":"", // 链ID (必填)
-     "pageNo": 1,//页数(必填)
-     "pageSize": 10,//页大小(必填)
-     "blockNumber":500, // 区块号(必填)
-     "txType":"", // 交易类型 (可选), 可以设置多个类型，使用逗号“,”分隔
-     transfer ：转账
-     MPCtransaction ： MPC交易
-     contractCreate ： 合约创建
-     voteTicket ： 投票
-     transactionExecute ： 合约执行
-     authorization ： 权限
-     candidateDeposit ： 竞选质押
-     candidateApplyWithdraw ： 减持质押
-     candidateWithdraw ： 提取质押
-     unknown ： 未知
-     }
-
-     @apiSuccessExample {json} Success-Response:
-     HTTP/1.1 200 OK
-     {
-     "errMsg": "",//描述信息
-     "code": 0,//成功（0），失败则由相关失败码
-     "displayTotalCount":18,//显示总数
-     "totalCount":18,// 小于等于500000记录的总数
-     "totalPages":1,//总页数
-     "data": [
-     {
-     "txHash": "0x234234",//交易hash
-     "blockHeight": "15566",//交易所在区块高度
-     "blockTime": 18080899999,//出块时间
-     "from": "0x667766",//发送方, 必定是钱包地址
-     "to": "0x667766",//接收方, 此字段存储的可能是钱包地址，也可能是合约地址，需要使用receiveType来进一步区分：
-     // 如果receiveType的值为account，则是钱包地址；如果receiveType的值为contract，则是合约地址
-     "value": "222",//数额(单位:Energon)
-     "actualTxCost": "22",//交易费用(单位:Energon)
-     "txReceiptStatus": 1,//交易状态 -1 pending 1 成功  0 失败
-     "txType": "", // 交易类型
-     transfer ：转账
-     MPCtransaction ： MPC交易
-     contractCreate ： 合约创建
-     voteTicket ： 投票
-     transactionExecute ： 合约执行
-     authorization ： 权限
-     candidateDeposit ： 竞选质押
-     candidateApplyWithdraw ： 减持质押
-     candidateWithdraw ： 提取质押
-     unknown ： 未知
-     "txInfo": "{
-     "functionName":"",//方法名称
-     "parameters":{},//参数
-     "type":"1"//交易类型
-     0：转账
-     1：合约发布
-     2：合约调用
-     4：权限
-     5：MPC交易
-     1000：投票
-     1001：竞选质押
-     1002：减持质押
-     1003：提取质押
-     }"//返回交易解析结构
-
-     "serverTime": 1123123,//服务器时间
-     "failReason":"",//失败原因
-     "receiveType":"account" // 此字段表示的是to字段存储的账户类型：account-钱包地址，contract-合约地址，
-     // 前端页面在点击接收方的地址时，根据此字段来决定是跳转到账户详情还是合约详情
-     }
-     ]
-
-     */
+     * @api {post} block/transactionList d.区块交易列表
+     * @apiVersion 1.0.0
+     * @apiName transactionList
+     * @apiGroup block
+     * @apiDescription 区块交易列表
+     * @apiUse CommonHeaderFiled
+     * @apiParamExample {json} Request-Example:
+     * {
+     * "cid":"", // 链ID (必填)
+     * "pageNo": 1,//页数(必填)
+     * "pageSize": 10,//页大小(必填)
+     * "blockNumber":500, // 区块号(必填)
+     * "txType":"", // 交易类型 (可选), 可以设置多个类型，使用逗号“,”分隔
+     *     transfer ：转账
+     *     MPCtransaction ： MPC交易
+     *     contractCreate ： 合约创建
+     *     voteTicket ： 投票
+     *     transactionExecute ： 合约执行
+     *     authorization ： 权限
+     *     candidateDeposit ： 竞选质押
+     *     candidateApplyWithdraw ： 减持质押
+     *     candidateWithdraw ： 提取质押
+     *     unknown ： 未知
+     * }
+*
+     * @apiSuccessExample {json} Success-Response:
+     * HTTP/1.1 200 OK
+     * {
+     * "errMsg": "",//描述信息
+     * "code": 0,//成功（0），失败则由相关失败码
+     * "displayTotalCount":18,//显示总数
+     * "totalCount":18,// 小于等于500000记录的总数
+     * "totalPages":1,//总页数
+     * "data": [
+     *     {
+     *     "txHash": "0x234234",//交易hash
+     *     "blockHeight": "15566",//交易所在区块高度
+     *     "blockTime": 18080899999,//出块时间
+     *     "from": "0x667766",//发送方, 必定是钱包地址
+     *     "to": "0x667766",//接收方, 此字段存储的可能是钱包地址，也可能是合约地址，需要使用receiveType来进一步区分：
+     *     // 如果receiveType的值为account，则是钱包地址；如果receiveType的值为contract，则是合约地址
+     *     "value": "222",//数额(单位:Energon)
+     *     "actualTxCost": "22",//交易费用(单位:Energon)
+     *     "txReceiptStatus": 1,//交易状态 -1 pending 1 成功  0 失败
+     *     "txType": "", // 交易类型
+     *         transfer ：转账
+     *         MPCtransaction ： MPC交易
+     *         contractCreate ： 合约创建
+     *         voteTicket ： 投票
+     *         transactionExecute ： 合约执行
+     *         authorization ： 权限
+     *         candidateDeposit ： 竞选质押
+     *         candidateApplyWithdraw ： 减持质押
+     *         candidateWithdraw ： 提取质押
+     *         unknown ： 未知
+     *     "txInfo": "{
+     *         "functionName":"",//方法名称
+     *         "parameters":{},//参数
+     *         "type":"1"//交易类型
+     *             0：转账
+     *             1：合约发布
+     *             2：合约调用
+     *             4：权限
+     *             5：MPC交易
+     *             1000：投票
+     *             1001：竞选质押
+     *             1002：减持质押
+     *             1003：提取质押
+     *     }"//返回交易解析结构
+     *
+     *     "serverTime": 1123123,//服务器时间
+     *     "failReason":"",//失败原因
+     *     "receiveType":"account" // 此字段表示的是to字段存储的账户类型：account-钱包地址，contract-合约地址，
+     *     // 前端页面在点击接收方的地址时，根据此字段来决定是跳转到账户详情还是合约详情
+     *   }
+     * ]
+     **/
     @PostMapping("transactionList")
     public RespPage<TransactionListItem> transactionList (@Valid @RequestBody BlockTransactionPageReq req) {
         if(!chainsConfig.isValid(req.getCid())){
@@ -285,41 +284,41 @@ public class BlockController  {
     }
 
     /**
-     @api {post} block/ticketList e.区块选票列表
-     @apiVersion 1.0.0
-     @apiName ticketList
-     @apiGroup block
-     @apiDescription 区块选票列表
-     @apiParam {String} cid 链ID.
-     @apiUse CommonHeaderFiled
-     @apiParamExample {json} Request-Example:
-     {
-     "cid":"", // 链ID (必填)
-     "blockNumber":330,(必填)
-     }
-     @apiSuccessExample {json} Success-Response:
-     HTTP/1.1 200 OK
-     {
-     "errMsg": "",//描述信息
-     "code": 0,//成功（0），失败则由相关失败码
-     "displayTotalCount":18,//显示总数
-     "totalCount":18,// 记录的总数
-     "totalPages":1,//总页数
-     "data": [
-     {
-     "ticketId": "0x234234",//选票ID
-     "txHash": "15566",//交易HASH
-     "candidateId": 18080899999,//候选人Id（节点Id）
-     "owner": "0x667766",//票的所属者
-     "blockNumber": "0x667766",//购票时的块高
-     "rblockNumber": "222",//票被释放时的块高
-     "state": "22",//选票状态（1->正常，2->被选中，3->过期，4->掉榜）
-     "income": 1,//收益
-     "estimateExpireTime": "", //预计过期时间
-     "actualExpireTime": "" //实际过期时间
-     }
-     ]
-     }
+     * @api {post} block/ticketList e.区块选票列表
+     * @apiVersion 1.0.0
+     * @apiName ticketList
+     * @apiGroup block
+     * @apiDescription 区块选票列表
+     * @apiParam {String} cid 链ID.
+     * @apiUse CommonHeaderFiled
+     * @apiParamExample {json} Request-Example:
+     * {
+     * "cid":"", // 链ID (必填)
+     * "blockNumber":330,(必填)
+     * }
+     * @apiSuccessExample {json} Success-Response:
+     * HTTP/1.1 200 OK
+     * {
+     * "errMsg": "",//描述信息
+     * "code": 0,//成功（0），失败则由相关失败码
+     * "displayTotalCount":18,//显示总数
+     * "totalCount":18,// 记录的总数
+     * "totalPages":1,//总页数
+     * "data": [
+     * {
+     * "ticketId": "0x234234",//选票ID
+     * "txHash": "15566",//交易HASH
+     * "candidateId": 18080899999,//候选人Id（节点Id）
+     * "owner": "0x667766",//票的所属者
+     * "blockNumber": "0x667766",//购票时的块高
+     * "rblockNumber": "222",//票被释放时的块高
+     * "state": "22",//选票状态（1->正常，2->被选中，3->过期，4->掉榜）
+     * "income": 1,//收益
+     * "estimateExpireTime": "", //预计过期时间
+     * "actualExpireTime": "" //实际过期时间
+     * }
+     * ]
+     * }
      */
     @PostMapping("ticketList")
     public RespPage<Ticket> ticketList (@Valid @RequestBody BlockTicketPageReq req) {
