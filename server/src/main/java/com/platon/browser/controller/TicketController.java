282b98c816e992478f98a9d653e80fa002e78130
chendongming <chendongming@juzix.net>
2019-02-15 16:46:01 +0800

    添加查询区块投票和竞选交易列表接口

--- server/src/main/java/com/platon/browser/controller/TicketController.java
+++ server/src/main/java/com/platon/browser/controller/TicketController.java
@@ -29,41 +29,45 @@ public class TicketController {
     private TicketService ticketService;
 
     /**
-      @api {post} ticket/list a.选票列表
-      @apiVersion 1.0.0
-      @apiName list
-      @apiGroup ticket
-      @apiDescription 选票列表
-      @apiParam {String} cid 链ID.
-      @apiUse CommonHeaderFiled
-      @apiParamExample {json} Request-Example:
-      {
-           "cid":"", // 链ID (必填)
-           "txHash":"" // 投票交易的hash (必填)
-      }
-      @apiSuccessExample {json} Success-Response:
-      HTTP/1.1 200 OK
-      {
-           "errMsg": "",//描述信息
-           "code": 0,//成功（0），失败则由相关失败码
-           "displayTotalCount":18,//显示总数
-           "totalCount":18,// 记录的总数
-           "totalPages":1,//总页数
-           "data": [
-                {
-                "ticketId": "0x234234",//选票ID
-                "txHash": "15566",//交易HASH
-                "candidateId": 18080899999,//候选人Id（节点Id）
-                "owner": "0x667766",//票的所属者
-                "blockNumber": "0x667766",//购票时的块高
-                "rblockNumber": "222",//票被释放时的块高
-                "state": "22",//选票状态（1->正常，2->被选中，3->过期，4->掉榜）
-                "income": 1,//收益
-                "estimateExpireTime": "", //预计过期时间
-                "actualExpireTime": "" //实际过期时间
-                }
-            ]
-      }
+     * @api {post} ticket/list a.选票列表
+     * @apiVersion 1.0.0
+     * @apiName list
+     * @apiGroup ticket
+     * @apiDescription 选票列表
+     * @apiParam {String} cid 链ID.
+     * @apiUse CommonHeaderFiled
+     * @apiParamExample {json} Request-Example:
+     * {
+     *      "cid":"", // 链ID (必填)
+     *      "txHash":"" // 投票交易的hash (必填)
+     * }
+     * @apiSuccessExample {json} Success-Response:
+     * HTTP/1.1 200 OK
+     * {
+     *      "errMsg": "",//描述信息
+     *      "code": 0,//成功（0），失败则由相关失败码
+     *      "displayTotalCount":18,//显示总数
+     *      "totalCount":18,// 记录的总数
+     *      "totalPages":1,//总页数
+     *      "data": [
+     *           {
+     *           "ticketId": "0x234234",//选票ID
+     *           "txHash": "15566",//交易HASH
+     *           "candidateId": 18080899999,//候选人Id（节点Id）（投票给）
+     *           "owner": "0x667766",//票的所属者
+     *           "blockNumber": "0x667766",//购票时的块高
+     *           "rblockNumber": "222",//票被释放时的块高
+     *           "state": "22",//选票状态（1->正常，2->被选中，3->过期，4->掉榜）
+     *                 1->候选中
+     *                 2->中选票
+     *                 3->已失效
+     *                 4->待确认
+     *           "income": 1,//收益
+     *           "estimateExpireTime": "", //预计过期时间
+     *           "actualExpireTime": "" //实际过期时间 （只有status = 3显示该时间的值，其他情况显示预期时间的值）
+     *           }
+     *       ]
+     * }
      */
     @PostMapping("list")
     public RespPage<Ticket> list (@Valid @RequestBody TicketListReq req) {
