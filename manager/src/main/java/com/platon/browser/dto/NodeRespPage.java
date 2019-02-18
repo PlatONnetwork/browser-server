2b701a119765acee390ab4bec3c8f4f8c5bedcb7
chendongming <chendongming@juzix.net>
2019-02-18 16:33:52 +0800

    投票接口

--- manager/src/main/java/com/platon/browser/dto/NodeRespPage.java
+++ manager/src/main/java/com/platon/browser/dto/NodeRespPage.java
@@ -10,8 +10,8 @@ public class NodeRespPage<T> extends RespPage<T> {
     private BigDecimal proportion;//占比
     private BigDecimal blockReward;//每个区块奖励
     private BigDecimal ticketPrice;//票价
-    private long selectedCount; //已选中节点
-    private long totalCount=200;//总竞选节点
+    private long selectedNodeCount; //已选中节点
+    private long totalNodeCount=200;//总竞选节点
     private BigDecimal lowestDeposit;//最低质押
     private BigDecimal highestDeposit;//最高质押
 }
