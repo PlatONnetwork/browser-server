12a21ca85097158397101c38f298693fbb8626e8
chendongming <chendongming@juzix.net>
2019-02-19 17:12:03 +0800

    投票接口

--- server/src/main/java/com/platon/browser/controller/TransactionController.java
+++ server/src/main/java/com/platon/browser/controller/TransactionController.java
@@ -698,7 +698,7 @@ public class TransactionController {
             TransactionExample trEx = new TransactionExample();
             trEx.createCriteria().andChainIdEqualTo(req.getCid()).andFromEqualTo(req.getAddress()).andTxTypeEqualTo(TransactionTypeEnum.TRANSACTION_CONTRACT_CREATE.code);
             List<Transaction> trans = transactionMapper.selectByExample(trEx);
-            if(trans.size()==0){
+            if(trans.size()!=0){
                 Transaction transaction = trans.get(0);
                 returnData.setDeveloper(transaction.getFrom());
             }
