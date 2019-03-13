34a9d0bbb0b5e2b486f6b2a532609240f511a7a9
chendongming <chendongming@juzix.net>
2019-03-13 18:36:15 +0800

    jenkins配置

--- browser-service/src/main/java/com/platon/browser/service/TicketService.java
+++
@@ -1,7 +0,0 @@
-//package com.platon.browser.service;
-//
-//public interface TicketService {
-////    RespPage<Ticket> getList(TicketListReq req);
-////    Map<String,BigDecimal> getTicketIncome(String chainId, List<String> ticketIds);
-////    Map<String,BigDecimal> getTicketIncome(String chainId, String txHash);
-//}
