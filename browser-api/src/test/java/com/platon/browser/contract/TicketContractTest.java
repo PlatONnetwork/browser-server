d5b2642d30b7e51f803a0459b2d375d8f03e7c37
chendongming <chendongming@juzix.net>
2019-03-18 10:31:36 +0800

    去除投票相关单元测试

--- browser-api/src/test/java/com/platon/browser/contract/TicketContractTest.java
+++ browser-api/src/test/java/com/platon/browser/contract/TicketContractTest.java
@@ -3,16 +3,13 @@ package com.platon.browser.contract;
 import com.platon.browser.TestBase;
 import com.platon.browser.client.PlatonClient;
 import lombok.Data;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.web3j.platon.contracts.TicketContract;
 
 import java.math.BigInteger;
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 public class TicketContractTest extends TestBase {
@@ -33,28 +30,4 @@ public class TicketContractTest extends TestBase {
         String functionName,type;
         Parameter parameters;
     }
-
-    @Test
-    public void getTicketList(){
-        chainsConfig.getChainIds().forEach(chainId->{
-            String txHash = "0xdc5ff0d689fc327d5df4fe21a66f231bd4f851736e212549dc8ff0d6a21a1ae5";
-            TicketContract ticketContract = platon.getTicketContract(chainId);
-            List<String> ticketIds = ticketContract.VoteTicketIds(100,txHash);
-            if(ticketIds.size()==0) return;
-
-            StringBuilder sb = new StringBuilder();
-            String tail = ticketIds.get(ticketIds.size()-1);
-            ticketIds.forEach(id->{
-                sb.append(id);
-                if(!tail.equals(id)) sb.append(":");
-            });
-            try {
-                String details = ticketContract.GetBatchTicketDetail(sb.toString()).send();
-                System.out.println(details);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-    }
-
 }
