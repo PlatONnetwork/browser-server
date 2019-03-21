a73e98399e721454ea79d23250f5a19aab7d5fd2
chendongming <chendongming@juzix.net>
2019-03-21 09:37:17 +0800

    调整区块分析线程代码

--- browser-agent/src/main/java/com/platon/browser/thread/AnalyseThread.java
+++ browser-agent/src/main/java/com/platon/browser/thread/AnalyseThread.java
@@ -63,37 +63,31 @@ public class AnalyseThread {
 
     public void analyse(List<EthBlock> blocks){
         if(blocks.size()==0) return;
-
         threshold++;
 
         List<AnalyseParam> params = new ArrayList<>();
         blocks.forEach(block->params.add(new AnalyseParam(block,platon.getWeb3j(chainId))));
         AnalyseResult result = new AnalyseResult();
-//        Map<Long,List<NodeRanking>> nodeGroups = new HashMap<>();
         CountDownLatch latch = new CountDownLatch(params.size());
         params.forEach(param->
             THREAD_POOL.submit(()->{
                 long threadTime = System.currentTimeMillis();
                 try {
-                    try {
-                        Block block = blockFilter.analyse(param);
-                        List<TransactionBean> transactions = transactionFilter.analyse(param, block.getTimestamp().getTime());
-//                        List<NodeRanking> nodes = nodeFilter.analyse(param);
-                        // 一切正常，则把分析结果添加到结果中
-                        result.blocks.add(block);
-                        result.transactions.addAll(transactions);
-//                        nodeGroups.put(block.getNumber(),nodes);
-                    } catch (Exception e) {
-                        // 出错之后记录下出错的区块号，并返回
-                        BlockMissing err = new BlockMissing();
-                        err.setChainId(chainId);
-                        err.setNumber(param.ethBlock.getBlock().getNumber().longValue());
-                        result.errorBlocks.add(err);
-                    }
+                    // 分析区块和交易信息
+                    Block block = blockFilter.analyse(param);
+                    result.blocks.add(block);
+                    List<TransactionBean> transactions = transactionFilter.analyse(param, block.getTimestamp().getTime());
+                    result.transactions.addAll(transactions);
+                } catch (Exception e) {
+                    // 出错之后记录下出错的区块号，并返回
+                    BlockMissing err = new BlockMissing();
+                    err.setChainId(chainId);
+                    err.setNumber(param.ethBlock.getBlock().getNumber().longValue());
+                    result.errorBlocks.add(err);
                 }finally {
                     latch.countDown();
                 }
-                if((System.currentTimeMillis()-threadTime)>0) logger.debug("Thread ID={},NAME={} : -> {}",Thread.currentThread().getId(),Thread.currentThread().getName(),System.currentTimeMillis()-threadTime);
+                if((System.currentTimeMillis()-threadTime)>0) logger.debug("Analyse Thread ID={},NAME={} : -> {}ms",Thread.currentThread().getId(),Thread.currentThread().getName(),System.currentTimeMillis()-threadTime);
             })
         );
 
@@ -104,9 +98,6 @@ public class AnalyseThread {
         }
 
         long startTime = System.currentTimeMillis();
-
-        // 聚合所有区块的节点信息
-//        result.nodes=nodeFilter.aggregateNodes(nodeGroups);
 
         // 分析完成后在同一事务中批量入库分析结果
         try {
@@ -128,6 +119,15 @@ public class AnalyseThread {
             }
         }
 
+        detectMissingBlocks();
+
+        if((System.currentTimeMillis()-startTime)>0) logger.debug("databaseService.flush(result): -> {}ms",System.currentTimeMillis()-startTime);
+    }
+
+    /**
+     * 检测缺失块
+     */
+    private void detectMissingBlocks(){
         // 处理缺失块，每批量采集五次，检测一下block_missing表里是否有缺失的块
         if(threshold%5==0){
             // 重置阈值，防止无限增长
@@ -136,11 +136,11 @@ public class AnalyseThread {
             example.createCriteria().andChainIdEqualTo(chainId);
             example.setOrderByClause("number ASC");
             PageHelper.startPage(1,batchNum);
-            List<BlockMissing> missings = blockMissingMapper.selectByExample(example);
-            if(missings.size()==0) return;
+            List<BlockMissing> missingBlocks = blockMissingMapper.selectByExample(example);
+            if(missingBlocks.size()==0) return;
 
             List<EthBlock> concurrentBlocks = new ArrayList<>();
-            missings.forEach(missing->{
+            missingBlocks.forEach(missing->{
                 try {
                     EthBlock ethBlock = platon.getWeb3j(chainId).ethGetBlockByNumber(DefaultBlockParameter.valueOf(BigInteger.valueOf(missing.getNumber())),true).send();
                     concurrentBlocks.add(ethBlock);
@@ -160,8 +160,6 @@ public class AnalyseThread {
                 analyse(concurrentBlocks);
             }
         }
-
-        if((System.currentTimeMillis()-startTime)>0) logger.debug("databaseService.flush(result): -> {}",System.currentTimeMillis()-startTime);
     }
 
     public static class AnalyseParam {
