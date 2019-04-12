c3e2f27923b0cd03f2a0b270e26e072a78f582b2
chendongming <chendongming@juzix.net>
2019-04-12 16:52:16 +0800

    增加批量删除功能

--- browser-api/src/test/java/com/platon/browser/service/cache/TransactionCacheServiceTest.java
+++ browser-api/src/test/java/com/platon/browser/service/cache/TransactionCacheServiceTest.java
@@ -1,17 +1,21 @@
 package com.platon.browser.service.cache;
 
 import com.alibaba.fastjson.JSON;
+import com.github.pagehelper.PageHelper;
 import com.platon.browser.TestBase;
 import com.platon.browser.dao.entity.Transaction;
+import com.platon.browser.dao.entity.TransactionExample;
 import com.platon.browser.dao.entity.TransactionWithBLOBs;
 import com.platon.browser.dto.ticket.Ticket;
 import com.platon.browser.enums.TransactionTypeEnum;
+import com.platon.browser.util.RedisPipleTool;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.data.redis.connection.StringRedisConnection;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -33,14 +37,27 @@ public class TransactionCacheServiceTest extends TestBase {
     @Test
     public void insertData () throws FileNotFoundException {
 
-
         BufferedReader br = new BufferedReader(new FileReader("D:\\Workspace\\browser-server\\browser-api\\src\\test\\resources\\transactions.json"));
         StringBuilder sb = new StringBuilder();
         br.lines().forEach(line->sb.append(line));
         List<TransactionWithBLOBs> transactions = JSON.parseArray(sb.toString(), TransactionWithBLOBs.class);
+        transactionCacheService.classifyByAddress("500",transactions);
 
-        transactionCacheService.classifyByAddress("100",transactions);
-
+/*
+        int page = 1;
+        while (true){
+            try{
+                PageHelper.startPage(page,5000);
+                page++;
+                List<TransactionWithBLOBs> transactions = transactionMapper.selectByExampleWithBLOBs(new TransactionExample());
+                if(transactions.size()==0) break;
+                String chainId = transactions.get(0).getChainId();
+                transactionCacheService.classifyByAddress(chainId,transactions);
+            }catch (Exception e){
+                logger.error(e.getMessage());
+                logger.error("Page num: {}",page);
+            }
+        }*/
         logger.error("insert done");
     }
 
@@ -52,12 +69,21 @@ public class TransactionCacheServiceTest extends TestBase {
         // 查所有交易
         //transactions = transactionCacheService.fuzzyQuery("100",null,null,null,null);
         // 查投票交易
-        transactions = transactionCacheService.fuzzyQuery("100",null, TransactionTypeEnum.TRANSACTION_VOTE_TICKET.code,null,null);
+        transactions = transactionCacheService.fuzzyQuery("500",null, TransactionTypeEnum.TRANSACTION_VOTE_TICKET.code,null,null);
         // 查转账交易
-        transactions = transactionCacheService.fuzzyQuery("100",null, TransactionTypeEnum.TRANSACTION_TRANSFER.code,null,null);
+        transactions = transactionCacheService.fuzzyQuery("500",null, TransactionTypeEnum.TRANSACTION_TRANSFER.code,null,null);
         // 查创建合约交易
-        transactions = transactionCacheService.fuzzyQuery("100",null, TransactionTypeEnum.TRANSACTION_CONTRACT_CREATE.code,null,null);
+        transactions = transactionCacheService.fuzzyQuery("500",null, TransactionTypeEnum.TRANSACTION_CONTRACT_CREATE.code,null,null);
         logger.error("query done");
+    }
+
+
+    @Test
+    public void batchDelete () {
+        Set<String> keys = redisTemplate.keys("browser:0.6.1:online:chain1:address-trans-list*");
+
+        long count = RedisPipleTool.batchDeleteByKeys(new ArrayList<>(keys),false,redisTemplate);
+        logger.error("Deleted: {}",count);
     }
 }
 
