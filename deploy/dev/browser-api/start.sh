8ffcdb052f7c863e6dbd6c4519db1fa0186aa1da
chendongming <chendongming@juzix.net>
2019-05-14 13:11:11 +0800

    更改测试部署配置

--- deploy/dev/browser-api/start.sh
+++ deploy/dev/browser-api/start.sh
@@ -1,3 +1,3 @@
-nohup java -jar browser-api-0.6.0-SNAPSHOT.jar  --spring.profiles.active=testa28060 > /dev/null 2>&1 &
-nohup java -jar browser-api-0.6.0-SNAPSHOT.jar  --spring.profiles.active=testa28062 > /dev/null 2>&1 &
+nohup java -jar browser-api.jar  --spring.profiles.active=apidev8080 > /dev/null 2>&1 &
+nohup java -jar browser-api.jar  --spring.profiles.active=apidev8081 > /dev/null 2>&1 &
 ps -elf|grep browser-api
