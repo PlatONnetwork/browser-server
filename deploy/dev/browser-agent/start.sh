acce3f47cd1555d20be74f7e3ea2bacb96f7faf5
chendongming <chendongming@juzix.net>
2019-05-14 13:56:10 +0800

    更改测试部署配置

--- deploy/dev/browser-agent/start.sh
+++
@@ -1,2 +0,0 @@
-nohup java -jar browser-agent.jar --spring.profiles.active=testa > /dev/null 2>&1 &
-nohup java -jar browser-agent.jar --spring.profiles.active=testb > /dev/null 2>&1 &
--- deploy/dev/browser-api/stop.sh
+++ deploy/dev/browser-api/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ps -ef|grep -v grep|grep 'browser-api-0.6.0-SNAPSHOT.jar --spring.profiles.active=testa28060'|awk '{print $2}'|xargs kill -9;
-ps -ef|grep -v grep|grep 'browser-api-0.6.0-SNAPSHOT.jar --spring.profiles.active=testa28062'|awk '{print $2}'|xargs kill -9;
+ps -ef|grep -v grep|grep '--spring.profiles.active=apidev8080'|awk '{print $2}'|xargs kill -9;
+ps -ef|grep -v grep|grep '--spring.profiles.active=apidev8081'|awk '{print $2}'|xargs kill -9;
 ps -elf|grep browser-api;
