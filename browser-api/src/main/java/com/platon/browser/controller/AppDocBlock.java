58d3609e05d1a5ca91e9d6c1a106218afac9817e
chendai <chendai@juzix.net>
2019-07-22 18:09:51 +0800

    update

--- browser-api/src/main/java/com/platon/browser/controller/AppDocBlock.java
+++ browser-api/src/main/java/com/platon/browser/controller/AppDocBlock.java
@@ -2,7 +2,6 @@ package com.platon.browser.controller;
 
 public class AppDocBlock {
 	
-	
     /**
      *
      * @api {post} block/blockList a.区块列表
@@ -11,38 +10,122 @@ public class AppDocBlock {
      * @apiGroup block
      * @apiDescription 区块列表
      * @apiParamExample {json} Request-Example:
-     *   {
-     *      "cid":"",                    //链ID (必填)
-     *      "pageNo":1,                  //页数(必填)
-     *      "pageSize":10,               //页大小(必填)
-     *   }
+     * {
+     *    "cid":"",                    //链ID (必填)
+     *    "pageNo":1,                  //页数(必填)
+     *    "pageSize":10,               //页大小(必填)
+     * }
      * @apiSuccessExample {json} Success-Response:
-     *   HTTP/1.1 200 OK
-     *   {
-     *      "errMsg":"",                 //描述信息
-     *      "code":0,                    //成功（0），失败则由相关失败码
-     *      "displayTotalCount":18,      //显示总数
-     *      "totalCount":18,             //小于等于500000记录的总数
-     *      "totalPages":1,              //总页数
-     *      "data":[
-     *         {
-     *            "height":17888,        //块高
-     *            "timestamp":12,        //出块时间
-     *            "serverTime":17,       //服务器时间
-     *            "transaction":10000,   //块内交易数
-     *            "size":188,            //块大小
-     *            "nodeId":"",           //出块节点地址
-     *            "nodeName":"",         //出块节点名称
-     *            "stakingHash":"",      //验证人id
-     *            "energonUsed":111,     //能量消耗
-     *            "energonLimit":24234,  //能量消耗限制  ?
-     *            "energonAverage":11,   //平均能量价值(单位:Energon)  ?
-     *         }
-     *      ]
-     *   }
+     * HTTP/1.1 200 OK
+     * {
+     *    "errMsg":"",                 //描述信息
+     *    "code":0,                    //成功（0），失败则由相关失败码
+     *    "displayTotalCount":18,      //显示总数
+     *    "totalCount":18,             //小于等于500000记录的总数
+     *    "totalPages":1,              //总页数
+     *    "data":[
+     *       {
+     *          "height":17888,        //块高
+     *          "timestamp":12,        //出块时间
+     *          "serverTime":17,       //服务器时间
+     *          "transaction":10000,   //块内交易数
+     *          "size":188,            //块大小
+     *          "miner":"",            //出块节点地址
+     *          "nodeName":"",         //出块节点名称
+     *          "stakingHash":"",      //验证人id
+     *          "energonUsed":111,     //区块能量消耗
+     *          "energonProvided":14   //交易所提供的能量
+     *       }
+     *    ]
+     * }
      */
 	
 	
+    /**
+     * @api {post} block/blockDetails b.区块详情
+     * @apiVersion 1.0.0
+     * @apiName blockDetails
+     * @apiGroup block
+     * @apiDescription 区块详情
+     * @apiParamExample {json} Request-Example:
+     * {
+     *    "cid":"",                    //链ID (必填)
+     *    "height":123                 //区块高度(必填)
+     * }
+     * @apiSuccessExample {json} Success-Response:
+     * HTTP/1.1 200 OK
+     * {
+     *    "errMsg":"",                 //描述信息
+     *    "code": 0,                   //成功（0），失败则由相关失败码
+     *    "data": {
+     *       "height":19988,           //块高
+     *       "timestamp":123123123879, //出块时间
+     *       "hash":"0x1238",          //区块hash
+     *       "parentHash":"0x234",     //父区块hash
+     *       "miner":"",               //出块节点地址
+     *       "nodeName":"",            //出块节点名称
+     *       "stakingHash":"",         //验证人id
+     *       "timeDiff":424234,        //当前块出块时间距离上一个块出块时间之差（毫秒）
+     *       "size":123,               //区块大小
+     *       "energonLimit":24234,     //能量消耗限制
+     *       "energonUsed":2342,       //能量消耗
+     *       "energonProvided":14      //交易所提供的能量
+     *       "blockReward":"123123",   //区块奖励(单位:Energon)
+     *       "extraData":"xxx",        //附加数据
+     *       "first":false,            //是否第一条记录
+     *       "last":true,              //是否最后一条记录
+     *       "transaction":1288,       //块内交易总数
+     *       "transferTransaction":11, //块内转账交易总数
+     *       "delegateTransaction":11, //块内委托交易总数
+     *       "stakingTransaction":11,  //块内验证人交易总数
+     *       "proposalTransaction":11  //块内治理交易总数
+     *    }
+     * }
+     */
+	
+	
+    /**
+     * @api {post} block/blockDetailNavigate c.区块详情前后跳转浏览
+     * @apiVersion 1.0.0
+     * @apiName blockDetailNavigate
+     * @apiGroup block
+     * @apiDescription 区块详情前后跳转浏览
+     * @apiParamExample {json} Request-Example:
+     * {
+     *    "cid":"",                    //链ID (必填)
+     *    "height":123                 //区块高度(必填)
+     *    "direction":"",              //方向：prev-上一个，next-下一个 (必填)
+     * }
+     * @apiSuccessExample {json} Success-Response:
+     * HTTP/1.1 200 OK
+     * {
+     *    "errMsg":"",                 //描述信息
+     *    "code": 0,                   //成功（0），失败则由相关失败码
+     *    "data": {
+     *       "height":19988,           //块高
+     *       "timestamp":123123123879, //出块时间
+     *       "hash":"0x1238",          //区块hash
+     *       "parentHash":"0x234",     //父区块hash
+     *       "miner":"",               //出块节点地址
+     *       "nodeName":"",            //出块节点名称
+     *       "stakingHash":"",         //验证人id
+     *       "timeDiff":424234,        //当前块出块时间距离上一个块出块时间之差（毫秒）
+     *       "size":123,               //区块大小
+     *       "energonLimit":24234,     //能量消耗限制
+     *       "energonUsed":2342,       //能量消耗
+     *       "energonProvided":14      //交易所提供的能量
+     *       "blockReward":"123123",   //区块奖励(单位:Energon)
+     *       "extraData":"xxx",        //附加数据
+     *       "first":false,            //是否第一条记录
+     *       "last":true,              //是否最后一条记录
+     *       "transaction":1288,       //块内交易总数
+     *       "baseTransaction":11,     //块内基础交易总数
+     *       "delegateTransaction":11, //块内委托交易总数
+     *       "stakingTransaction":11,  //块内验证人交易总数
+     *       "proposalTransaction":11  //块内治理交易总数
+     *    }
+     * }
+     */
 	
 	
 }
--- browser-api/src/main/java/com/platon/browser/controller/AppDocHome.java
+++ browser-api/src/main/java/com/platon/browser/controller/AppDocHome.java
@@ -8,256 +8,136 @@ public class AppDocHome {
      * @apiVersion 1.0.0
      * @apiName query
      * @apiGroup home
-     * @apiDescription 
-     *     根据区块高度、区块hash查询区块详情；
-     *     根据交易hash查询交易详情；
-     *     根据钱包地址查询地址详情；
-     *     根据合约地址查询合约详情；
-     *     根据验证人节点地址、验证人节点名称查询验证人详情；
-     * 
+     * @apiDescription 根据区块 高度、区块hash查询区块详情；根据交易hash查询交易详情；根据钱包地址查询地址详情；根据合约地址查询合约详情；根据验证人节点地址、验证人节点名称查询验证人详情；
      * @apiParamExample {json} Request-Example:
-     *   {
-     *      "cid":"",                    //链ID (必填)
-     *      "parameter":""               //用户输入
-     *   }
+     * {
+     *    "cid":"",                    //链ID (必填)
+     *    "parameter":""               //用户输入
+     * }
      * @apiSuccessExample {json} Success-Response:
-     *   HTTP/1.1 200 OK
-     *   {
-     *      "errMsg": "",                //描述信息
-     *      "code": 0,                   //成功（0），失败则由相关失败码
-     *      "data":{
-     *         "type":"",                //block：区块；  transaction：交易；node：验证人；contract：合约；account：地址
-     *         "struct":{
-     *            "height":17888,        //块高，type=block存在 
-     *            "txHash":"",           //交易hash，type=transaction存在 
-     *            "address":"",          //地址，type=contract 或者 type=account存在 
-     *            "nodeId":"",           //节点id，type=node存在 
-     *            "stakingHash":""       //节点质押标识，type=node存在 
-     *         }
-     *      }
-     *   }
+     * HTTP/1.1 200 OK
+     * {
+     *    "errMsg": "",                //描述信息
+     *    "code": 0,                   //成功（0），失败则由相关失败码
+     *    "data":{
+     *       "type":"",                //block：区块；  transaction：交易；node：验证人；contract：合约；account：地址
+     *       "struct":{
+     *          "height":17888,        //块高，type=block存在 
+     *          "txHash":"",           //交易hash，type=transaction存在 
+     *          "address":"",          //地址，type=contract 或者 type=account存在 
+     *          "nodeAddress":"",      //节点地址，type=node存在 
+     *          "stakingHash":""       //节点质押标识，type=node存在 
+     *       }
+     *    }
+     * }
      */
 	
 	
     /**
-     * @api {subscribe} /app/block/statistic/init?cid=:chainId b.出块趋势（websocket请求）全量数据
-     * @apiVersion 1.0.0
-     * @apiName block/statistic/init
-     * @apiGroup home
-     * @apiDescription 全量数据
-     * @apiParam {String} cid 链ID.
-     * @apiParamExample {json} Request-Example:
-     *   {}
-     * @apiSuccessExample  Success-Response:
-     *   HTTP/1.1 200 OK
-     *   {
-     *      "errMsg":"",                 //描述信息
-     *      "code":0,                    //成功（0），失败则由相关失败码
-     *      "data":{
-     *         "graphData":{
-     *           "x":long[],             //区块高度
-     *           "ya":double[],          //出块的时间
-     *           "yb":long[]             //交易数量数量
-     *         }
-     *      }
-     *   }
-     */
-	
-	
-    /**
-     * @api {subscribe} /topic/block/statistic/new?cid=:chainId c.出块趋势（websocket请求）增量数据
+     * @api {subscribe} /topic/block/statistic/new?cid=:chainId b.出块趋势（websocket请求）全量数据
      * @apiVersion 1.0.0
      * @apiName block/statistic/new
      * @apiGroup home
-     * @apiDescription 增量数据
+     * @apiDescription 全量数据
      * @apiParam {String} cid 链ID.
      * @apiSuccessExample  Success-Response:
-     *   HTTP/1.1 200 OK
-     *   {
-     *      "errMsg":"",                 //描述信息
-     *      "code":0,                    //成功（0），失败则由相关失败码
-     *      "data":{
-     *         "graphData":{
-     *           "x":long[],             //区块高度
-     *           "ya":double[],          //出块的时间
-     *           "yb":long[]             //交易数量数量
-     *         }
-     *      }
-     *   }
+     * HTTP/1.1 200 OK
+     * {
+     *    "errMsg":"",                 //描述信息
+     *    "code":0,                    //成功（0），失败则由相关失败码
+     *    "data":{
+     *       "graphData":{
+     *         "x":long[],             //区块高度
+     *         "ya":double[],          //出块的时间
+     *         "yb":long[]             //交易数量数量
+     *       }
+     *    }
+     * }
      */
 	
 	
     /**
-     * @api {subscribe} /app/chain/statistic/init?cid=:chainId d.区块链基础数据（websocket请求）全量数据
-     * @apiVersion 1.0.0
-     * @apiName app/chain/statistic/init
-     * @apiGroup home
-     * @apiDescription 全量数据
-     * @apiParam {String} cid 链ID.
-     * @apiParamExample {json} Request-Example:
-     *   {}
-     * @apiSuccessExample  Success-Response:
-     *   HTTP/1.1 200 OK
-     *   {
-     *      "errMsg":"",                 //描述信息
-     *      "code":0,                    //成功（0），失败则由相关失败码
-     *      "data":{
-     *         "currentHeight":111,      //当前区块高度
-     *         "nodeId":"",              //出块节点地址
-     *         "nodeName":"",            //出块节点名称
-     *         "stakingHash":"",         //验证人id
-     *         "currentTransaction":"",  //实时交易笔数
-     *         "currentTps":111,         //当前的TPS
-     *         "maxTps":111,             //最大交易TPS
-     *         "turnVolume":"",          //当前流通量
-     *         "issueVolume":"",         //当前发行量
-     *         "stakingTotalVolume":"",  //当前质押总数=有效的质押+委托
-     *         "addressAmount":"",       //地址数
-     *         "proposalVolume":"",      //总提案数
-     *         "doingProposalVolume":"", //进行中提案数
-     *      }
-     *   }
-     */	
-	
-	
-    /**
-     * @api {subscribe} /topic/chain/statistic/new?cid=:chainId e.区块链基础数据（websocket请求）增量数据
+     * @api {subscribe} /topic/chain/statistic/new?cid=:chainId c.区块链基础数据（websocket请求）全量数据
      * @apiVersion 1.0.0
      * @apiName topic/chain/statistic/new
      * @apiGroup home
-     * @apiDescription 增量数据
+     * @apiDescription 全量数据
      * @apiParam {String} cid 链ID.
      * @apiSuccessExample  Success-Response:
-     *   HTTP/1.1 200 OK
-     *   {
-     *      "errMsg":"",                 //描述信息
-     *      "code":0,                    //成功（0），失败则由相关失败码
-     *      "data":{
-     *         "currentHeight":111,      //当前区块高度
-     *         "nodeId":"",              //出块节点地址
-     *         "nodeName":"",            //出块节点名称
-     *         "stakingHash":"",         //验证人id
-     *         "currentTransaction":"",  //实时交易笔数
-     *         "currentTps":111,         //当前的TPS
-     *         "maxTps":111,             //最大交易TPS
-     *         "turnVolume":"",          //当前流通量
-     *         "issueVolume":"",         //当前发行量
-     *         "stakingTotalVolume":"",  //当前质押总数=有效的质押+委托
-     *         "addressAmount":"",       //地址数
-     *         "proposalVolume":"",      //总提案数
-     *         "doingProposalVolume":"", //进行中提案数
-     *      }
-     *   }
+     * HTTP/1.1 200 OK
+     * {
+     *    "errMsg":"",                 //描述信息
+     *    "code":0,                    //成功（0），失败则由相关失败码
+     *    "data":{
+     *       "currentHeight":111,      //当前区块高度
+     *       "nodeAddress":"",         //出块节点地址
+     *       "stakingName":"",         //验证人名称
+     *       "stakingHash":"",         //验证人id
+     *       "currentTransaction":"",  //实时交易笔数
+     *       "currentTps":111,         //当前的TPS
+     *       "maxTps":111,             //最大交易TPS
+     *       "turnVolume":"",          //当前流通量
+     *       "issueVolume":"",         //当前发行量
+     *       "stakingTotalVolume":"",  //当前质押总数=有效的质押+委托
+     *       "addressVolume":"",       //地址数
+     *       "proposalVolume":"",      //总提案数
+     *       "doingProposalVolume":"", //进行中提案数
+     *    }
+     * }
      */	
 	
 	
     /**
-     * @api {subscribe} /app/block/list/init?cid=:chainId f.区块列表（websocket请求）全量数据
-     * @apiVersion 1.0.0
-     * @apiName app/block/list/init
-     * @apiGroup home
-     * @apiDescription 全量数据
-     * @apiParam {String} cid 链ID.
-     * @apiParamExample {json} Request-Example:
-     *   {}
-     * @apiSuccessExample  Success-Response:
-     *   HTTP/1.1 200 OK
-     *   {
-     *      "errMsg":"",                 //描述信息
-     *      "code":0,                    //成功（0），失败则由相关失败码
-     *      "data":[
-     *         {
-     *            "height":33,           //区块高度
-     *            "timestamp":33333,     //出块时间
-     *            "serverTime":44444,    //服务器时间
-     *            "nodeId":"",           //出块节点地址
-     *            "nodeName":"",         //出块节点名称
-     *            "stakingHash":"",      //验证人id
-     *            "transaction":333,     //交易数
-     *         }
-     *      ]
-     *   }
-     */
-	
-	
-    /**
-     * @api {subscribe} /topic/block/list/new?cid=:chainId h.区块列表（websocket请求）增量数据
+     * @api {subscribe} /topic/block/list/new?cid=:chainId d.区块列表（websocket请求）全量数据
      * @apiVersion 1.0.0
      * @apiName topic/block/list/new
      * @apiGroup home
-     * @apiDescription 增量数据
-     * @apiParam {String} cid 链ID.
-     * @apiParamExample {json} Request-Example:
-     *   {}
-     * @apiSuccessExample  Success-Response:
-     *   HTTP/1.1 200 OK
-     *   {
-     *      "errMsg":"",                 //描述信息
-     *      "code":0,                    //成功（0），失败则由相关失败码
-     *      "data":[
-     *         {
-     *            "height":33,           //区块高度
-     *            "timestamp":33333,     //出块时间
-     *            "serverTime":44444,    //服务器时间
-     *            "nodeId":"",           //出块节点地址
-     *            "nodeName":"",         //出块节点名称
-     *            "stakingHash":"",      //验证人id
-     *            "transaction":333,     //交易数
-     *         }
-     *      ]
-     *   }
-     */
-	
-	
-    /**
-     * @api {subscribe} /app/staking/list/init?cid=:chainId i.验证人列表（websocket请求）全量数据
-     * @apiVersion 1.0.0
-     * @apiName app/staking/list/init
-     * @apiGroup home
      * @apiDescription 全量数据
      * @apiParam {String} cid 链ID.
      * @apiParamExample {json} Request-Example:
-     *   {}
+     * {}
      * @apiSuccessExample  Success-Response:
-     *   HTTP/1.1 200 OK
-     *   {
-     *      "errMsg":"",                 //描述信息
-     *      "code":0,                    //成功（0），失败则由相关失败码
-     *      "data":[
-     *         {
-     *            "nodeId":"",           //出块节点地址
-     *            "nodeName":"",         //出块节点名称
-     *            "stakingIcon":"",      //验证人图片
-     *            "ranking":333,         //节点排行
-     *            "expectedIncome":""    //预计年收化率（从验证人加入时刻开始计算）
-     *         }
-     *      ]
-     *   }
+     * HTTP/1.1 200 OK
+     * {
+     *    "errMsg":"",                 //描述信息
+     *    "code":0,                    //成功（0），失败则由相关失败码
+     *    "data":[
+     *       {
+     *          "height":33,           //区块高度
+     *          "timestamp":33333,     //出块时间
+     *          "serverTime":44444,    //服务器时间
+     *          "nodeAddress":"",      //出块节点地址
+     *          "stakingName":"",      //验证人名称
+     *          "stakingHash":"",      //验证人id
+     *          "transaction":333      //交易数
+     *       }
+     *    ]
+     * }
      */
 	
 	
     /**
-     * @api {subscribe} /topic/staking/list/new?cid=:chainId js.验证人列表（websocket请求）增量数据
+     * @api {subscribe} /topic/staking/list/new?cid=:chainId e.验证人列表（websocket请求）全量数据
      * @apiVersion 1.0.0
      * @apiName topic/staking/list/new
      * @apiGroup home
-     * @apiDescription 增量数据
+     * @apiDescription 全量数据
      * @apiParam {String} cid 链ID.
      * @apiSuccessExample  Success-Response:
-     *   HTTP/1.1 200 OK
-     *   {
-     *      "errMsg":"",                 //描述信息
-     *      "code":0,                    //成功（0），失败则由相关失败码
-     *      "data":[
-     *         {
-     *            "nodeId":"",           //出块节点地址
-     *            "nodeName":"",         //出块节点名称
-     *            "stakingIcon":"",      //验证人图片
-     *            "ranking":333,         //节点排行
-     *            "expectedIncome":""    //预计年收化率（从验证人加入时刻开始计算）
-     *         }
-     *      ]
-     *   }
+     * HTTP/1.1 200 OK
+     * {
+     *    "errMsg":"",                 //描述信息
+     *    "code":0,                    //成功（0），失败则由相关失败码
+     *    "data":[
+     *       {
+     *          "miner":"",            //出块节点地址
+     *          "nodeName":"",         //出块节点名称
+     *          "stakingIcon":"",      //验证人图片
+     *          "ranking":333,         //节点排行
+     *          "expectedIncome":""    //预计年收化率（从验证人加入时刻开始计算）
+     *       }
+     *    ]
+     * }
      */
 	
 }
