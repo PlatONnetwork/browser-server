2a5fc144c812bb68518c5292792e85c3493d566c
dongqile <dongqile@juzix.io>
2019-08-10 15:25:24 +0800

    update transactionInfo bean

--- browser-agent/src/test/java/com/platon/browser/TestBase.java
+++ browser-agent/src/test/java/com/platon/browser/TestBase.java
@@ -1,16 +1,12 @@
 package com.platon.browser;
 
 import com.platon.browser.client.PlatonClient;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.filters.BlockFilter;
-
-import javax.annotation.PostConstruct;
 
 @SpringBootTest(classes= BrowserAgentApplication.class, value = "spring.profiles.active=dev")
 public class TestBase {
