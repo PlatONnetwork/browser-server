e91a7ed7793776a64bb384e6725aaf4dfd7f9239
chendongming <chendongming@juzix.net>
2019-08-10 11:27:00 +0800

    配置修改

---
+++ browser-agent/src/main/java/com/platon/browser/task/BlockSyncTask.java
@@ -0,0 +1,148 @@
+package com.platon.browser.task;
+
+import com.alibaba.fastjson.JSON;
+import com.platon.browser.client.PlatonClient;
+import com.platon.browser.dao.entity.Block;
+import com.platon.browser.dao.entity.BlockMissing;
+import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.web3j.protocol.core.DefaultBlockParameter;
+import org.web3j.protocol.core.methods.response.EthBlock;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.*;
+
+/**
+ * @Auther: Chendongming
+ * @Date: 2019/8/10 09:14
+ * @Description:
+ */
+@Component
+public class BlockSyncTask {
+    private static Logger logger = LoggerFactory.getLogger(BlockSyncTask.class);
+
+    private static ExecutorService THREAD_POOL;
+
+    @Autowired
+    private PlatonClient client;
+
+    // 已采集入库的最高块
+    private long commitBlockNumber=0;
+
+    // 每一批次采集区块的数量
+    @Value("${platon.web3j.collect.batch-size}")
+    private int collectBatchSize;
+
+    /**
+     * 初始化已有业务数据
+     */
+    public void init(){
+        THREAD_POOL = Executors.newFixedThreadPool(collectBatchSize);
+        // 从数据库查询最高块号，赋值给commitBlockNumber
+    }
+
+    public void start() throws InterruptedException {
+
+        while (true){
+
+            // 构造连续的待采区块号列表
+            List<BigInteger> blockNumbers = new ArrayList<>();
+            for (long blockNumber=(commitBlockNumber+1);blockNumber<=(commitBlockNumber+collectBatchSize);blockNumber++){
+                blockNumbers.add(BigInteger.valueOf(blockNumber));
+            }
+
+            // 并行采集区块
+            List<EthBlock.Block> blocks = getBlockAndTransaction(blockNumbers);
+
+
+
+            commitBlockNumber=commitBlockNumber+collectBatchSize;
+            TimeUnit.MILLISECONDS.sleep(500);
+        }
+
+    }
+
+    /**
+     * 并行采集区块
+     * @param blockNumbers 批量采集的区块号
+     * @return
+     */
+    private List<EthBlock.Block> getBlockAndTransaction(List<BigInteger> blockNumbers){
+        @Data
+        class Error{
+            public Error(BigInteger blockNumber,String msg){
+                this.blockNumber=blockNumber;
+                this.msg=msg;
+            }
+            BigInteger blockNumber;
+            String msg;
+        }
+        @Data
+        class Result{
+            List<EthBlock.Block> blocks = new CopyOnWriteArrayList<>();
+            List<Error> errors = new CopyOnWriteArrayList<>();
+        }
+        Result result = new Result();
+
+        CountDownLatch latch = new CountDownLatch(blockNumbers.size());
+        blockNumbers.forEach(blockNumber->
+            THREAD_POOL.submit(()->{
+                try {
+                    EthBlock.Block block = client.getWeb3j().ethGetBlockByNumber(DefaultBlockParameter.valueOf(blockNumber),true).send().getBlock();
+                    result.blocks.add(block);
+                } catch (Exception e) {
+                    Error error = new Error(blockNumber,e.getMessage());
+                    result.errors.add(error);
+                    logger.error("采集区块[{}]出错!{}",blockNumber,e.getMessage());
+                }finally {
+                    latch.countDown();
+                }
+            })
+        );
+
+        Error error = new Error(BigInteger.ONE,"88759358te545");
+
+        result.errors.add(error);
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if(result.errors.size()>0){
+            // 采集出错，终止程序
+            throw new RuntimeException("区块采集出错："+JSON.toJSONString(result.errors));
+        }
+
+        // 按区块号从大到小排序
+        Collections.sort(result.blocks,(c1,c2)->{
+            if (c1.getNumber().compareTo(c2.getNumber())>0) return 1;
+            if (c1.getNumber().compareTo(c2.getNumber())<0) return -1;
+            return 0;
+        });
+
+        return result.blocks;
+
+    }
+
+    /**
+     * 并行分析区块
+     */
+    private void analyzeBlockAndTransaction(List<EthBlock.Block> blocks){
+
+    }
+
+    private void batchSaveResult(){
+
+    }
+}
--- browser-service/src/main/java/com/platon/browser/client/PlatonClient.java
+++ browser-service/src/main/java/com/platon/browser/client/PlatonClient.java
@@ -1,17 +1,17 @@
 package com.platon.browser.client;
 
-import com.platon.browser.config.ChainsConfig;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.web3j.platon.contracts.CandidateContract;
-import org.web3j.platon.contracts.TicketContract;
 import org.web3j.protocol.Web3j;
-import org.web3j.tx.ReadonlyTransactionManager;
-import org.web3j.tx.gas.DefaultWasmGasProvider;
+import org.web3j.protocol.core.methods.response.EthBlockNumber;
+import org.web3j.protocol.http.HttpService;
 
 import javax.annotation.PostConstruct;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * User: dongqile
@@ -20,46 +20,60 @@ import java.util.Map;
  */
 @Component
 public class PlatonClient {
-    @Autowired
-    private ChainsConfig chainsConfig;
+    private static final ReentrantReadWriteLock WEB3J_CONFIG_LOCK = new ReentrantReadWriteLock();
 
-    public static class ContractHolder {
-        public ContractHolder(Web3j web3j, TicketContract tc, CandidateContract cc){
-            this.web3j=web3j;
-            this.ticketContract=tc;
-            this.candidateContract=cc;
-        }
-        Web3j web3j;
-        TicketContract ticketContract;
-        CandidateContract candidateContract;
-    }
+    private List<Web3j> allWeb3jList=new ArrayList<>();
+    private List<Web3j> validWeb3jList=new ArrayList <>();
 
-    private Map<String,ContractHolder> map = new HashMap<>();
+    @Value("${platon.web3j.addresses}")
+    private List<String> web3jAddresses;
 
     @PostConstruct
-    private void init () {
-        chainsConfig.getChainIds().forEach(chainId->{
-            Web3j web3j = chainsConfig.getWeb3j(chainId);
-            TicketContract tc = TicketContract.load(web3j,new ReadonlyTransactionManager(web3j, TicketContract.CONTRACT_ADDRESS),new DefaultWasmGasProvider());
-            CandidateContract cc = CandidateContract.load(web3j,new ReadonlyTransactionManager(web3j, CandidateContract.CONTRACT_ADDRESS), new DefaultWasmGasProvider());
-            ContractHolder ch = new ContractHolder(web3j,tc,cc);
-            map.put(chainId,ch);
-        });
+    public void init(){
+        // 初始化所有web3j实例
+        try {
+            WEB3J_CONFIG_LOCK.writeLock().lock();
+            web3jAddresses.forEach(address->{
+                Web3j web3j = Web3j.build(new HttpService(address));
+                allWeb3jList.add(web3j);
+            });
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            WEB3J_CONFIG_LOCK.writeLock().unlock();
+        }
+        // 更新有效web3j实例列表
+        updateValidWeb3jList();
     }
 
-    public CandidateContract getCandidateContract(String chainId) {
-        ContractHolder ch = map.get(chainId);
-        if(ch==null) return null;
-        return ch.candidateContract;
+    public Web3j getWeb3j(){
+        try{
+            WEB3J_CONFIG_LOCK.readLock().lock();
+            Random random = new Random();
+            int index = random.nextInt(validWeb3jList.size());
+            return validWeb3jList.get(index);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            WEB3J_CONFIG_LOCK.readLock().unlock();
+        }
+        return null;
     }
-    public TicketContract getTicketContract(String chainId) {
-        ContractHolder ch = map.get(chainId);
-        if(ch==null) return null;
-        return ch.ticketContract;
-    }
-    public Web3j getWeb3j (String chainId) {
-        ContractHolder ch = map.get(chainId);
-        if(ch==null) return null;
-        return ch.web3j;
+
+    public void updateValidWeb3jList(){
+        try {
+            WEB3J_CONFIG_LOCK.writeLock().lock();
+            validWeb3jList.clear();
+            allWeb3jList.forEach((web3j)->{
+                try {
+                    EthBlockNumber ethBlockNumber = web3j.ethBlockNumber().send();
+                    validWeb3jList.add(web3j);
+                } catch (IOException e) {
+                    e.getMessage();
+                }
+            });
+        }finally {
+            WEB3J_CONFIG_LOCK.writeLock().unlock();
+        }
     }
 }
