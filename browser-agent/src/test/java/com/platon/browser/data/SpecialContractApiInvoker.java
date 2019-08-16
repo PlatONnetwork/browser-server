c40a18651d0b7508ec60b3d3019fb48a959e394a
chendongming <chendongming@juzix.net>
2019-08-16 10:01:04 +0800

    补充造交易工具

--- browser-agent/src/test/java/com/platon/browser/data/SpecialContractApiInvoker.java
+++ browser-agent/src/test/java/com/platon/browser/data/SpecialContractApiInvoker.java
@@ -21,6 +21,8 @@ import org.web3j.protocol.core.RemoteCall;
 import org.web3j.protocol.core.methods.request.Transaction;
 import org.web3j.protocol.core.methods.response.PlatonCall;
 import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.ReadonlyTransactionManager;
+import org.web3j.tx.gas.DefaultWasmGasProvider;
 import org.web3j.utils.JSONUtil;
 import org.web3j.utils.Numeric;
 import org.web3j.utils.PlatOnUtil;
@@ -43,10 +45,27 @@ public class SpecialContractApiInvoker {
     // 特殊合约接口测试
     @Test
     public void getCandidates() throws Exception {
-        BaseResponse<List<Node>> verifierList = getHistoryVerifierList(BigInteger.ONE);
+        NodeContract nodeContract = NodeContract.load(currentValidWeb3j,new ReadonlyTransactionManager(currentValidWeb3j, NodeContract.NODE_CONTRACT_ADDRESS),new DefaultWasmGasProvider());
+
+        // 当前区块号
+        BigInteger blockNumber = currentValidWeb3j.platonBlockNumber().send().getBlockNumber();
+        logger.error("{}",blockNumber);
+        // 查询区块号为1时的结算周期历史验证人列表
+        BaseResponse<List<Node>> historyVerifierList = getHistoryVerifierList(BigInteger.ONE);
+        logger.error("{}",historyVerifierList);
+        // 查询实时结算验证人列表
+        BaseResponse<List<Node>> verifierList = nodeContract.getVerifierList().send();
         logger.error("{}",verifierList);
-        BaseResponse<List<Node>> validatorList = getHistoryValidatorList(BigInteger.ONE);
+        // 查询区块号为1时的共识周期历史验证人列表
+        BaseResponse<List<Node>> historyValidatorList = getHistoryValidatorList(BigInteger.ONE);
+        logger.error("{}",historyValidatorList);
+        // 查询实时共识验证人列表
+        BaseResponse<List<Node>> validatorList = nodeContract.getValidatorList().send();
         logger.error("{}",validatorList);
+
+        // 获取可用和锁仓余额
+        BaseResponse<List<RestrictingBalance>> balances = getRestrictingBalance("0x8b77ac9fabb6fe247ee91ca07ea4f62c6761e79b");
+        logger.error("{}",balances);
     }
 
 
