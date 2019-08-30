27fe631bfc7294a41263fc171e5b59d0bfa2d927
dongqile <dongqile@juzix.io>
2019-08-30 15:04:34 +0800

    task update

--- browser-agent/src/main/java/com/platon/browser/task/StakingInfoUpdateTask.java
+++ browser-agent/src/main/java/com/platon/browser/task/StakingInfoUpdateTask.java
@@ -1,17 +1,11 @@
 package com.platon.browser.task;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.fasterxml.jackson.annotation.JsonAlias;
-import com.platon.browser.dao.entity.Staking;
-import com.platon.browser.dto.CustomBlock;
 import com.platon.browser.dto.CustomStaking;
 import com.platon.browser.engine.BlockChain;
 import com.platon.browser.engine.bean.keystore.Completion;
 import com.platon.browser.engine.bean.keystore.Components;
-import com.platon.browser.engine.bean.keystore.KeystoreUser;
-import com.platon.browser.enums.InnerContractAddrEnum;
+import com.platon.browser.engine.bean.keystore.KeyBaseUser;
 import com.platon.browser.util.MarkDownParserUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -19,18 +13,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.web3j.protocol.core.DefaultBlockParameter;
-import org.web3j.utils.Convert;
 
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import static com.platon.browser.engine.BlockChain.NODE_CACHE;
-import static com.platon.browser.engine.BlockChain.STAGE_DATA;
 
 /**
  * User: dongqile
@@ -50,18 +38,18 @@ public class StakingInfoUpdateTask {
 
     @Scheduled(cron = "0/3  * * * * ?")
     protected void start () {
-        String keyStoreUrl = blockChain.getChainConfig().getKeyStore();
+        String keyStoreUrl = blockChain.getChainConfig().getKeyBase();
         try {
             Set <CustomStaking> customStakingSet = NODE_CACHE.getAllStaking();
-            if(customStakingSet.size() == 0)return;
+            if (customStakingSet.size() == 0) return;
             customStakingSet.forEach(customStaking -> {
-                if(StringUtils.isNotBlank(customStaking.getExternalId())){
+                if (StringUtils.isNotBlank(customStaking.getExternalId())) {
                     String queryUrl = keyStoreUrl.concat(fingerprintpPer.concat(customStaking.getExternalId()));
                     try {
                         String queryResult = MarkDownParserUtil.httpGet(queryUrl);
-                        KeystoreUser keystoreUser = JSON.parseObject(queryResult,KeystoreUser.class);
-                        List<Completion> completions = keystoreUser.getCompletions();
-                        if(completions==null||completions.size()==0) return;
+                        KeyBaseUser keyBaseUser = JSON.parseObject(queryResult, KeyBaseUser.class);
+                        List <Completion> completions = keyBaseUser.getCompletions();
+                        if (completions == null || completions.size() == 0) return;
                         // 取最新一条
                         Completion completion = completions.get(0);
                         // 取缩略图
@@ -73,30 +61,14 @@ public class StakingInfoUpdateTask {
                         customStaking.setExternalName(username);
 
                     } catch (IOException e) {
-                        logger.error("[StakingInfoUpdateTask] IOException {}",e.getMessage());
-                    } catch (Exception e){
-                        logger.error("[StakingInfoUpdateTask] Exception {}",e.getMessage());
+                        logger.error("[StakingInfoUpdateTask] IOException {}", e.getMessage());
+                    } catch (Exception e) {
+                        logger.error("[StakingInfoUpdateTask] Exception {}", e.getMessage());
                     }
                 }
             });
         } catch (Exception e) {
             logger.error("{}", e.getMessage());
         }
-    }
-
-    private String getUserName(String seachInfo)throws  Exception{
-        JSONObject jsonObject = JSONObject.parseObject(seachInfo);
-        JSONArray completionsArray = jsonObject.getJSONArray("completions");
-        JSONObject fristJson = completionsArray.getJSONObject(0);
-        JSONObject componentsObject = fristJson.getJSONObject("components");
-        JSONObject userNameJson = componentsObject.getJSONObject("username");
-        return userNameJson.getString("val");
-    }
-
-    private String getIcon(String seachInfo)throws Exception{
-        JSONObject jsonObject = JSONObject.parseObject(seachInfo);
-        JSONArray completionsArray = jsonObject.getJSONArray("completions");
-        JSONObject fristJson = completionsArray.getJSONObject(0);
-        return  fristJson.getString("thumbnail");
     }
 }
--- browser-service/src/main/java/com/platon/browser/config/BlockChainConfig.java
+++ browser-service/src/main/java/com/platon/browser/config/BlockChainConfig.java
@@ -85,6 +85,6 @@ public class BlockChainConfig {
     private String proposalUrlTemplate;
     //文本提案默认结束轮数
     private String proposalTextEndRound;
-    //keyStore地址
-    private String keyStore;
+    //keyBase地址
+    private String keyBase;
 }
