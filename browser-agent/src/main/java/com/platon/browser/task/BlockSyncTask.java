87545938836139c6b1e03e93cfc07f1b025eeb2d
chendongming <chendongming@juzix.net>
2019-09-03 12:45:01 +0800

    修改初始化节点获取逻辑

--- browser-agent/src/main/java/com/platon/browser/task/BlockSyncTask.java
+++ browser-agent/src/main/java/com/platon/browser/task/BlockSyncTask.java
@@ -25,7 +25,6 @@ import com.platon.browser.utils.HexTool;
 import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
