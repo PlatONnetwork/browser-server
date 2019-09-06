3efa28c0a7c61a4df329556ba0b15770b9ee0907
chendongming <chendongming@juzix.net>
2019-09-06 18:16:14 +0800

    区块采集功能使用循环替代递归，避免栈溢出

--- browser-agent/src/main/java/com/platon/browser/service/BlockService.java
+++ browser-agent/src/main/java/com/platon/browser/service/BlockService.java
@@ -13,8 +13,8 @@ import org.web3j.protocol.core.DefaultBlockParameter;
 import org.web3j.protocol.core.methods.response.PlatonBlock;
 
 import java.math.BigInteger;
-import java.util.Collections;
 import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.CountDownLatch;
 
 import static com.platon.browser.task.BlockSyncTask.THREAD_POOL;
@@ -22,7 +22,7 @@ import static com.platon.browser.task.BlockSyncTask.THREAD_POOL;
 /**
  * @Auther: Chendongming
  * @Date: 2019/9/6 13:34
- * @Description:
+ * @Description: 区块采集服务
  */
 @Service
 public class BlockService {
@@ -37,53 +37,49 @@ public class BlockService {
      * @return void
      */
     public void collect(Set<BigInteger> blockNumbers) throws BlockCollectingException {
-        // 清空重试区块号列表
+        // 先重置重试列表
         CollectResult.RETRY_NUMBERS.clear();
-        // 并行批量采集区块
-        CountDownLatch latch = new CountDownLatch(blockNumbers.size());
-        blockNumbers.forEach(blockNumber->
+        // 把待采块放入重试列表，当作重试块号操作
+        CollectResult.RETRY_NUMBERS.addAll(blockNumbers);
+        // 记录每次重试出异常的块号，方便放入下次重试
+        Set<BigInteger> exceptionNumbers = new CopyOnWriteArraySet<>();
+        while (CollectResult.RETRY_NUMBERS.size()>0){
+            exceptionNumbers.clear();
+            // 并行批量采集区块
+            CountDownLatch latch = new CountDownLatch(CollectResult.RETRY_NUMBERS.size());
+            CollectResult.RETRY_NUMBERS.forEach(blockNumber->
                 THREAD_POOL.submit(()->{
                     try {
                         Web3j web3j = client.getWeb3j();
                         PlatonBlock.Block initData = web3j.platonGetBlockByNumber(DefaultBlockParameter.valueOf(blockNumber),true).send().getBlock();
-                        if(initData!=null) {
-                            try{
-                                CustomBlock block = new CustomBlock();
-                                block.updateWithBlock(initData);
-                                CollectResult.CONCURRENT_BLOCK_MAP.put(blockNumber.longValue(),block);
-                            }catch (Exception ex){
-                                logger.debug("初始化区块信息异常, 原因: {}", ex.getMessage());
-                                throw ex;
-                            }
+                        if (initData==null) throw new RuntimeException("原生区块["+blockNumber+"]为空！");
+                        try{
+                            CustomBlock block = new CustomBlock();
+                            block.updateWithBlock(initData);
+                            CollectResult.CONCURRENT_BLOCK_MAP.put(blockNumber.longValue(),block);
+                        }catch (Exception ex){
+                            logger.debug("初始化区块信息异常, 原因: {}", ex.getMessage());
+                            throw ex;
                         }
                     } catch (Exception e) {
-                        // 把出现异常的区块号加入重试列表
-                        CollectResult.RETRY_NUMBERS.add(blockNumber);
+                        // 把出现异常的区块号加入异常块号列表
+                        exceptionNumbers.add(blockNumber);
                     }finally {
                         latch.countDown();
                     }
                 })
-        );
+            );
 
-        try {
-            latch.await();
-        } catch (InterruptedException e) {
-            throw new BlockCollectingException("区块采集线程被中断:"+e.getMessage());
-        }
-
-        if (CollectResult.CONCURRENT_BLOCK_MAP.size() > 0) {
-            // 查看已采块是否连续，把缺失的区块号放入重试列表
-            Set<Long> collected = CollectResult.CONCURRENT_BLOCK_MAP.keySet();
-            long start = Collections.min(collected), end = Collections.max(collected);
-            for (long i = start; i < end; i++) {
-                if (!collected.contains(i)) CollectResult.RETRY_NUMBERS.add(BigInteger.valueOf(i));
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                throw new BlockCollectingException("区块采集线程被中断:"+e.getMessage());
             }
-        }
 
-        if (CollectResult.RETRY_NUMBERS.size() > 0) {
-            logger.debug("区块重试列表：{}", CollectResult.RETRY_NUMBERS);
-            collect(CollectResult.RETRY_NUMBERS);
+            // 清空重试列表
+            CollectResult.RETRY_NUMBERS.clear();
+            // 把本轮异常区块号加入重试列表
+            CollectResult.RETRY_NUMBERS.addAll(exceptionNumbers);
         }
     }
-
 }
