4f3c788174d805001e37713b1f00176100f7ad16
dongqile <dongqile@juzix.io>
2019-09-06 16:37:37 +0800

    1.新增rpc接口，根据快高时时查询proposal结果

--- browser-service/src/main/java/com/platon/browser/client/AccuVerifiersCount.java
+++ browser-service/src/main/java/com/platon/browser/client/AccuVerifiersCount.java
@@ -9,8 +9,15 @@ import lombok.Data;
  */
 @Data
 public class AccuVerifiersCount {
-    private Integer accuVerifiers;
-    private Integer yeas;
-    private Integer nays;
-    private Integer abstentions;
+    private Integer accuVerifiers = 0 ;
+    private Integer yeas = 0 ;
+    private Integer nays = 0 ;
+    private Integer abstentions = 0 ;
+
+    public void init(String accuVerifiers,String yeas,String nays,String abstentions){
+        this.setAccuVerifiers(Integer.valueOf(accuVerifiers));
+        this.setYeas(Integer.valueOf(yeas));
+        this.setNays(Integer.valueOf(nays));
+        this.setAbstentions(Integer.valueOf(abstentions));
+    }
 }
