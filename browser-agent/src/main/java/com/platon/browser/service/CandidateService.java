03f3af9f730a54419f681d58ad0b67f70f9f914e
chendongming <chendongming@juzix.net>
2019-09-06 17:01:31 +0800

    调整采集代码

--- browser-agent/src/main/java/com/platon/browser/service/CandidateService.java
+++ browser-agent/src/main/java/com/platon/browser/service/CandidateService.java
@@ -3,20 +3,32 @@ package com.platon.browser.service;
 import com.platon.browser.client.PlatonClient;
 import com.platon.browser.client.SpecialContractApi;
 import com.platon.browser.config.BlockChainConfig;
+import com.platon.browser.dto.CustomNode;
+import com.platon.browser.dto.CustomStaking;
 import com.platon.browser.engine.BlockChain;
+import com.platon.browser.engine.stage.BlockChainStage;
 import com.platon.browser.exception.BlockNumberException;
+import com.platon.browser.exception.BusinessException;
+import com.platon.browser.exception.CacheConstructException;
 import com.platon.browser.exception.CandidateException;
 import com.platon.browser.utils.EpochUtil;
 import com.platon.browser.utils.HexTool;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.web3j.platon.BaseResponse;
 import org.web3j.platon.bean.Node;
+import org.web3j.utils.Convert;
 
+import java.io.IOException;
+import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
+import static com.platon.browser.engine.BlockChain.NODE_NAME_MAP;
 import static java.lang.String.format;
 
 /**
@@ -26,15 +38,22 @@ import static java.lang.String.format;
  */
 @Service
 public class CandidateService {
-
+    private static Logger logger = LoggerFactory.getLogger(CandidateService.class);
     @Autowired
     private BlockChain blockChain;
-
     @Autowired
     private BlockChainConfig chainConfig;
     @Autowired
     private PlatonClient client;
+    @Autowired
+    private DbService dbService;
 
+    /**
+     * 从指定区块号初始化BlockChain的共识周期验证人
+     * @param blockNumber
+     * @throws BlockNumberException
+     * @throws CandidateException
+     */
     public void initValidator(Long blockNumber) throws BlockNumberException, CandidateException {
         BaseResponse<List <Node>> result;
         // 取入参区块号的前一共识周期结束块号，因此可以通过它查询前一共识周期验证人历史列表
@@ -80,6 +99,12 @@ public class CandidateService {
         }
     }
 
+    /**
+     * 从指定区块号初始化BlockChain的结算周期验证人
+     * @param blockNumber
+     * @throws CandidateException
+     * @throws BlockNumberException
+     */
     public void initVerifier (Long blockNumber) throws CandidateException, BlockNumberException {
         BaseResponse<List<Node>> result;
         // ==================================更新前一周期验证人列表=======================================
@@ -122,6 +147,95 @@ public class CandidateService {
 
         if(blockChain.getCurVerifier().size()==0){
             throw new CandidateException("查询不到结算周期验证人(当前块号="+blockNumber+",当前结算轮数="+blockChain.getCurSettingEpoch()+")");
+        }
+    }
+
+    /**
+     * 从区块号0初始化BlockChain的共识周期验证人和结算周期验证人
+     * @throws Exception
+     */
+    public void init() throws Exception {
+        // 如果库里区块为空，则：
+        try {
+            // 根据区块号0查询共识周期验证人，以便对结算周期验证人设置共识标识
+            BaseResponse<List<Node>> result = SpecialContractApi.getHistoryValidatorList(client.getWeb3j(),BigInteger.ZERO);
+            if(!result.isStatusOk()){
+                logger.debug("查询实时共识周期验证人列表...");
+                result = client.getNodeContract().getValidatorList().send();
+                if(!result.isStatusOk()){
+                    throw new CandidateException("底层链查询实时共识周期验证节点列表出错:"+result.errMsg);
+                }
+            }
+            // 查询内置共识周期验证人初始化blockChain的curValidator属性
+            Set<String> validatorSet = new HashSet<>();
+            result.data.forEach(node->validatorSet.add(HexTool.prefix(node.getNodeId())));
+
+            // 查询所有候选人
+            Map<String,Node> candidateMap = new HashMap<>();
+            result = client.getNodeContract().getCandidateList().send();
+            if(!result.isStatusOk()){
+                throw new CandidateException("底层链查询候选验证节点列表出错:"+result.errMsg);
+            }
+            result.data.forEach(node->candidateMap.put(HexTool.prefix(node.getNodeId()),node));
+
+            // 配置中的默认内置节点信息
+            Map<String,CustomStaking> defaultStakingMap = new HashMap<>();
+            chainConfig.getDefaultStakings().forEach(staking -> defaultStakingMap.put(staking.getNodeId(),staking));
+
+            // 根据区块号0查询结算周期验证人列表并入库
+            result = SpecialContractApi.getHistoryVerifierList(client.getWeb3j(),BigInteger.ZERO);
+            if(!result.isStatusOk()){
+                logger.debug("查询实时结算周期验证人列表...");
+                result = client.getNodeContract().getVerifierList().send();
+                if(!result.isStatusOk()){
+                    throw new CandidateException("底层链查询实时结算周期验证节点列表出错:"+result.errMsg);
+                }
+            }
+
+            result.data.stream().filter(Objects::nonNull).forEach(verifier->{
+                Node candidate = candidateMap.get(HexTool.prefix(verifier.getNodeId()));
+                // 补充完整属性
+                if(candidate!=null) BeanUtils.copyProperties(candidate,verifier);
+
+                CustomNode node = new CustomNode();
+                node.updateWithNode(verifier);
+                node.setIsRecommend(CustomNode.YesNoEnum.YES.code);
+                node.setStatVerifierTime(BigInteger.ONE.intValue()); // 提前设置验证轮数
+                node.setStatExpectBlockQty(chainConfig.getExpectBlockCount().longValue());
+                BlockChain.STAGE_DATA.getStakingStage().insertNode(node);
+
+                CustomStaking staking = new CustomStaking();
+                staking.updateWithNode(verifier);
+                staking.setStatVerifierTime(BigInteger.ONE.intValue()); // 提前设置验证轮数
+                staking.setIsInit(CustomStaking.YesNoEnum.YES.code);
+                staking.setIsSetting(CustomStaking.YesNoEnum.YES.code);
+                // 内置节点默认设置状态为1
+                staking.setStatus(CustomStaking.StatusEnum.CANDIDATE.code);
+                // 设置内置节点质押锁定金额
+                BigDecimal initStakingLocked = Convert.toVon(chainConfig.getDefaultStakingLockedAmount(), Convert.Unit.LAT);
+                staking.setStakingLocked(initStakingLocked.toString());
+                // 如果当前候选节点在共识周期验证人列表，则标识其为共识周期节点
+                if(validatorSet.contains(node.getNodeId())) staking.setIsConsensus(CustomStaking.YesNoEnum.YES.code);
+                staking.setIsSetting(CustomStaking.YesNoEnum.YES.code);
+
+                CustomStaking defaultStaking = defaultStakingMap.get(staking.getNodeId());
+                if(StringUtils.isBlank(staking.getStakingName())&&defaultStaking!=null)
+                    staking.setStakingName(defaultStaking.getStakingName());
+
+                // 暂存至新增质押待入库列表
+                BlockChain.STAGE_DATA.getStakingStage().insertStaking(staking);
+
+                // 更新节点名称映射缓存
+                NODE_NAME_MAP.put(staking.getNodeId(),staking.getStakingName());
+            });
+            BlockChainStage bcr = blockChain.exportResult();
+            dbService.batchSave(Collections.emptyList(),bcr);
+            blockChain.commitResult();
+
+            // 通知质押引擎重新初始化节点缓存
+            blockChain.getStakingExecute().loadNodes();
+        } catch (IOException | CacheConstructException | BusinessException e) {
+            throw new CandidateException("查询内置初始验证人列表失败："+e.getMessage());
         }
     }
 }
--- browser-agent/src/main/java/com/platon/browser/task/BlockSyncTask.java
+++ browser-agent/src/main/java/com/platon/browser/task/BlockSyncTask.java
@@ -2,45 +2,31 @@ package com.platon.browser.task;
 
 import com.platon.browser.bean.CollectResult;
 import com.platon.browser.client.PlatonClient;
-import com.platon.browser.client.SpecialContractApi;
 import com.platon.browser.config.BlockChainConfig;
 import com.platon.browser.dao.mapper.CustomBlockMapper;
 import com.platon.browser.dto.CustomBlock;
-import com.platon.browser.dto.CustomNode;
-import com.platon.browser.dto.CustomStaking;
 import com.platon.browser.engine.BlockChain;
-import com.platon.browser.engine.cache.AddressCacheUpdater;
-import com.platon.browser.engine.cache.StakingCacheUpdater;
 import com.platon.browser.engine.stage.BlockChainStage;
 import com.platon.browser.exception.BlockCollectingException;
 import com.platon.browser.exception.BusinessException;
-import com.platon.browser.exception.CacheConstructException;
-import com.platon.browser.exception.CandidateException;
 import com.platon.browser.service.BlockService;
 import com.platon.browser.service.CandidateService;
 import com.platon.browser.service.DbService;
 import com.platon.browser.service.TransactionService;
-import com.platon.browser.utils.HexTool;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.web3j.platon.BaseResponse;
-import org.web3j.platon.bean.Node;
-import org.web3j.utils.Convert;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-
-import static com.platon.browser.engine.BlockChain.NODE_NAME_MAP;
 
 /**
  * @Auther: Chendongming
@@ -63,10 +49,7 @@ public class BlockSyncTask {
     private BlockChainConfig chainConfig;
     @Autowired
     private PlatonClient client;
-    @Autowired
-    private AddressCacheUpdater addressCacheUpdater;
-    @Autowired
-    private StakingCacheUpdater stakingCacheUpdater;
+
     @Autowired
     private BlockService blockService;
     @Autowired
@@ -106,88 +89,7 @@ public class BlockSyncTask {
          * 查询内置结算周期验证人初始化blockChain的curVerifier属性
           */
         if(maxBlockNumber==null){
-            // 如果库里区块为空，则：
-            try {
-                // 根据区块号0查询共识周期验证人，以便对结算周期验证人设置共识标识
-                BaseResponse<List<Node>> result = SpecialContractApi.getHistoryValidatorList(client.getWeb3j(),BigInteger.ZERO);
-                if(!result.isStatusOk()){
-                    logger.debug("查询实时共识周期验证人列表...");
-                    result = client.getNodeContract().getValidatorList().send();
-                    if(!result.isStatusOk()){
-                        throw new CandidateException("底层链查询实时共识周期验证节点列表出错:"+result.errMsg);
-                    }
-                }
-                // 查询内置共识周期验证人初始化blockChain的curValidator属性
-                Set<String> validatorSet = new HashSet<>();
-                result.data.forEach(node->validatorSet.add(HexTool.prefix(node.getNodeId())));
-
-                // 查询所有候选人
-                Map<String,Node> candidateMap = new HashMap<>();
-                result = client.getNodeContract().getCandidateList().send();
-                if(!result.isStatusOk()){
-                    throw new CandidateException("底层链查询候选验证节点列表出错:"+result.errMsg);
-                }
-                result.data.forEach(node->candidateMap.put(HexTool.prefix(node.getNodeId()),node));
-
-                // 配置中的默认内置节点信息
-                Map<String,CustomStaking> defaultStakingMap = new HashMap<>();
-                chainConfig.getDefaultStakings().forEach(staking -> defaultStakingMap.put(staking.getNodeId(),staking));
-
-                // 根据区块号0查询结算周期验证人列表并入库
-                result = SpecialContractApi.getHistoryVerifierList(client.getWeb3j(),BigInteger.ZERO);
-                if(!result.isStatusOk()){
-                    logger.debug("查询实时结算周期验证人列表...");
-                    result = client.getNodeContract().getVerifierList().send();
-                    if(!result.isStatusOk()){
-                        throw new CandidateException("底层链查询实时结算周期验证节点列表出错:"+result.errMsg);
-                    }
-                }
-
-                result.data.stream().filter(Objects::nonNull).forEach(verifier->{
-                    Node candidate = candidateMap.get(HexTool.prefix(verifier.getNodeId()));
-                    // 补充完整属性
-                    if(candidate!=null) BeanUtils.copyProperties(candidate,verifier);
-
-                    CustomNode node = new CustomNode();
-                    node.updateWithNode(verifier);
-                    node.setIsRecommend(CustomNode.YesNoEnum.YES.code);
-                    node.setStatVerifierTime(BigInteger.ONE.intValue()); // 提前设置验证轮数
-                    node.setStatExpectBlockQty(chainConfig.getExpectBlockCount().longValue());
-                    BlockChain.STAGE_DATA.getStakingStage().insertNode(node);
-
-                    CustomStaking staking = new CustomStaking();
-                    staking.updateWithNode(verifier);
-                    staking.setStatVerifierTime(BigInteger.ONE.intValue()); // 提前设置验证轮数
-                    staking.setIsInit(CustomStaking.YesNoEnum.YES.code);
-                    staking.setIsSetting(CustomStaking.YesNoEnum.YES.code);
-                    // 内置节点默认设置状态为1
-                    staking.setStatus(CustomStaking.StatusEnum.CANDIDATE.code);
-                    // 设置内置节点质押锁定金额
-                    BigDecimal initStakingLocked = Convert.toVon(chainConfig.getDefaultStakingLockedAmount(), Convert.Unit.LAT);
-                    staking.setStakingLocked(initStakingLocked.toString());
-                    // 如果当前候选节点在共识周期验证人列表，则标识其为共识周期节点
-                    if(validatorSet.contains(node.getNodeId())) staking.setIsConsensus(CustomStaking.YesNoEnum.YES.code);
-                    staking.setIsSetting(CustomStaking.YesNoEnum.YES.code);
-
-                    CustomStaking defaultStaking = defaultStakingMap.get(staking.getNodeId());
-                    if(StringUtils.isBlank(staking.getStakingName())&&defaultStaking!=null)
-                        staking.setStakingName(defaultStaking.getStakingName());
-
-                    // 暂存至新增质押待入库列表
-                    BlockChain.STAGE_DATA.getStakingStage().insertStaking(staking);
-
-                    // 更新节点名称映射缓存
-                    NODE_NAME_MAP.put(staking.getNodeId(),staking.getStakingName());
-                });
-                BlockChainStage bcr = blockChain.exportResult();
-                batchSave(Collections.emptyList(),bcr);
-                blockChain.commitResult();
-
-                // 通知质押引擎重新初始化节点缓存
-                blockChain.getStakingExecute().loadNodes();
-            } catch (IOException | CacheConstructException | BusinessException e) {
-                throw new CandidateException("查询内置初始验证人列表失败："+e.getMessage());
-            }
+            candidateService.init();
         }
     }
 
@@ -238,7 +140,7 @@ public class BlockSyncTask {
             BlockChainStage bizData = blockChain.exportResult();
             try {
                 // 入库失败，立即停止，防止采集后续更高的区块号，导致不连续区块号出现
-                batchSave(blocks, bizData);
+                dbService.batchSave(blocks, bizData);
             } catch (BusinessException e) {
                 break;
             }
@@ -249,22 +151,6 @@ public class BlockSyncTask {
             } catch (InterruptedException e) {
                 throw new BlockCollectingException("区块采集暂停被中断:"+e.getMessage());
             }
-        }
-    }
-
-    private void batchSave(List<CustomBlock> basicData, BlockChainStage bizData) throws BusinessException {
-        try{
-            // 入库前更新统计信息
-            addressCacheUpdater.updateAddressStatistics();
-            stakingCacheUpdater.updateStakingStatistics();
-            // 串行批量入库
-            dbService.insertOrUpdate(basicData,bizData);
-            blockChain.commitResult();
-            // 缓存整理
-            BlockChain.NODE_CACHE.sweep();
-            BlockChain.PROPOSALS_CACHE.sweep();
-        }catch (Exception e){
-            throw new BusinessException("数据批量入库出错："+e.getMessage());
         }
     }
 }
