578fedaf533f11ec54bcef186d87c4233f0d8459
xiaozhiqiang <fw-xiaozhiqiang@juzix.net>
2019-09-16 19:06:02 +0800

    common dto单元测试提交

---
+++ browser-common/src/test/java/com/platon/browser/dto/CustomRpPlanTest.java
@@ -0,0 +1,15 @@
+package com.platon.browser.dto;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class CustomRpPlanTest {
+
+	@Test
+	public void testCustomRpPlan() {
+		CustomRpPlan plan = new CustomRpPlan();
+		assertNotNull(plan);
+	}
+
+}
