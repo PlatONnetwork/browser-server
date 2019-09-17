bdb21810b61e73482f6d606f5fdeb30f520bd047
dongqile <dongqile@juzix.io>
2019-09-17 16:17:38 +0800

    1.单元测试代码提交

--- browser-agent/src/test/java/com/platon/browser/handler/delagation/DelegateHandlerTest.java
+++ browser-agent/src/test/java/com/platon/browser/handler/delagation/DelegateHandlerTest.java
@@ -1,10 +1,32 @@
 package com.platon.browser.handler.delagation;
 
+import com.platon.browser.TestBase;
+import com.platon.browser.engine.handler.EventContext;
+import com.platon.browser.engine.handler.delegation.DelegateHandler;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.assertTrue;
+
 /**
  * @Auther: dongqile
  * @Date: 2019/9/5
- * @Description:
+ * @Description: 委托处理业务测试类
  */
 
-public class DelegateHandlerTest {
+public class DelegateHandlerTest extends TestBase {
+
+    @Autowired
+    private DelegateHandler delegateHandler;
+
+    @Test
+    public void testHandler(){
+        EventContext eventContext = new EventContext();
+        try{
+            delegateHandler.handle(eventContext);
+            assertTrue(true);
+        }catch (Exception e){
+            assertTrue(false);
+        }
+    }
 }
--- browser-agent/src/test/resources/sample-data-export.sql
+++ browser-agent/src/test/resources/sample-data-export.sql
@@ -13,50 +13,56 @@ FROM
         JOIN un_delegation ud
              ON ud.`staking_block_num` = st.`staking_block_num` and st.`node_id`=ud.`node_id`;
 
+
+
 select DISTINCT
     bl.*
 from
     block bl
-        join `transaction` tx
-             on tx.`block_number` = bl.`number`
-        join staking st
-             on st.`staking_block_num` = bl.`number`
-        join delegation de
-             on de.`staking_block_num` = st.`staking_block_num`
-        join node nd
-             on nd.`node_id` = bl.`node_id`
-        join un_delegation ud
-             on ud.`staking_block_num` = st.`staking_block_num`;
+        JOIN `transaction` tx
+             ON tx.`block_number` = bl.`number`
+        JOIN staking st
+             ON st.`staking_block_num` = bl.`number`
+        JOIN delegation de
+             ON de.`staking_block_num` = st.`staking_block_num`
+        JOIN node nd
+             ON nd.`node_id` = bl.`node_id`
+        JOIN un_delegation ud
+             ON ud.`staking_block_num` = st.`staking_block_num`;
+
 
 select DISTINCT
     tx.*
 from
     block bl
-        join `transaction` tx
-             on tx.`block_number` = bl.`number`
-        join staking st
-             on st.`staking_block_num` = bl.`number`
-        join delegation de
-             on de.`staking_block_num` = st.`staking_block_num`
+        JOIN `transaction` tx
+             ON tx.`block_number` = bl.`number`
+        JOIN staking st
+             ON st.`staking_block_num` = bl.`number`
+        JOIN delegation de
+             ON de.`staking_block_num` = st.`staking_block_num`
         JOIN node nd
              ON nd.`node_id` = bl.`node_id`
         JOIN un_delegation ud
              ON ud.`staking_block_num` = st.`staking_block_num`;
+        JOIN proposal pro
+             ON pro.`hash` = tx.`hash`
 
 select DISTINCT
     st.*
 from
     block bl
-        join `transaction` tx
-             on tx.`block_number` = bl.`number`
-        join staking st
-             on st.`staking_block_num` = bl.`number`
-        join delegation de
-             on de.`staking_block_num` = st.`staking_block_num`
+        JOIN `transaction` tx
+             ON tx.`block_number` = bl.`number`
+        JOIN staking st
+             ON st.`staking_block_num` = bl.`number`
+        JOIN delegation de
+             ON de.`staking_block_num` = st.`staking_block_num`
         JOIN node nd
              ON nd.`node_id` = bl.`node_id`
         JOIN un_delegation ud
              ON ud.`staking_block_num` = st.`staking_block_num`;
+
 
 SELECT DISTINCT
     de.*
@@ -73,6 +79,7 @@ FROM
         JOIN un_delegation ud
              ON ud.`staking_block_num` = st.`staking_block_num`;
 
+
 SELECT DISTINCT
     ud.*
 FROM
@@ -87,3 +94,21 @@ FROM
              ON nd.`node_id` = bl.`node_id`
         JOIN un_delegation ud
              ON ud.`staking_block_num` = st.`staking_block_num`;
+
+SELECT DISTINCT
+    pro.*
+FROM
+    block bl
+        JOIN `transaction` tx
+             ON tx.`block_number` = bl.`number`
+        JOIN `proposal` p
+             ON p.`hash` = tx.`hash`;
+
+SELECT DISTINCT
+    ad.*
+FROM
+    block bl
+        JOIN `transaction` tx
+             ON tx.`block_number` = bl.`number`
+        JOIN `address` ad
+             ON ad.`address` = tx.`from` OR ad.`address` = tx.`to`;
