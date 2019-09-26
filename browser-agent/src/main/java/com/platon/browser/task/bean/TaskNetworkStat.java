5d40cde2cc606d65e8ddc5078317a3440d63078d
chendongming <chendongming@juzix.net>
2019-09-26 18:49:38 +0800

    添加统计任务缓存，采块线程通过任务缓存获取最新统计数据并入库，防止同时修改相同数据

--- browser-agent/src/main/java/com/platon/browser/task/bean/TaskNetworkStat.java
+++ browser-agent/src/main/java/com/platon/browser/task/bean/TaskNetworkStat.java
@@ -1,5 +1,8 @@
 package com.platon.browser.task.bean;
 
+import lombok.Data;
+
+@Data
 public class TaskNetworkStat  extends TaskBase{
     private volatile String issueValue;
     private volatile String turnValue;
