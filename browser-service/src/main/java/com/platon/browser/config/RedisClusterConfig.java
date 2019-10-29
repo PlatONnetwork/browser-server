ab76f1afc2227fb991a3f9b4cf827615d29de5f1
chendongming <chendongming@juzix.net>
2019-10-29 11:00:53 +0800

    把Redis从Sentinel模式更换为集群模式

--- browser-service/src/main/java/com/platon/browser/config/RedisClusterConfig.java
+++ browser-service/src/main/java/com/platon/browser/config/RedisClusterConfig.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
