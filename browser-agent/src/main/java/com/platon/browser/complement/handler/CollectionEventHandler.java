0af834a2ae5041cb30618f784ffa74b12fc087f7
chendongming <chendongming@juzix.net>
2019-11-01 14:11:22 +0800

    1、区块采集单独使用disruptor队列保证顺序和异步处理；

--- browser-agent/src/main/java/com/platon/browser/complement/handler/CollectionEventHandler.java
+++ browser-agent/src/main/java/com/platon/browser/complement/handler/CollectionEventHandler.java
@@ -3,7 +3,6 @@ package com.platon.browser.complement.handler;
 import com.platon.browser.queue.collection.event.CollectionEvent;
 import com.platon.browser.queue.collection.handler.ICollectionEventHandler;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.retry.annotation.Retryable;
 
 /**
  * 区块事件处理器
@@ -11,8 +10,6 @@ import org.springframework.retry.annotation.Retryable;
 @Slf4j
 public class CollectionEventHandler implements ICollectionEventHandler {
 
-    @Override
-    @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE)
     public void onEvent(CollectionEvent event, long sequence, boolean endOfBatch) {
 
         // 此处调用 complement模块的功能
