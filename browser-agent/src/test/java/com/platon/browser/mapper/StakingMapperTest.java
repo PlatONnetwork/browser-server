e2c7d3a22b0ba709f1a37120e030508f79076c2d
dongqile <dongqile@juzix.io>
2019-11-01 16:23:05 +0800

    1.sql测试类优化

--- browser-agent/src/test/java/com/platon/browser/mapper/StakingMapperTest.java
+++ browser-agent/src/test/java/com/platon/browser/mapper/StakingMapperTest.java
@@ -2,18 +2,14 @@ package com.platon.browser.mapper;
 
 import com.platon.browser.AgentApplication;
 import com.platon.browser.TestBase;
-import com.platon.browser.dao.entity.Staking;
-import com.platon.browser.dao.entity.StakingExample;
-import com.platon.browser.dao.entity.StakingKey;
+import com.platon.browser.dao.entity.*;
 import com.platon.browser.dao.mapper.NodeMapper;
 import com.platon.browser.dao.mapper.NodeOptMapper;
 import com.platon.browser.dao.mapper.StakingMapper;
-import com.platon.browser.mapper.ParamBuild.ParamBuildTest;
 import com.platon.browser.persistence.dao.mapper.CreateStakingMapper;
 import com.platon.browser.persistence.dao.mapper.ModifyStakingMapper;
 import com.platon.browser.persistence.dao.param.CreateStakingParam;
 import com.platon.browser.persistence.dao.param.ModifyStakingParam;
-import org.checkerframework.checker.units.qual.A;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,12 +17,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.sql.Date;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 /**
  * @Auther: dongqile
@@ -45,8 +40,6 @@ public class StakingMapperTest extends TestBase {
     @Autowired
     private ModifyStakingMapper modifyStakingMapper;
 
-    @Autowired
-    private ParamBuildTest paramBuildTest;
 
     @Autowired
     private NodeOptMapper nodeOptMapper;
@@ -59,14 +52,43 @@ public class StakingMapperTest extends TestBase {
 
     @Test
     public void createStakingMapper(){
-        CreateStakingParam createStakingParam = paramBuildTest.crateStakingBuild();
+
+        List<Node> nodes = nodeMapper.selectByExample(null);
+
+        CreateStakingParam createStakingParam = stakingParam();
+        //删除数据
+        deleteCreateStaking(createStakingParam);
+        //数据插入
         createStakingMapper.createStaking(createStakingParam);
+        //staking数据插入验证
         StakingKey stakingKey = new StakingKey();
         stakingKey.setNodeId(createStakingParam.getNodeId());
         stakingKey.setStakingBlockNum(createStakingParam.getStakingBlockNum().longValue());
         Staking staking = stakingMapper.selectByPrimaryKey(stakingKey);
         assertEquals(createStakingParam.getNodeId(), staking.getNodeId());
+        //node数据插入验证
+        Node node = nodeMapper.selectByPrimaryKey(createStakingParam.getNodeId());
+        assertEquals(createStakingParam.getStakingBlockNum(),node.getStakingBlockNum());
+        //opt数据插入验证
+        NodeOptExample nodeOptExample = new NodeOptExample();
+        nodeOptExample.createCriteria().andNodeIdEqualTo(createStakingParam.getNodeId())
+                .andBNumEqualTo(createStakingParam.getStakingBlockNum().longValue());
+        List <NodeOpt> nodeOptList = nodeOptMapper.selectByExample(nodeOptExample);
+        assertEquals(nodeOptList.get(0).getNodeId(),createStakingParam.getNodeId());
+    }
 
+    public void deleteCreateStaking(CreateStakingParam param){
+        //删除staking数据
+        StakingKey stakingKey = new StakingKey();
+        stakingKey.setNodeId(param.getNodeId());
+        stakingKey.setStakingBlockNum(param.getStakingBlockNum().longValue());
+        stakingMapper.deleteByPrimaryKey(stakingKey);
+        //删除node数据
+        nodeMapper.deleteByPrimaryKey(param.getNodeId());
+        //删除opt数据
+        NodeOptExample nodeOptExample = new NodeOptExample();
+        nodeOptExample.createCriteria().andNodeIdEqualTo(param.getNodeId()).andBNumEqualTo(param.getStakingBlockNum().longValue());
+        nodeOptMapper.deleteByExample(nodeOptExample);
     }
 
     @Test
@@ -85,4 +107,6 @@ public class StakingMapperTest extends TestBase {
         modifyStakingParam.setNodeId("0x20a090d94bc5015c9339a46e9ca5d80057a5ef25cc14e71cef67b502ec32949253f046821e80dfb6ff666ef0e0badf58fdb719368c38393f7c40ebcf18d8ed18");
         modifyStakingMapper.modifyStaking(modifyStakingParam);
     }
+
+
 }
