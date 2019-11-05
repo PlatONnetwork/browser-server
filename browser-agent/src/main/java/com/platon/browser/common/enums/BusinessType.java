855a4970405ea2a2611615e2bf1e518c524c06b4
chendongming <chendongming@juzix.net>
2019-11-05 21:10:11 +0800

    修改业务类型

--- browser-agent/src/main/java/com/platon/browser/common/enums/BusinessType.java
+++ browser-agent/src/main/java/com/platon/browser/common/enums/BusinessType.java
@@ -1,20 +1,25 @@
 package com.platon.browser.common.enums;
 
 /**
- * 业务类型：
- * 质押：创建、编辑、增持、减持、退出
- * 委托：委托、撤销
- * 提案：文本、版本、升级、投票
- * 处罚：低出块率、双签
- * 版本声明、创建锁仓计划
+ * 业务类型枚举
  */
 public enum BusinessType {
+    // 质押：创建、编辑、增持、退出
     STAKE_CREATE,STAKE_MODIFY,STAKE_INCREASE,STAKE_EXIT,
+    // 委托：委托、撤销
     DELEGATE_CREATE,DELEGATE_EXIT,
+    // 提案：文本、版本、升级、投票、取消
     PROPOSAL_TEXT,PROPOSAL_VERSION,PROPOSAL_UPGRADE,PROPOSAL_VOTE,PROPOSAL_CANCEL,
+    // 处罚：低出块率、举报
     SLASH_LOW_RATE,REPORT,
-    VERSION_DECLARE,RESTRICTING_CREATE,
+    // 版本声明
+    VERSION_DECLARE,
+    // 创建锁仓计划
+    RESTRICTING_CREATE,
+    // 新区块
     NEW_BLOCK,
-    SETTLE_EPOCH,CONSENSUS_EPOCH,ELECTION_EPOCH,
+    // 选举、共识周期、结算周期
+    ELECTION_EPOCH,CONSENSUS_EPOCH,SETTLE_EPOCH,
+    // 网络统计
     NETWORK_STATISTIC
 }
