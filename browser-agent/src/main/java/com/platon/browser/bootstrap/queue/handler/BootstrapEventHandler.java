4e607961bc041b16a2bcb27c18e635afe3b361cc
chendongming <chendongming@juzix.net>
2019-11-09 21:00:49 +0800

    1、交易中难以计算的数据字段入库到mysql，操作日志也入库到mysql，维护步骤如下： 2、自检流程补充缺失区块和交易，交易信息补充数据从数据库备份表获取，入库ES/Redis后通知定时任务删除； 3、操作日志从数据库获取，入库ES/Redis后通知定时任务删除；

--- browser-agent/src/main/java/com/platon/browser/bootstrap/queue/handler/BootstrapEventHandler.java
+++ browser-agent/src/main/java/com/platon/browser/bootstrap/queue/handler/BootstrapEventHandler.java
@@ -4,21 +4,28 @@ import com.lmax.disruptor.EventHandler;
 import com.platon.browser.bootstrap.queue.event.BootstrapEvent;
 import com.platon.browser.client.result.ReceiptResult;
 import com.platon.browser.common.collection.dto.CollectionBlock;
+import com.platon.browser.common.complement.dto.ComplementNodeOpt;
 import com.platon.browser.common.service.elasticsearch.EsImportService;
 import com.platon.browser.common.service.redis.RedisImportService;
+import com.platon.browser.common.utils.BakDataDeleteUtil;
+import com.platon.browser.dao.entity.NOptBak;
+import com.platon.browser.dao.entity.NOptBakExample;
+import com.platon.browser.dao.entity.TxBak;
+import com.platon.browser.dao.entity.TxBakExample;
+import com.platon.browser.dao.mapper.NOptBakMapper;
+import com.platon.browser.dao.mapper.TxBakMapper;
 import com.platon.browser.elasticsearch.dto.Block;
 import com.platon.browser.elasticsearch.dto.NodeOpt;
 import com.platon.browser.elasticsearch.dto.Transaction;
 import com.platon.browser.exception.BeanCreateOrUpdateException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.web3j.protocol.core.methods.response.PlatonBlock;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -32,10 +39,13 @@ public class BootstrapEventHandler implements EventHandler<BootstrapEvent> {
     private EsImportService esImportService;
     @Autowired
     private RedisImportService redisImportService;
+    @Autowired
+    private TxBakMapper txBakMapper;
+    @Autowired
+    private NOptBakMapper nOptBakMapper;
 
     private Set<Block> blocks=new HashSet<>();
     private Set<Transaction> transactions=new HashSet<>();
-    private Set<NodeOpt> nodeOpts=new HashSet<>();
 
     private Long preBlockNum=0L;
     @Override
@@ -56,8 +66,43 @@ public class BootstrapEventHandler implements EventHandler<BootstrapEvent> {
             blocks.add(block);
             transactions.addAll(block.getTransactions());
             block.setTransactions(null);
+
+            // 查询交易信息表
+            List<String> txHashes = new ArrayList<>();
+            transactions.forEach(tx->txHashes.add(tx.getHash()));
+            TxBakExample txBakExample = new TxBakExample();
+            txBakExample.createCriteria().andHashIn(txHashes);
+            List<TxBak> txBaks = txBakMapper.selectByExample(txBakExample);
+            Map<String,TxBak> txBakMap = new HashMap<>();
+            Long txMaxId = 0L;
+            for (TxBak bak : txBaks) {
+                if(bak.getId()>txMaxId) txMaxId=bak.getId();
+                txBakMap.put(bak.getHash(), bak);
+            }
+            // 更新交易入库到ES和Redis的交易信息
+            transactions.forEach(tx->{
+                TxBak bak = txBakMap.get(tx.getHash());
+                BeanUtils.copyProperties(bak,tx);
+            });
+
+            // 从数据库查询日志信息,入库到ES/Redis
+            NOptBakExample nOptBakExample = new NOptBakExample();
+            List<NOptBak> nOptBaks = nOptBakMapper.selectByExample(nOptBakExample);
+            Set<NodeOpt> nodeOpts = new HashSet<>();
+            Long nOptMaxId = 0L;
+            for (NOptBak bak : nOptBaks) {
+                if(bak.getId()>nOptMaxId) nOptMaxId=bak.getId();
+                NodeOpt no = ComplementNodeOpt.newInstance();
+                BeanUtils.copyProperties(bak, no);
+                nodeOpts.add(no);
+            }
+
             esImportService.batchImport(blocks,transactions,nodeOpts);
             redisImportService.batchImport(blocks,transactions, Collections.emptySet());
+            // 更新已处理的最大ID, 方便删除任务删除已用完的数据
+            BakDataDeleteUtil.updateTxBakMaxId(txMaxId);
+            BakDataDeleteUtil.updateNOptBakMaxId(nOptMaxId);
+
             clear();
             preBlockNum=block.getNum();
             event.getCallback().call(block.getNum());
@@ -72,6 +117,5 @@ public class BootstrapEventHandler implements EventHandler<BootstrapEvent> {
     private void clear(){
         blocks.clear();
         transactions.clear();
-        nodeOpts.clear();
     }
 }
