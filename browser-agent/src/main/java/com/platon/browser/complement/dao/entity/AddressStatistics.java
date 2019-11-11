94b9adbf7a980bfc49fe2259e0f286799da43b75
chendai <chendai@juzix.net>
2019-11-11 11:49:55 +0800

    地址统计

---
+++ browser-agent/src/main/java/com/platon/browser/complement/dao/entity/AddressStatistics.java
@@ -0,0 +1,21 @@
+package com.platon.browser.complement.dao.entity;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+import lombok.Data;
+
+@Data
+public class AddressStatistics {
+    private String stakingAddr;
+    private BigDecimal stakingHes;
+    private BigDecimal stakingLocked;
+    private BigDecimal stakingReduction;
+    private String delegateAddr;
+    private BigDecimal delegateHes;
+    private BigDecimal delegateLocked;
+    private BigDecimal delegateReleased; 
+    private String nodeId;
+    private Set<String> nodeIdSet = new HashSet<String>();
+}
