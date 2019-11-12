ff759f882d206908ac954bb4084fdf3cd5263301
chendai <chendai@juzix.net>
2019-11-12 11:24:15 +0800

    补充锁仓入库逻辑

--- browser-agent/src/main/java/com/platon/browser/complement/converter/restricting/RestrictingCreateConverter.java
+++ browser-agent/src/main/java/com/platon/browser/complement/converter/restricting/RestrictingCreateConverter.java
@@ -1,17 +1,21 @@
 package com.platon.browser.complement.converter.restricting;
 
+import java.math.BigInteger;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.platon.browser.common.queue.collection.event.CollectionEvent;
 import com.platon.browser.complement.converter.BusinessParamConverter;
+import com.platon.browser.complement.dao.mapper.RestrictingBusinessMapper;
 import com.platon.browser.complement.dao.param.restricting.RestrictingCreate;
 import com.platon.browser.complement.dao.param.restricting.RestrictingItem;
 import com.platon.browser.elasticsearch.dto.Transaction;
 import com.platon.browser.param.RestrictingCreateParam;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
-import java.math.BigInteger;
-import java.util.List;
-import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @description: 委托业务参数转换器
@@ -21,6 +25,9 @@ import java.util.stream.Collectors;
 @Slf4j
 @Service
 public class RestrictingCreateConverter extends BusinessParamConverter<RestrictingCreate> {
+	
+	@Autowired
+	private RestrictingBusinessMapper restrictingBusinessMapper;
 	
     @Override
     public RestrictingCreate convert(CollectionEvent event, Transaction tx) {
@@ -43,6 +50,9 @@ public class RestrictingCreateConverter extends BusinessParamConverter<Restricti
     	RestrictingCreate businessParam= RestrictingCreate.builder()
     			.itemList(restrictingItems)
                 .build();
+    	
+    	// 锁仓记录入库
+    	restrictingBusinessMapper.create(businessParam);
 
 		log.debug("处理耗时:{} ms",System.currentTimeMillis()-startTime);
 
