40387069a9187f802d49046af32c135396e495e0
chendongming <chendongming@juzix.net>
2019-11-16 10:35:11 +0800

    添加环形缓冲区队列监控任务

--- browser-agent/src/main/java/com/platon/browser/bootstrap/queue/publisher/BootstrapEventPublisher.java
+++ browser-agent/src/main/java/com/platon/browser/bootstrap/queue/publisher/BootstrapEventPublisher.java
@@ -2,13 +2,13 @@ package com.platon.browser.bootstrap.queue.publisher;
 
 import com.lmax.disruptor.EventFactory;
 import com.lmax.disruptor.EventTranslatorThreeArg;
-import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.dsl.Disruptor;
 import com.lmax.disruptor.util.DaemonThreadFactory;
 import com.platon.browser.bootstrap.queue.callback.Callback;
 import com.platon.browser.bootstrap.queue.event.BootstrapEvent;
 import com.platon.browser.bootstrap.queue.handler.BootstrapEventHandler;
 import com.platon.browser.client.result.ReceiptResult;
+import com.platon.browser.common.queue.AbstractPublisher;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,28 +24,30 @@ import java.util.concurrent.CompletableFuture;
  */
 @Slf4j
 @Component
-public class BootstrapEventPublisher {
+public class BootstrapEventPublisher extends AbstractPublisher<BootstrapEvent> {
     private static final EventTranslatorThreeArg<BootstrapEvent,CompletableFuture<PlatonBlock>,CompletableFuture<ReceiptResult>, Callback>
     TRANSLATOR = (event, sequence, blockCF,receiptCF,callback)->event.setBlockCF(blockCF).setReceiptCF(receiptCF).setCallback(callback);
-    private RingBuffer<BootstrapEvent> ringBuffer;
-    // 指定环形队列大小,必须是2的指数倍
     @Value("${disruptor.queue.block.buffer-size}")
     private int ringBufferSize;
+    @Override
+    public int getRingBufferSize() {
+        return ringBufferSize;
+    }
+
     private EventFactory<BootstrapEvent> eventFactory = () -> BootstrapEvent.builder().build();
     @Autowired
     private BootstrapEventHandler handler;
 
-    @Getter private Disruptor<BootstrapEvent> disruptor;
+    @Getter
+    private Disruptor<BootstrapEvent> disruptor;
 
     @PostConstruct
     public void init(){
         disruptor = new Disruptor<>(eventFactory, ringBufferSize, DaemonThreadFactory.INSTANCE);
-        // 设置事件处理器
         disruptor.handleEventsWith(handler);
-        // 启动Disruptor,让所有生产和消费线程运行
         disruptor.start();
-        // 获取环形缓冲引用，用于发布事件
         ringBuffer = disruptor.getRingBuffer();
+        register(BootstrapEventPublisher.class.getSimpleName(),this);
     }
 
     public void publish(CompletableFuture<PlatonBlock> blockCF, CompletableFuture<ReceiptResult> receiptCF, Callback callback){
@@ -54,5 +56,6 @@ public class BootstrapEventPublisher {
 
     public void shutdown() {
         disruptor.shutdown();
+        unregister(BootstrapEventPublisher.class.getSimpleName());
     }
 }
--- browser-agent/src/main/java/com/platon/browser/common/queue/collection/publisher/CollectionEventPublisher.java
+++ browser-agent/src/main/java/com/platon/browser/common/queue/collection/publisher/CollectionEventPublisher.java
@@ -2,10 +2,10 @@ package com.platon.browser.common.queue.collection.publisher;
 
 import com.lmax.disruptor.EventFactory;
 import com.lmax.disruptor.EventTranslatorThreeArg;
-import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.dsl.Disruptor;
 import com.lmax.disruptor.util.DaemonThreadFactory;
 import com.platon.browser.common.collection.dto.EpochMessage;
+import com.platon.browser.common.queue.AbstractPublisher;
 import com.platon.browser.common.queue.collection.event.CollectionEvent;
 import com.platon.browser.common.queue.collection.handler.ICollectionEventHandler;
 import com.platon.browser.elasticsearch.dto.Block;
@@ -23,13 +23,15 @@ import java.util.List;
  */
 @Slf4j
 @Component
-public class CollectionEventPublisher {
+public class CollectionEventPublisher extends AbstractPublisher<CollectionEvent> {
     private static final EventTranslatorThreeArg<CollectionEvent, Block, List<Transaction>,EpochMessage>
     TRANSLATOR = (event, sequence, block,transactions,epochMessage)->event.setBlock(block).setTransactions(transactions).setEpochMessage(epochMessage);
-    private RingBuffer<CollectionEvent> ringBuffer;
-    // 指定环形队列大小,必须是2的指数倍
     @Value("${disruptor.queue.collection.buffer-size}")
     private int ringBufferSize;
+    @Override
+    public int getRingBufferSize() {
+        return ringBufferSize;
+    }
     private EventFactory<CollectionEvent> eventFactory = () -> CollectionEvent.builder().build();
     @Autowired
     private ICollectionEventHandler handler;
@@ -37,12 +39,10 @@ public class CollectionEventPublisher {
     @PostConstruct
     private void init(){
         Disruptor<CollectionEvent> disruptor = new Disruptor<>(eventFactory, ringBufferSize, DaemonThreadFactory.INSTANCE);
-        // 设置事件处理器
         disruptor.handleEventsWith(handler);
-        // 启动Disruptor,让所有生产和消费线程运行
         disruptor.start();
-        // 获取环形缓冲引用，用于发布事件
         ringBuffer = disruptor.getRingBuffer();
+        register(CollectionEventPublisher.class.getSimpleName(),this);
     }
 
     public void publish(Block block, List<Transaction> transactions,EpochMessage epochMessage){
