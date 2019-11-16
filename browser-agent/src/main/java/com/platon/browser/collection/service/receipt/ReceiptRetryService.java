957832f206c983647df23fd56da8fdccf1dc76a9
chendongming <chendongming@juzix.net>
2019-11-16 21:09:53 +0800

    web3j重试调用更新

--- browser-agent/src/main/java/com/platon/browser/collection/service/receipt/ReceiptRetryService.java
+++ browser-agent/src/main/java/com/platon/browser/collection/service/receipt/ReceiptRetryService.java
@@ -36,10 +36,8 @@ public class ReceiptRetryService {
             param.setMethod(RECEIPT_RPC_INTERFACE);
             param.getParams().add(blockNumber);
             ReceiptResult result = platOnClient.getReceiptResult(blockNumber);
-            log.debug("回执结果:{}", result);
-
+            log.debug("回执结果数:{}", result.getResult().size());
             log.debug("处理耗时:{} ms",System.currentTimeMillis()-startTime);
-
             return result;
         }catch (Exception e){
             platOnClient.updateCurrentWeb3jWrapper();
