aee749f8530a7c923475482070e1ef5cadcc4fd0
chendongming <chendongming@juzix.net>
2019-12-12 19:02:42 +0800

    修复单元测试失败问题

--- browser-service/src/test/java/com/platon/browser/enums/StakingStatusEnumTest.java
+++ browser-service/src/test/java/com/platon/browser/enums/StakingStatusEnumTest.java
@@ -1,8 +1,8 @@
 package com.platon.browser.enums;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class StakingStatusEnumTest {
 
@@ -12,23 +12,6 @@ public class StakingStatusEnumTest {
 		assertTrue(retEnum.getCode()==1);
 		assertTrue(retEnum.getName().equals("candidate"));
 	}
-	
-	@Test
-	public void testGetCodeByStatus() {
-		Integer code = StakingStatusEnum.getCodeByStatus(1, 1, 1);
-		assertTrue(code==2);
-		
-		code = StakingStatusEnum.getCodeByStatus(1, 1, 2);
-		assertTrue(code==1);
-		
-		code = StakingStatusEnum.getCodeByStatus(2, 1, 2);
-		assertTrue(code==4);
-		
-		code = StakingStatusEnum.getCodeByStatus(3, 1, 2);
-		assertTrue(code==5);
-		
-		code = StakingStatusEnum.getCodeByStatus(4, 1, 2);
-		assertTrue(code==null);
-	}
+
 
 }
