cc5984a939b94b76006ee020ccba03360b397eb5
chendongming <chendongming@juzix.net>
2020-01-07 21:38:54 +0800

    1、修正质押修改委托解码顺序逻辑

--- browser-service/src/main/java/com/platon/browser/util/decode/StakeModifyDecoder.java
+++ browser-service/src/main/java/com/platon/browser/util/decode/StakeModifyDecoder.java
@@ -6,12 +6,29 @@ import org.web3j.rlp.RlpList;
 import org.web3j.rlp.RlpString;
 import org.web3j.utils.Numeric;
 
+import java.math.BigInteger;
+
+import static com.platon.browser.util.decode.Decoder.bigIntegerResolver;
 import static com.platon.browser.util.decode.Decoder.stringResolver;
 
 /**
  * @description: 创建验证人交易输入参数解码器
  * @author: chendongming@juzix.net
  * @create: 2019-11-04 20:13:04
+ *
+ *  return Arrays.<Type>asList(new Uint16(stakingAmountType.getValue()) 1
+ *          , new BytesType(Numeric.hexStringToByteArray(benifitAddress)) 2
+ *          , new BytesType(Numeric.hexStringToByteArray(nodeId)) 3
+ *          , new Utf8String(externalId) 4
+ *          , new Utf8String(nodeName) 5
+ *          , new Utf8String(webSite) 6
+ *          , new Utf8String(details) 7
+ *          , new Int256(amount) 8
+ *          , new Uint16(rewardPer) 9
+ *          , new Uint32(processVersion.getProgramVersion()) 10
+ *          , new BytesType(Numeric.hexStringToByteArray(processVersion.getProgramVersionSign())) 11
+ *          , new BytesType(Numeric.hexStringToByteArray(blsPubKey)) 12
+ *          , new BytesType(Numeric.hexStringToByteArray(blsProof)) 13
  **/
 public class StakeModifyDecoder {
     private StakeModifyDecoder(){}
@@ -22,16 +39,18 @@ public class StakeModifyDecoder {
         //被质押的节点的NodeId
         String nodeId = stringResolver((RlpString) rootList.getValues().get(2));
         //外部Id
-        String externalId = stringResolver((RlpString) rootList.getValues().get(3));
+        BigInteger rewardPer = bigIntegerResolver((RlpString) rootList.getValues().get(3));
+        //外部Id externalId
+        String externalId = stringResolver((RlpString) rootList.getValues().get(4));
         externalId = new String(Numeric.hexStringToByteArray(externalId));
         //被质押节点的名称
-        String nodeName = stringResolver((RlpString) rootList.getValues().get(4));
+        String nodeName = stringResolver((RlpString) rootList.getValues().get(5));
         nodeName = new String(Numeric.hexStringToByteArray(nodeName));
         //节点的第三方主页
-        String website = stringResolver((RlpString) rootList.getValues().get(5));
+        String website = stringResolver((RlpString) rootList.getValues().get(6));
         website = new String(Numeric.hexStringToByteArray(website));
         //节点的描述
-        String detail = stringResolver((RlpString) rootList.getValues().get(6));
+        String detail = stringResolver((RlpString) rootList.getValues().get(7));
         detail = new String(Numeric.hexStringToByteArray(detail));
 
         return StakeModifyParam.builder()
@@ -41,7 +60,7 @@ public class StakeModifyDecoder {
                 .nodeName(nodeName)
                 .website(website)
                 .details(detail)
-                .delegateRewardPer(0/*TODO:需要解析出委托奖励比例*/) // 非必填
+                .delegateRewardPer(rewardPer.intValue()) // 非必填
                 .build();
     }
 }
