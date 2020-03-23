e08871f54787932644bdc91ec63604c008cc1cb5
chendongming <chendongming@juzix.net>
2020-03-23 14:39:32 +0800

    在选举区块时刻从特殊节点接口获取低出块率节点ID，并执行处罚逻辑

--- browser-agent/src/main/java/com/platon/browser/complement/converter/epoch/OnElectionConverter.java
+++ browser-agent/src/main/java/com/platon/browser/complement/converter/epoch/OnElectionConverter.java
@@ -1,31 +1,32 @@
 package com.platon.browser.complement.converter.epoch;
 
+import com.platon.browser.client.HistoryLowRateSlash;
+import com.platon.browser.client.PlatOnClient;
+import com.platon.browser.client.SpecialApi;
 import com.platon.browser.common.complement.cache.NetworkStatCache;
 import com.platon.browser.common.complement.dto.ComplementNodeOpt;
 import com.platon.browser.common.queue.collection.event.CollectionEvent;
 import com.platon.browser.complement.dao.mapper.EpochBusinessMapper;
-import com.platon.browser.complement.dao.param.BusinessParam;
 import com.platon.browser.complement.dao.param.epoch.Election;
 import com.platon.browser.config.BlockChainConfig;
 import com.platon.browser.dao.entity.Staking;
+import com.platon.browser.dao.entity.StakingExample;
+import com.platon.browser.dao.mapper.StakingMapper;
+import com.platon.browser.dto.CustomStaking;
 import com.platon.browser.elasticsearch.dto.Block;
 import com.platon.browser.elasticsearch.dto.NodeOpt;
-import com.platon.browser.enums.ModifiableGovernParamEnum;
-import com.platon.browser.exception.BlockNumberException;
 import com.platon.browser.exception.BusinessException;
-import com.platon.browser.service.govern.ParameterService;
 import com.platon.browser.utils.EpochUtil;
-import com.platon.sdk.contracts.ppos.dto.resp.Node;
+import com.platon.browser.utils.HexTool;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.web3j.protocol.Web3j;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -39,74 +40,45 @@ public class OnElectionConverter {
     @Autowired
     private BlockChainConfig blockChainConfig;
 	@Autowired
-	private ParameterService parameterService;
+	private SpecialApi specialApi;
+	@Autowired
+	private PlatOnClient platOnClient;
+	@Autowired
+	private StakingMapper stakingMapper;
 	
-	public Optional<List<NodeOpt>> convert(CollectionEvent event, Block block) throws BlockNumberException {
-
+	public List<NodeOpt> convert(CollectionEvent event, Block block) {
 		long startTime = System.currentTimeMillis();
-
-		//当前周期共识验证人
-		List<String> curValidatorList = event.getEpochMessage().getCurValidatorList().stream()
-				.map(Node::getNodeId).collect(Collectors.toList());
-
-		//前一周期共识验证人
-		List<String> preValidatorList = event.getEpochMessage().getPreValidatorList().stream()
-				.map(Node::getNodeId).collect(Collectors.toList());
-		if(preValidatorList.isEmpty()) {
-			return Optional.ofNullable(null);
-		}
-
 		// 操作日志列表
 		List<NodeOpt> nodeOpts = new ArrayList<>();
-
-		/**1、先检查前一共识周期是否有异常节点，有则在此时进行处罚*/
-		//查询在上一轮共识周期被设置为异常的节点列表
-		List<Staking> preExceptionNodeList = epochBusinessMapper.getException(preValidatorList);
-		if(!preExceptionNodeList.isEmpty()){
-			//处罚上一共识周期被设置为异常的节点：即上上轮低出块的节点;
-			BigInteger curConsEpochLastBlockNumber = EpochUtil.getCurEpochLastBlockNumber(BigInteger.valueOf(block.getNum()),blockChainConfig.getConsensusPeriodBlockCount());
-			BigInteger prePreConsEpochLastBlockNumber = curConsEpochLastBlockNumber.subtract(blockChainConfig.getConsensusPeriodBlockCount().multiply(BigInteger.valueOf(2)));
-			BigInteger prePreSettleEpoch = EpochUtil.getEpoch(prePreConsEpochLastBlockNumber,blockChainConfig.getSettlePeriodBlockCount());
-			List<NodeOpt> exceptionNodeOpts = slash(block,prePreSettleEpoch.intValue(),preExceptionNodeList, BusinessParam.YesNoEnum.YES,event.getEpochMessage().getBlockReward());
-			nodeOpts.addAll(exceptionNodeOpts);
-		}
-
-		/**2、再检查前一共识周期是否有低出块节点，把这些节点分成两类:
-		 *   a、参与当前共识周期，则标识为异常；
-		 *   b、不参与当前共识周期，则直接处罚；
-		 * */
-		//查询在上一轮共识周期出块率低的节点列表
-		List<Staking> preLowRateNodeList = epochBusinessMapper.querySlashNode(preValidatorList);
-		// 前一周期出块率低且在当前周期当选的异常节点列表, 这些列表的节点需要被设置为异常状态
-		List<String> curExceptionNodeList = new ArrayList<>();
-		List<Staking> curSlashNodeList = new ArrayList<>();
-		// 前一周期出块率低且在当前周期未当选的低出块节点列表, 这些列表的节点需要被处罚
-		List<Staking> preSlashNodeList = new ArrayList<>();
-		preLowRateNodeList.forEach(n->{
-			if(curValidatorList.contains(n.getNodeId())){
-				curExceptionNodeList.add(n.getNodeId());
-				curSlashNodeList.add(n);
-			}else{
-				preSlashNodeList.add(n);
+		try {
+			Web3j web3j = platOnClient.getWeb3jWrapper().getWeb3j();
+			List<HistoryLowRateSlash> slashList = specialApi.getHistoryLowRateSlashList(web3j,BigInteger.valueOf(block.getNum()));
+			if(!slashList.isEmpty()){
+				List<String> slashNodeIdList = new ArrayList<>();
+				// 统一节点ID格式： 0x开头
+				slashList.forEach(n->slashNodeIdList.add(HexTool.prefix(n.getNodeId())));
+				log.info("低出块节点：{}",slashNodeIdList);
+				// 查询节点
+				StakingExample stakingExample = new StakingExample();
+				stakingExample.createCriteria()
+						.andStatusEqualTo(CustomStaking.StatusEnum.CANDIDATE.getCode())
+						.andNodeIdIn(slashNodeIdList);
+				List<Staking> slashStakingList = stakingMapper.selectByExample(stakingExample);
+				if(slashStakingList.isEmpty()){
+					log.info("特殊节点查询到的低出块率节点["+slashNodeIdList+"]在staking表中查询不到对应的候选中节点数据!");
+				}else {
+					//处罚低出块率的节点;
+					BigInteger curSettleEpoch = EpochUtil.getEpoch(BigInteger.valueOf(block.getNum()),blockChainConfig.getSettlePeriodBlockCount());
+					List<NodeOpt> exceptionNodeOpts = slash(block,curSettleEpoch.intValue(),slashStakingList, event.getEpochMessage().getBlockReward());
+					nodeOpts.addAll(exceptionNodeOpts);
+					log.debug("被处罚节点列表["+slashStakingList+"]");
+				}
 			}
-		});
-
-		if (!curExceptionNodeList.isEmpty()){
-			//a、参与当前共识周期，则标识为异常；且登记节点行为
-			epochBusinessMapper.setException(curExceptionNodeList);
-			List<NodeOpt> preLowRateNodeOpts = slashOnlyOpt(block, curSlashNodeList, event.getEpochMessage().getBlockReward());
-			nodeOpts.addAll(preLowRateNodeOpts);
+		} catch (Exception e) {
+			throw new BusinessException(e.getMessage());
 		}
-
-		if(!preSlashNodeList.isEmpty()){
-			//b、不参与当前共识周期，则直接处罚；
-			List<NodeOpt> preLowRateNodeOpts = slash(block,event.getEpochMessage().getSettleEpochRound().intValue(),preSlashNodeList, BusinessParam.YesNoEnum.NO,event.getEpochMessage().getBlockReward());
-			nodeOpts.addAll(preLowRateNodeOpts);
-		} 
-
 		log.debug("处理耗时:{} ms",System.currentTimeMillis()-startTime);
-
-       return Optional.ofNullable(nodeOpts);
+       return nodeOpts;
 	}
 
 	/**
@@ -114,61 +86,16 @@ public class OnElectionConverter {
 	 * @param block 区块
 	 * @param settleEpoch 所在结算周期
 	 * @param slashNodeList 被处罚的节点列表
-	 * @param isPrePreRound slashNodeList是否是上上个共识周期的低出块节点
 	 * @return
 	 */
-	private List<NodeOpt> slash(Block block, int settleEpoch, List<Staking> slashNodeList, BusinessParam.YesNoEnum isPrePreRound,BigDecimal blockReward){
+	private List<NodeOpt> slash(Block block, int settleEpoch, List<Staking> slashNodeList,BigDecimal blockReward){
 		//惩罚节点
 		Election election = Election.builder()
 				.time(block.getTime())
 				.settingEpoch(settleEpoch)
 				.slashNodeList(slashNodeList)
-				.isPrePreRound(isPrePreRound.getCode())
 				.build();
 		epochBusinessMapper.slashNode(election);
-
-		//节点操作日志
-		BigInteger bNum = BigInteger.valueOf(block.getNum());
-		List<NodeOpt> nodeOpts = slashNodeList.stream()
-				.map(node -> {
-					StringBuffer desc = new StringBuffer("0|");
-					/**
-					 * 如果低出块惩罚不等于0的时候，需要配置惩罚金额
-					 */
-					String amount =  blockReward
-							.multiply(blockChainConfig.getSlashBlockRewardCount()).toString();
-					desc.append(blockChainConfig.getSlashBlockRewardCount().toString()).append("|").append(amount).append( "|1");
-					NodeOpt nodeOpt = ComplementNodeOpt.newInstance();
-					nodeOpt.setId(networkStatCache.getAndIncrementNodeOptSeq());
-					nodeOpt.setNodeId(node.getNodeId());
-					nodeOpt.setType(Integer.valueOf(NodeOpt.TypeEnum.LOW_BLOCK_RATE.getCode()));
-					nodeOpt.setBNum(bNum.longValue());
-					nodeOpt.setTime(block.getTime());
-					nodeOpt.setDesc(desc.toString());
-					return nodeOpt;
-				}).collect(Collectors.toList());
-		return nodeOpts;
-	}
-	
-	/**
-	 * 不处罚节点只登记操作日志
-	 * @param block 区块
-	 * @param slashNodeList 被处罚的节点列表
-	 * @return
-	 */
-	private List<NodeOpt> slashOnlyOpt(Block block, List<Staking> slashNodeList,BigDecimal blockReward){
-
-		// 更新解质押到账需要经过的结算周期数
-		String configVal = parameterService.getValueInBlockChainConfig(ModifiableGovernParamEnum.UN_STAKE_FREEZE_DURATION.getName());
-		if(StringUtils.isBlank(configVal)){
-			throw new BusinessException("参数表参数缺失："+ModifiableGovernParamEnum.UN_STAKE_FREEZE_DURATION.getName());
-		}
-		Integer unStakeFreezeDuration = Integer.parseInt(configVal);
-		Election election = Election.builder()
-				.slashNodeList(slashNodeList)
-				.unStakeFreezeDuration(unStakeFreezeDuration)
-				.build();
-		epochBusinessMapper.updateUnStakeFreezeDuration(election);
 
 		//节点操作日志
 		BigInteger bNum = BigInteger.valueOf(block.getNum());
