c19fe1f47aa47127126a395a3f5ff1e3eee378d9
zhangrongjin <zhangrongjin@juzix.net>
2020-05-20 17:23:20 +0800

    fix

--- browser-service/src/main/java/com/platon/browser/service/govern/ParameterService.java
+++ browser-service/src/main/java/com/platon/browser/service/govern/ParameterService.java
@@ -167,7 +167,7 @@ public class ParameterService {
                 staleValue = chainConfig.getRewardPerChangeInterval().toString();
                 break;
             case INCREASE_ISSUANCE_RATIO:
-                staleValue = chainConfig.getAddIssueRate().multiply(new BigDecimal(10000)).toString();
+                staleValue = chainConfig.getAddIssueRate().multiply(new BigDecimal(10000)).toPlainString();
                 break;
             default:
                 break;
