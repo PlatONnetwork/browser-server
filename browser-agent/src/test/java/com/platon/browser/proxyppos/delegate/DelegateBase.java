b2b40163fb5d4575d1173c58d73d12c0f4abf103
chendongming <chendongming@juzix.net>
2020-06-08 14:29:40 +0800

    普通合约代理PPOS验证:版本声明、取消提案、投票提案、锁仓计划、双签举报

--- browser-agent/src/test/java/com/platon/browser/proxyppos/delegate/DelegateBase.java
+++ browser-agent/src/test/java/com/platon/browser/proxyppos/delegate/DelegateBase.java
@@ -17,6 +17,7 @@ public class DelegateBase extends TestBase {
         // lax1alckh87rsx0ks2vkh4wzt3477xk3vymcwldxf4
         Web3j web3j = Web3j.build(new HttpService("http://192.168.120.145:6790"));
         TransactionManager manager = new RawTransactionManager(web3j, delegateCredentials, chainId);
+        // 委托不能与质押使用同一个代理合约，否则会提示“当前账户不允许委托”
         ProxyContract contract = ProxyContract.load(proxyDelegateContractAddress, web3j, manager, gasProvider, chainId);
         BigInteger contractBalance = web3j.platonGetBalance(proxyDelegateContractAddress, DefaultBlockParameterName.LATEST).send().getBalance();
         BigInteger delegatorBalance = web3j.platonGetBalance(delegateCredentials.getAddress(chainId), DefaultBlockParameterName.LATEST).send().getBalance();
--- browser-agent/src/test/java/com/platon/browser/proxyppos/slash/SlashTest.java
+++ browser-agent/src/test/java/com/platon/browser/proxyppos/slash/SlashTest.java
@@ -37,13 +37,12 @@ public class SlashTest extends TestBase {
         /**
          * 双签证据的生成
          * ./duplicateSign -dtype 1 -sk <被举报节点私钥> -blskey <被举报节点BLS私钥> -blockNumber <最近出的一个块号>
-         *     ./duplicateSign -dtype 1 -sk c47153938d9bbbb2bbf3024c65b7676fceba40dd4b7e81a781102abdefb97039 -blskey 8ee60655f883f6f76702dce3624e286c3cb9345e58077223b801952ed3e8e819 -blockNumber 296941
+         *
+         * ./duplicateSign -dtype 1 -sk c47153938d9bbbb2bbf3024c65b7676fceba40dd4b7e81a781102abdefb97039 -blskey 8ee60655f883f6f76702dce3624e286c3cb9345e58077223b801952ed3e8e819 -blockNumber 296941
          */
-
 
         String evidencePath = this.getClass().getClassLoader().getResource("proxyppos/evidence.json").getPath();
         String evidence = FileUtils.readFileToString(new File(evidencePath),"UTF-8");
-
 
         BigInteger contractBalance = defaultWeb3j.platonGetBalance(proxyStakingContractAddress, DefaultBlockParameterName.LATEST).send().getBalance();
         BigInteger delegatorBalance = defaultWeb3j.platonGetBalance(defaultCredentials.getAddress(chainId), DefaultBlockParameterName.LATEST).send().getBalance();
@@ -56,6 +55,7 @@ public class SlashTest extends TestBase {
 
         Web3j web3j = Web3j.build(new HttpService("http://192.168.112.141:8789"));
         TransactionManager manager = new RawTransactionManager(web3j, delegateCredentials, chainId);
+        // 举报不能与质押使用同一个代理合约，否则会提示“不能举报自己”
         ProxyContract contract = ProxyContract.load(proxySlashContractAddress, web3j, manager, gasProvider, chainId);
         invokeProxyContract(
                 contract,
