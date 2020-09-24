7dc45e86c489e176a464a4db797b811056a7f0cc
Your Name <you@example.com>
2020-09-24 15:40:56 +0800

    完善controller

--- browser-api/src/main/java/com/platon/browser/controller/AppDocErc20TokenTransferRecordController.java
+++ browser-api/src/main/java/com/platon/browser/controller/AppDocErc20TokenTransferRecordController.java
@@ -1,8 +1,12 @@
 package com.platon.browser.controller;
 
+import com.platon.browser.common.BrowserConst;
+import com.platon.browser.config.CommonMethod;
+import com.platon.browser.now.service.Erc20TokenTransferRecordService;
 import com.platon.browser.req.token.QueryTokenTransferRecordListReq;
 import com.platon.browser.res.RespPage;
 import com.platon.browser.res.token.QueryTokenTransferRecordListResp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.WebAsyncTask;
 
@@ -17,8 +21,13 @@ import javax.validation.Valid;
 @RestController
 public class AppDocErc20TokenTransferRecordController implements AppDocErc20TokenTransferRecord {
 
+    @Autowired
+    private Erc20TokenTransferRecordService erc20TokenTransferRecordService;
+
     @Override
     public WebAsyncTask<RespPage<QueryTokenTransferRecordListResp>> tokenTransferList(@Valid QueryTokenTransferRecordListReq req) {
-        return null;
+        WebAsyncTask<RespPage<QueryTokenTransferRecordListResp>> webAsyncTask = new WebAsyncTask<>(BrowserConst.WEB_TIME_OUT, () -> erc20TokenTransferRecordService.queryTokenRecordList(req));
+        CommonMethod.onTimeOut(webAsyncTask);
+        return webAsyncTask;
     }
 }
