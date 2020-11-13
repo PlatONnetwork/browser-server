bbed5ee0653a54bec3ef306442bf9603b0829190
chendongming <chendongming@juzix.net>
2020-11-13 17:00:25 +0800

    零出块惩罚验证节点 & 历史验证节点  添加预估解锁块高

--- browser-service/src/main/java/com/platon/browser/now/service/impl/StakingServiceImpl.java
+++ browser-service/src/main/java/com/platon/browser/now/service/impl/StakingServiceImpl.java
@@ -42,6 +42,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Date;
