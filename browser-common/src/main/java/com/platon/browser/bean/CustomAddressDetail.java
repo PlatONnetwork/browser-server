775c9850a12873797f3d65f3a2d9762dc6d7c687
HYPWolf <hyp1573796294@gmail.com>
2021-01-27 18:10:17 +0800

    修改地址详情返回参数

---
+++ browser-common/src/main/java/com/platon/browser/bean/CustomAddressDetail.java
@@ -0,0 +1,20 @@
+package com.platon.browser.bean;
+
+import com.platon.browser.dao.entity.Address;
+import lombok.Data;
+
+/**
+ * 合约地址详情
+ *
+ * @author huangyongpeng@matrixelements.com
+ * @date 2021/1/27
+ */
+@Data
+public class CustomAddressDetail extends Address {
+
+    /**
+     * 合约类型：erc20 | erc721
+     */
+    private String tokenType;
+
+}
