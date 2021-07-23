10ea76ce3409730d54698e7838b7573195dd7d8d
LAPTOP-RO72N473\15737 <1573796294@qq.com>
2021-07-23 11:21:19 +0800

    修改bug19770

--- scan-api/src/main/java/com/platon/browser/service/InternalAddressService.java
+++ scan-api/src/main/java/com/platon/browser/service/InternalAddressService.java
@@ -1,5 +1,6 @@
 package com.platon.browser.service;
 
+import cn.hutool.core.collection.CollUtil;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.platon.browser.bean.CountBalance;
@@ -9,10 +10,12 @@ import com.platon.browser.dao.custommapper.CustomInternalAddressMapper;
 import com.platon.browser.request.PageReq;
 import com.platon.browser.response.RespPage;
 import com.platon.browser.response.address.InternalAddressResp;
+import com.platon.utils.Convert;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,18 +37,33 @@ public class InternalAddressService {
         RespPage<InternalAddressResp> respPage = new RespPage<>();
         InternalAddressResp internalAddressResp = new InternalAddressResp();
         List<CountBalance> countBalanceList = customInternalAddressMapper.countBalance();
-        CountBalance foundationValue = countBalanceList.stream().filter(v -> v.getType() == 0).findFirst().orElseGet(CountBalance::new);
-        internalAddressResp.setTotalBalance(foundationValue.getFree());
-        internalAddressResp.setTotalRestrictingBalance(foundationValue.getLocked());
+        CountBalance foundationValue = countBalanceList.stream()
+                                                       .filter(v -> v.getType() == 0)
+                                                       .findFirst()
+                                                       .orElseGet(CountBalance::new);
+        BigDecimal free = Convert.fromVon(foundationValue.getFree(), Convert.Unit.KPVON);
+        internalAddressResp.setTotalBalance(free);
+        BigDecimal lock = Convert.fromVon(foundationValue.getLocked(), Convert.Unit.KPVON);
+        internalAddressResp.setTotalRestrictingBalance(lock);
         List<InternalAddressResp> lists = new ArrayList<>();
         lists.add(internalAddressResp);
         InternalAddressExample internalAddressExample = new InternalAddressExample();
         internalAddressExample.createCriteria().andTypeEqualTo(0);
         internalAddressExample.setOrderByClause(" balance,restricting_balance desc");
         PageHelper.startPage(req.getPageNo(), req.getPageSize());
-        Page<InternalAddress> InternalAddressList = customInternalAddressMapper.selectListByExample(internalAddressExample);
-        internalAddressResp.setInternalAddressBaseResp(InternalAddressList);
-        respPage.init(InternalAddressList, lists);
+        Page<InternalAddress> internalAddressList = customInternalAddressMapper.selectListByExample(
+                internalAddressExample);
+        if (CollUtil.isNotEmpty(internalAddressList)) {
+            for (int i = 0; i < internalAddressList.size(); i++) {
+                BigDecimal balance = Convert.fromVon(internalAddressList.get(i).getBalance(), Convert.Unit.KPVON);
+                internalAddressList.get(i).setBalance(balance);
+                BigDecimal restrictingBalance = Convert.fromVon(internalAddressList.get(i).getRestrictingBalance(),
+                                                              Convert.Unit.KPVON);
+                internalAddressList.get(i).setRestrictingBalance(restrictingBalance);
+            }
+        }
+        internalAddressResp.setInternalAddressBaseResp(internalAddressList);
+        respPage.init(internalAddressList, lists);
         return respPage;
     }
 
