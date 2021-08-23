2b710d6a33027d26cde61c0f585f69fa26afaf15
chendongming <chendongming@juzix.net>
2021-08-23 16:34:33 +0800

    PRC20/PRC721 令牌详情      概览显示所属合约状态：接口 /token/detail 判断 isDestroy 0-否，1-是
    PRC20/PRC721 合约详情
         概览显示合约状态： 接口 address/details 取 isDestroy字段
         交易列表tab:  交易类型需要显示合约销毁状态
    
    地址详情：
          PRC20/PRC721 令牌tab: 增加合约状态列

--- scan-common/src/main/java/com/platon/browser/bean/CustomTokenDetail.java
+++ scan-common/src/main/java/com/platon/browser/bean/CustomTokenDetail.java
@@ -2,9 +2,6 @@ package com.platon.browser.bean;
 
 import lombok.Data;
 
-import java.math.BigDecimal;
-import java.util.Date;
-
 @Data
 public class CustomTokenDetail extends CustomToken{
     private String creator;
@@ -14,5 +11,10 @@ public class CustomTokenDetail extends CustomToken{
     private Integer txCount;
 
     private String binCode;
+
+    /**
+     * token所属合约是否已销毁：0-否，1-是
+     */
+    private int isContractDestroy;
 
 }
