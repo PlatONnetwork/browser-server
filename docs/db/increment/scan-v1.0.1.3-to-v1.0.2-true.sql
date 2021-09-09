c99d6dcf1cf22cf255da96e5aa085015640249f9
LAPTOP-RO72N473\15737 <1573796294@qq.com>
2021-09-09 10:12:12 +0800

    修改SQL

--- docs/db/increment/scan-v1.0.1.3-to-v1.0.2-true.sql
+++ docs/db/increment/scan-v1.0.1.3-to-v1.0.2-true.sql
@@ -13,3 +13,8 @@ ALTER TABLE token_inventory AUTO_INCREMENT=1;
 ALTER TABLE token_inventory ADD UNIQUE (`token_address`,`token_id`);
 
 alter table node add node_settle_statis_info text DEFAULT NULL COMMENT '节点结算周期的出块统计信息';
+
+ALTER TABLE token_expand add COLUMN `is_show_in_aton_admin` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否显示在aton管理台，1为显示，0不显示，默认是0';
+ALTER TABLE token_expand add COLUMN `is_show_in_scan_admin` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否显示在scan管理台，1为显示，0不显示，默认是0';
+update token_expand set `is_show_in_aton_admin` = 1 where `is_show_in_aton` = 1;
+update token_expand set `is_show_in_scan_admin` = 1 where `is_show_in_scan` = 1;
