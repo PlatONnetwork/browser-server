fb7f895e62c31ab177b7a66801c9b259ab7f3b81
LAPTOP-RO72N473\15737 <1573796294@qq.com>
2021-09-24 15:50:46 +0800

    已经解码后的版本号自动过滤

--- scan-service/src/main/java/com/platon/browser/config/json/CustomVersionSerializer.java
+++ scan-service/src/main/java/com/platon/browser/config/json/CustomVersionSerializer.java
@@ -1,5 +1,6 @@
 package com.platon.browser.config.json;
 
+import cn.hutool.core.util.StrUtil;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
@@ -10,21 +11,24 @@ import java.io.IOException;
 import java.math.BigInteger;
 
 /**
- *  返回版本转化实现类
- *  @file CustomVersionSerializer.java
- *  @description 使用方式在具体的get方法上添加@JsonSerialize(using = CustomVersionSerializer.class)
- *	@author zhangrj
- *  @data 2019年9月9日
+ * 返回版本转化实现类
+ *
+ * @author zhangrj
+ * @file CustomVersionSerializer.java
+ * @description 使用方式在具体的get方法上添加@JsonSerialize(using = CustomVersionSerializer.class)
+ * @data 2019年9月9日
  */
-public class CustomVersionSerializer  extends JsonSerializer<String>{
+public class CustomVersionSerializer extends JsonSerializer<String> {
 
-	@Override
-	public void serialize(String value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-		if(StringUtils.isNotBlank(value)) {
-			gen.writeString(ChainVersionUtil.toStringVersion(new BigInteger(value)));
-		} else {
-			gen.writeString("");
-		}
-	}
+    @Override
+    public void serialize(String value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        if (StrUtil.contains(value, ".")) {
+            gen.writeString(value);
+        } else if (StringUtils.isNotBlank(value)) {
+            gen.writeString(ChainVersionUtil.toStringVersion(new BigInteger(value)));
+        } else {
+            gen.writeString("");
+        }
+    }
 
 }
