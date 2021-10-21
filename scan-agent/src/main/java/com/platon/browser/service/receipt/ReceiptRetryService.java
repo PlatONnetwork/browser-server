4e9d78cf1a970687ebeb93fcfa699bb277a0be7e
LAPTOP-RO72N473\15737 <1573796294@qq.com>
2021-10-21 14:53:12 +0800

    修改日志输出

--- scan-agent/src/main/java/com/platon/browser/service/receipt/ReceiptRetryService.java
+++ scan-agent/src/main/java/com/platon/browser/service/receipt/ReceiptRetryService.java
@@ -1,5 +1,6 @@
 package com.platon.browser.service.receipt;
 
+import cn.hutool.core.util.StrUtil;
 import com.platon.browser.bean.ReceiptResult;
 import com.platon.browser.client.PlatOnClient;
 import com.platon.browser.bean.RpcParam;
@@ -18,32 +19,35 @@ import javax.annotation.Resource;
 @Slf4j
 @Service
 public class ReceiptRetryService {
+
     private static final String RECEIPT_RPC_INTERFACE = "platon_getTransactionByBlock";
+
     @Resource
     private PlatOnClient platOnClient;
 
     /**
      * 带有重试功能的根据区块号获取区块内所有交易的回执信息
+     *
      * @param blockNumber
      * @return 交易回扏信息
      * @throws
      */
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE)
-    public ReceiptResult getReceipt(Long blockNumber){
+    public ReceiptResult getReceipt(Long blockNumber) {
         long startTime = System.currentTimeMillis();
 
         try {
-            log.debug("获取回执:{}({})",Thread.currentThread().getStackTrace()[1].getMethodName(),blockNumber);
+            log.debug("获取回执:{}({})", Thread.currentThread().getStackTrace()[1].getMethodName(), blockNumber);
             RpcParam param = new RpcParam();
             param.setMethod(RECEIPT_RPC_INTERFACE);
             param.getParams().add(blockNumber);
             ReceiptResult result = platOnClient.getReceiptResult(blockNumber);
             log.debug("回执结果数:{}", result.getResult().size());
-            log.debug("处理耗时:{} ms",System.currentTimeMillis()-startTime);
+            log.debug("处理耗时:{} ms", System.currentTimeMillis() - startTime);
             return result;
-        }catch (Exception e){
+        } catch (Exception e) {
             platOnClient.updateCurrentWeb3jWrapper();
-            log.error("",e);
+            log.error(StrUtil.format("区块[{}]获取回执异常", blockNumber), e);
             throw new BusinessException(e.getMessage());
         }
     }
