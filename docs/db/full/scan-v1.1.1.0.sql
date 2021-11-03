0f443abbd1f0a38dac44e61acb3ce5b1ef7e746b
HYPWolf <hyp1573796294@gmail.com>
2021-11-03 17:45:00 +0800

    修改SQL

--- docs/db/full/scan-v1.1.1.0.sql
+++ docs/db/full/scan-v1.1.1.0.sql
@@ -400,7 +400,7 @@ CREATE TABLE `token` (
                          `token_tx_qty` int(11) NOT NULL DEFAULT '0' COMMENT 'token对应的交易数',
                          `holder` int(11) NOT NULL DEFAULT '0' COMMENT 'token对应的持有人的数量',
                          `contract_destroy_block` bigint(20) DEFAULT NULL COMMENT '合约的销毁块高',
-                         `contract_destroy_update` tinyint(1) NOT NULL DEFAULT '0' COMMENT '销毁的合约是否已更新显示在scan管理台，1为是，0为否，默认是0',
+                         `contract_destroy_update` tinyint(1) NOT NULL DEFAULT '0' COMMENT '销毁的合约是否已更新，1为是，0为否，默认是0',
                          PRIMARY KEY (`address`),
                          UNIQUE KEY `token_address` (`address`)
 );
