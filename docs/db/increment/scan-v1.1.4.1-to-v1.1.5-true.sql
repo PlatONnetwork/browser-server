197c8aad6c60be71c217e90664678b0d2d671b41
dex <dexin.y@digquant.com>
2022-06-29 15:40:24 +0800

    取消插入表 point_log 中 tx_1155_bak的初始记录

--- docs/db/increment/scan-v1.1.4.1-to-v1.1.5-true.sql
+++ docs/db/increment/scan-v1.1.4.1-to-v1.1.5-true.sql
@@ -1,7 +1,4 @@
 
-
-alter table tx_erc_721_bak add column  `token_id`  varchar(255) NOT NULL COMMENT 'tokenId' after `to_type`;
-
 alter table address add column `erc1155_tx_qty`  int(11)   NOT NULL DEFAULT '0' COMMENT 'erc1155 token对应的交易数' after `proposal_qty`;
 
 alter table network_stat add column  `erc1155_tx_qty`           int(11)          NOT NULL DEFAULT '0' COMMENT 'erc1155 token对应的交易数' after  `avg_pack_time`;
@@ -64,7 +61,4 @@ CREATE TABLE `tx_erc_1155_bak`
 INSERT INTO `point_log`(`id`, `type`, `name`, `desc`, `position`, `create_time`, `update_time`)
 VALUES (8, 1, 'tx_1155_bak', '从erc1155交易备份表统计地址表和token表交易数的断点记录', '0', '2021-12-06 02:47:41', '2021-12-06 02:47:41');
 INSERT INTO `point_log`(`id`, `type`, `name`, `desc`, `position`, `create_time`, `update_time`)
-VALUES (9, 1, 'tx_1155_bak', '从erc1155交易备份表统计TokenHolder持有者数的断点记录', '0', '2021-12-06 10:25:34', '2021-12-06 10:25:39');
-INSERT INTO `point_log`(`id`, `type`, `name`, `desc`, `position`, `create_time`, `update_time`)
-VALUES (10, 1, 'token_1155_inventory', '增量更新token1155库存信息断点记录', '0', '2021-12-10 02:44:32', '2021-12-10 02:44:32');
-
+VALUES (9, 1, 'token_1155_inventory', '增量更新token1155库存信息断点记录', '0', '2021-12-10 02:44:32', '2021-12-10 02:44:32');
