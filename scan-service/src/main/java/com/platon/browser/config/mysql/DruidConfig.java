22d7be5d4e2fd8b042339e454e7ac5bee7a7093e
chenxiaodai <chenxiaodai@126.com>
2023-06-26 14:55:59 +0800

    fix #185

--- scan-service/src/main/java/com/platon/browser/config/mysql/DruidConfig.java
+++ scan-service/src/main/java/com/platon/browser/config/mysql/DruidConfig.java
@@ -1,18 +1,11 @@
 package com.platon.browser.config.mysql;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import com.alibaba.druid.support.http.StatViewServlet;
-import com.alibaba.druid.support.http.WebStatFilter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Druid 配置
@@ -27,46 +20,4 @@ public class DruidConfig {
     public DataSource druid() {
         return new DruidDataSource();
     }
-
-    /**
-     * 配置 Druid 监控管理后台的Servlet
-     * 内置 Servlet 容器时没有web.xml文件，所以使用 Spring Boot 的注册 Servlet 方式
-     *
-     * @param
-     * @return org.springframework.boot.web.servlet.ServletRegistrationBean
-     * @date 2021/4/1
-     */
-    @Bean
-    public ServletRegistrationBean statViewServlet() {
-        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
-        // 这些参数可以在 com.alibaba.druid.support.http.StatViewServlet 的父类 com.alibaba.druid.support.http.ResourceServlet 中找到
-        Map<String, String> initParams = new HashMap<>();
-        initParams.put("loginUsername", "admin");
-        initParams.put("loginPassword", "123456");
-        //默认就是允许所有访问
-        initParams.put("allow", "");
-        // deny：Druid 后台拒绝谁访问，表示禁止此ip访问
-        // initParams.put("deny","192.168.10.132");
-        bean.setInitParameters(initParams);
-        return bean;
-    }
-
-    /**
-     * 配置一个web监控的filter
-     *
-     * @param
-     * @return org.springframework.boot.web.servlet.FilterRegistrationBean
-     * @date 2021/4/1
-     */
-    @Bean
-    public FilterRegistrationBean webStatFilter() {
-        FilterRegistrationBean bean = new FilterRegistrationBean();
-        bean.setFilter(new WebStatFilter());
-        Map<String, String> initParams = new HashMap<>();
-        initParams.put("exclusions", "*.js,*.css,/druid/*");
-        bean.setInitParameters(initParams);
-        bean.setUrlPatterns(Arrays.asList("/*"));
-        return bean;
-    }
-
 }
