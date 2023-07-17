fe82344fefc6a38994eae81b9bf9842af3f040c4
formingform <lvxiaoyi@matrixelements.com>
2023-07-17 11:19:04 +0800

    docs: describe contract type enums

--- scan-common/src/main/java/com/platon/browser/bean/ContractInfo.java
+++ scan-common/src/main/java/com/platon/browser/bean/ContractInfo.java
@@ -6,8 +6,17 @@ import java.math.BigDecimal;
 
 @Data
 public class ContractInfo {
+
     private String address;
     private String bin;     //为空不传
+    /**
+     * 	EVM     ContractType = iota + 1 //1, start with 1
+     * 	WASM                            //2
+     * 	_                               //3, skipped
+     * 	ERC20                           //4
+     * 	ERC721                          //5
+     * 	ERC1155                         //6
+     */
     private int contractType; //为空不传 //0:erc20; 1:erc721; 2:erc1155; 3:general
     private String tokenName; //为空不传
     private String tokenSymbol; //为空不传
