298fc6e4c2c2b79535703e07bc9ed7a9788bd9a6
chenxiaodai <chenxiaodai@126.com>
2023-07-27 17:22:42 +0800

    版本号升级为1.4.0.1

--- docs/db/increment/scan-v1.4.0.0-to-v1.4.0.1-false.sql
+++ docs/db/increment/scan-v1.4.0.0-to-v1.4.0.1-false.sql
@@ -1,3 +1,3 @@
 USE `scan_platon`;
 
-INSERT INTO `point_log`(`id`, `type`, `name`, `desc`, `position`) SELECT 12 AS 'id', 1 AS 'type', 'tx_1155_bak' AS 'name', '从tx_1155_bak交易备份表统计TokenHolder持有者数的断点记录' AS 'desc', tb.`id` AS 'position' FROM `tx_erc_1155_bak` tb ORDER BY tb.`id` DESC LIMIT 1;
+INSERT INTO `point_log`(`id`, `type`, `name`, `desc`, `position`) SELECT * FROM (( SELECT 12 AS 'id', 1 AS 'type', 'tx_1155_bak' AS 'name', '从tx_1155_bak交易备份表统计TokenHolder持有者数的断点记录' AS 'desc', tb.`id` AS 'position' FROM `tx_erc_1155_bak` tb ORDER BY tb.`id` DESC LIMIT 1 ) UNION ( SELECT 12 AS 'id', 1 AS 'type', 'tx_1155_bak' AS 'name', '从tx_1155_bak交易备份表统计TokenHolder持有者数的断点记录' AS 'desc', '0' AS 'position')) t LIMIT 1;
